b1051c7855a6a55debab104f8b2ff9adfad7f309
Haz-git <zhouharry1@gmail.com>
2021-06-21 13:52:24 -0700

    Converted to functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Holding landing page for rankmyramen</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+const App = () => {
+    return <div>Hold for landing page</div>;
+};
 
 export default App;
