0ff68191aafe944d6da3a0b3719efe484709663d
Haz-git <zhouharry1@gmail.com>
2021-06-22 10:16:55 -0700

    Added backdrop

---
+++ src/components/backdrop/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+const index = () => {
+    return <div>Find and review your favorite instant ramen.</div>;
+};
+
+export default index;
