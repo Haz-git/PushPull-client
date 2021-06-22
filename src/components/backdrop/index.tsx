6b03cbf2b259530bee3fc0d64456b78ca7040176
Haz-git <zhouharry1@gmail.com>
2021-06-22 10:36:32 -0700

    Added input field to backdrop

--- src/components/backdrop/index.tsx
+++ src/components/backdrop/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+//Components:
+import SearchBar from '../general_components/SearchBar';
+
 //Styles:
+const MainContainer = styled.section`
+    border: 1px solid black;
+    padding: 1em 1em;
+    text-align: center;
+    margin: 0 auto;
+`;
 
 const index = () => {
-    return <div>Find and review your favorite instant ramen.</div>;
+    return (
+        <MainContainer>
+            Find and review your favorite instant ramen.
+            <SearchBar />
+        </MainContainer>
+    );
 };
 
 export default index;
