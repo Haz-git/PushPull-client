336d6c3aa9d3d44f68c97ee9b9c01ff8cb08030e
Haz-git <zhouharry1@gmail.com>
2021-06-22 10:05:27 -0700

    Minor details

--- src/components/nav_bar/index.tsx
+++ src/components/nav_bar/index.tsx
@@ -4,10 +4,11 @@ import styled from 'styled-components';
 //Styles:
 const MainContainer = styled.div`
     border: 1px solid black;
+    padding: 1rem 1rem;
 `;
 
 const index = () => {
-    return <MainContainer>RamenRankings</MainContainer>;
+    return <MainContainer>RamenRankings (Logo)</MainContainer>;
 };
 
 export default index;
