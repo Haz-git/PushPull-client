36e2ae55c4bc91251b849e7885fc9893be28936f
Haz-git <zhouharry1@gmail.com>
2021-08-03 14:37:37 -0700

    removed StyledComponent

--- src/components/nav_bar/index.tsx
+++ src/components/nav_bar/index.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import styled from 'styled-components';
-
-//Styles:
-const MainContainer = styled.div`
-    border: 1px solid black;
-    padding: 1rem 1rem;
-`;
 
 const index = () => {
-    return <MainContainer>RamenRankings (Logo)</MainContainer>;
+    return <div>RamenRankings (Logo)</div>;
 };
 
 export default index;
