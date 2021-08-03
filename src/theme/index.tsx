e3029daacc0f77d4244d7c3cae08c6bb8aaee047
Haz-git <zhouharry1@gmail.com>
2021-08-03 15:22:41 -0700

    Replaced globalstyle with theme

---
+++ src/theme/index.tsx
@@ -0,0 +1,9 @@
+const themeObject = {
+    fontFamily: 'Lato, Nunito, Roboto',
+    shadows: {
+        md: '1px 1px 3px rgba(0,0,0,.25)',
+        xl: '5px 5px 3px rgba(0,0,0,.25)',
+    },
+};
+
+export default themeObject;
