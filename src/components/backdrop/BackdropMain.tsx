e1c79f8b1d81bb19425d19f6efee94f46c84097a
Haz-git <zhouharry1@gmail.com>
2021-08-04 10:05:32 -0700

    Changed text sizings

---
+++ src/components/backdrop/BackdropMain.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+const BackdropMainContainer = styled.section`
+    background: salmon;
+    width: 100%;
+`;
+
+const BackdropMain = () => {
+    return <BackdropMainContainer>backdrop</BackdropMainContainer>;
+};
+
+export default BackdropMain;
