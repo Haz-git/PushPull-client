b237813f11ce082c0c65c6764397c17643b07153
Haz-git <zhouharry1@gmail.com>
2021-08-04 07:09:34 -0700

    Added new components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 //Components
-import Navbar from './nav_bar';
+import Navbar from './nav_bar/Navbar';
 import Backdrop from './backdrop';
 import FeatureSectionOne from './body_section/FeatureSectionOne';
 import FeatureSectionTwo from './body_section/FeatureSectionTwo';
@@ -9,8 +9,6 @@ import ReviewSectionThree from './body_section/ReviewSectionThree';
 import GlobalSearchSectionFour from './body_section/GlobalSearchSectionFour';
 import Footer from './footer';
 import GeneralButton from './general_components/GeneralButton';
-
-//Styles:
 
 const App = () => {
     return (
