53395257144a2213e154480fe522c67f6d338e6c
Haz-git <zhouharry1@gmail.com>
2021-08-04 08:43:59 -0700

    Testing

--- src/components/nav_bar/Navbar.tsx
+++ src/components/nav_bar/Navbar.tsx
@@ -1,35 +1,21 @@
 import * as React from 'react';
 
 //Styles:
-import { createUseStyles } from 'react-jss';
-import { Container, theming } from '@mantine/core';
+import styled from 'styled-components';
 
-const useStyles = createUseStyles(
-    (theme) => ({
-        navbar__container: {
-            boxShadow: theme.shadows.sm,
-            padding: '1rem 1rem',
-        },
-        navbar__logo: {
-            fontFamily: 'Lato, sans-serif',
-            fontWeight: '700',
-            fontSize: theme.fontSizes.sm,
-            padding: '0',
-            margin: '0',
-        },
-    }),
-    { theming }
-);
+const StyledNavbar = styled.nav`
+    background: ${(props) => props.theme.background};
+    width: 100%;
+    border: 1px solid black;
+`;
 
 //Interfaces:
 
 const Navbar = () => {
-    const classes = useStyles();
-
     return (
-        <nav className={classes.navbar__container}>
-            <h1 className={classes.navbar__logo}>RankMyWorkout</h1>
-        </nav>
+        <StyledNavbar>
+            <h1>RankMyWorkout</h1>
+        </StyledNavbar>
     );
 };
 
