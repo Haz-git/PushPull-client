67cba8039108677389ab72abcfe3982a5542b0a4
Haz-git <zhouharry1@gmail.com>
2021-08-05 08:22:43 -0700

    Added some common themes

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,7 +1,22 @@
 export const lightTheme = {
-    background: '#ffffff',
+    background: 'rgba(255, 255, 255, 1)',
+    fontSizes: {
+        xs: '.5rem',
+        sm: '.8rem',
+        md: '1.1rem',
+        lg: '1.5rem',
+        xl: '2rem',
+    },
+    shadows: {
+        sm: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 2px 4px;',
+    },
 };
 
 export const darkTheme = {
-    background: '#1c1e37',
+    background: 'rgba(28, 30, 55, 1)',
+    sizes: {
+        sm: '.8rem',
+        md: '1.1rem',
+        lg: '1.4rem',
+    },
 };
