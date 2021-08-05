9f2672e7d904b12a7376fdc7417211e9ade30cde
Haz-git <zhouharry1@gmail.com>
2021-08-05 09:30:41 -0700

    Uploaded SVG and backdropText

--- src/components/backdrop/BackdropMain.tsx
+++ src/components/backdrop/BackdropMain.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
 
+//Components:
+import BackdropText from './backdrop_components/BackdropText';
+import { ReactComponent as BackdropSVG } from '../../assets/backdrop_workout_pic.svg';
+
 //Styles:
 import styled from 'styled-components';
 
 const BackdropMainContainer = styled.section`
-    background: salmon;
+    display: grid;
+    grid-template-columns: 50% auto;
+    align-items: center;
+    justify-content: center;
     width: 100%;
+    background: rgba(255, 183, 0, 1);
+`;
+
+const SVGContainer = styled.div`
+    height: 31rem;
+    width: 31rem;
 `;
 
 const BackdropMain = () => {
-    return <BackdropMainContainer></BackdropMainContainer>;
+    return (
+        <BackdropMainContainer>
+            <BackdropText />
+            <SVGContainer>
+                <BackdropSVG />
+            </SVGContainer>
+        </BackdropMainContainer>
+    );
 };
 
 export default BackdropMain;
