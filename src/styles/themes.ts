408ef745f4ed26aa9719fd39724afdded6aac792
Haz-git <zhouharry1@gmail.com>
2021-08-05 09:31:14 -0700

    Added some basic styling themes

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,11 +1,16 @@
 export const lightTheme = {
     background: 'rgba(255, 255, 255, 1)',
+    mainText: 'rgba(0, 0, 34, 1)',
+    subText: 'rgba(0, 0, 34, .7)',
     fontSizes: {
         xs: '.5rem',
         sm: '.8rem',
         md: '1.1rem',
         lg: '1.5rem',
         xl: '2rem',
+    },
+    textShadows: {
+        sm: '1px 1px 1px rgba(0,0,0,.25)',
     },
     shadows: {
         sm: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 2px 4px;',
