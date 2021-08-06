c19e06663e529e91ee2415bf799ba348dc28c708
Haz-git <zhouharry1@gmail.com>
2021-08-06 07:05:34 -0700

    Added search bar

--- src/components/backdrop/BackdropMain.tsx
+++ src/components/backdrop/BackdropMain.tsx
@@ -3,32 +3,47 @@ import * as React from 'react';
 //Components:
 import BackdropText from './backdrop_components/BackdropText';
 import { ReactComponent as BackdropSVG } from '../../assets/backdrop_workout_pic.svg';
+import SearchBar from '../general_components/SearchBar';
 
 //Styles:
 import styled from 'styled-components';
 
 const BackdropMainContainer = styled.section`
+    width: 100%;
+    background: rgba(255, 183, 0, 1);
+    padding: 2rem 0;
+`;
+
+const GridWrapper = styled.div`
     display: grid;
     grid-template-columns: 40% auto;
     align-items: center;
     justify-content: center;
-    width: 100%;
-    background: rgba(255, 183, 0, 1);
 `;
 
 const SVGContainer = styled.div`
-    margin-top: -5rem;
+    margin-top: -6rem;
     height: 30rem;
     width: 30rem;
+`;
+
+const SearchBarContainer = styled.div`
+    padding: 0 5rem;
+    width: 100%;
 `;
 
 const BackdropMain = () => {
     return (
         <BackdropMainContainer>
-            <BackdropText />
-            <SVGContainer>
-                <BackdropSVG />
-            </SVGContainer>
+            <GridWrapper>
+                <BackdropText />
+                <SVGContainer>
+                    <BackdropSVG />
+                </SVGContainer>
+            </GridWrapper>
+            <SearchBarContainer>
+                <SearchBar />
+            </SearchBarContainer>
         </BackdropMainContainer>
     );
 };
