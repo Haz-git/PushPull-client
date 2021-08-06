7c92ff7f5138c5271f50808b4b5b768d0c3e25aa
Haz-git <zhouharry1@gmail.com>
2021-08-06 09:12:48 -0700

    Added styling and containers

--- src/components/body_section/body_section_components/BodySection.tsx
+++ src/components/body_section/body_section_components/BodySection.tsx
@@ -7,18 +7,46 @@ import styled from 'styled-components';
 
 const MainContainer = styled.section``;
 
-const ItemWrapper = styled.div``;
+const ItemWrapper = styled.div`
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    grid-template-columns: auto 40%;
+`;
 
-const TextContainer = styled.div``;
+const SVGContainer = styled.div`
+    height: 40rem;
+    width: 40rem;
+`;
 
-const BodyHeader = styled.h2``;
+const TextContainer = styled.div`
+    text-align: left;
+    margin-left: 2rem;
+`;
 
-const BodyDesc = styled.p``;
+const BodyHeader = styled.h2`
+    font-size: ${(props) => props.theme.fontSizes.xxl};
+    color: ${(props) => props.theme.mainText};
+    font-weight: 700;
+    margin: 2rem 0;
+`;
+
+const BodyDesc = styled.p`
+    font-size: ${(props) => props.theme.fontSizes.lg};
+    color: ${(props) => props.theme.subText};
+    font-weight: 600;
+    margin: 2rem 0;
+`;
+
+const ButtonContainer = styled.div`
+    max-width: 8rem;
+    margin: 2rem 0;
+`;
 
 //Interfaces:
 
 interface IComponentProps {
-    SVGImage?: HTMLElement & SVGElement;
+    SVGImage?: JSX.Element;
     textHeader?: string;
     textDesc?: string;
     primaryButton?: JSX.Element;
@@ -33,9 +61,11 @@ const BodySection = ({
     return (
         <MainContainer>
             <ItemWrapper>
+                <SVGContainer>{SVGImage}</SVGContainer>
                 <TextContainer>
                     <BodyHeader>{textHeader}</BodyHeader>
                     <BodyDesc>{textDesc}</BodyDesc>
+                    <ButtonContainer>{primaryButton}</ButtonContainer>
                 </TextContainer>
             </ItemWrapper>
         </MainContainer>
