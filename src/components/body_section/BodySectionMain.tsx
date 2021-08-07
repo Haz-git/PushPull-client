5535fcf4114c5a9bc32d310661eb6e77749bc3d3
Haz-git <zhouharry1@gmail.com>
2021-08-07 07:58:31 -0700

    Created reversed body section, altered svg

--- src/components/body_section/BodySectionMain.tsx
+++ src/components/body_section/BodySectionMain.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
+import { deviceMin } from '../../devices/breakpoints';
 
 //Components:
 import BodySection from './body_section_components/BodySection';
 import GeneralButton from '../general_components/GeneralButton';
 import { ReactComponent as SquatSVG } from '../../assets/body_section_squat.svg';
+import { ReactComponent as RankingSVG } from '../../assets/body_section_ranking.svg';
+import { ReactComponent as ReviewSVG } from '../../assets/body_section_review.svg';
 
 //Styles:
 import styled from 'styled-components';
 const MainBodySectionContainer = styled.div``;
+
+const StyledRankingSVG = styled(RankingSVG)`
+    margin-top: 6rem;
+
+    @media ${deviceMin.mobileS} {
+        margin-top: 1rem;
+    }
+
+    @media ${deviceMin.browserSm} {
+        margin-top: -3rem;
+    }
+
+    @media ${deviceMin.laptopHalf} {
+        margin-top: 6rem;
+    }
+
+    @media ${deviceMin.laptopS} {
+    }
+`;
 
 //Interfaces:
 
@@ -26,6 +48,23 @@ const BodySectionMain = () => {
                     />
                 }
                 SVGImage={<SquatSVG />}
+            />
+            <BodySection
+                isReversed={true}
+                textHeader="Add a Ranking"
+                textDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur metus sit amet dui finibus sodales."
+                primaryButton={
+                    <GeneralButton
+                        buttonLabel="Add Ranking"
+                        onClick={() => console.log('test')}
+                        fontSize="1.25rem"
+                        padding="1rem 1rem"
+                    />
+                }
+                SVGImage={<StyledRankingSVG />}
+                backgroundColor="#0F0628"
+                mainTextColor="rgba(255, 255, 255, 1)"
+                subTextColor="rgba(255, 255, 255, .7)"
             />
         </MainBodySectionContainer>
     );
