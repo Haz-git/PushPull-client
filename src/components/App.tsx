16812b96b1d8c60c5fa958ac92cf211e304a990b
Haz-git <zhouharry1@gmail.com>
2021-08-09 07:04:03 -0700

    Re-ordered frontpage components into MainLandingPage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
+import { Switch, Router, Route } from 'react-router-dom';
 
 //Styles:
 import GlobalStyle from '../styles/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from '../styles/themes';
 
-//Components
+//Components:
+import history from '../utils/historyObject';
 import Navbar from './nav_bar/Navbar';
-import Backdrop from './backdrop/BackdropMain';
-import BodySectionMain from './body_section/BodySectionMain';
-import MainFooter from './footer/MainFooter';
+import MainLandingPageView from './main_landing_page/MainLandingPageView';
+import MainSearchPage from './search_page/MainSearchPage';
 
 const App = () => {
     return (
         <>
             <ThemeProvider theme={lightTheme}>
                 <GlobalStyle />
-                <Navbar />
-                <Backdrop />
-                <BodySectionMain />
-                <MainFooter />
+                <Router history={history}>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={MainLandingPageView} />
+                        <Route
+                            exact
+                            path="/search"
+                            component={MainSearchPage}
+                        />
+                    </Switch>
+                </Router>
             </ThemeProvider>
         </>
     );
