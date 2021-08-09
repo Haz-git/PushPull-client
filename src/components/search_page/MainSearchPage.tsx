6920e1807f1e95476ef6cd4aded18a2f16d855de
Haz-git <zhouharry1@gmail.com>
2021-08-09 06:48:33 -0700

    Added directory for searching for categories and exercises

---
+++ src/components/search_page/MainSearchPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const MainSearchPage = () => {
+    return <div>Should be viewing exercises here</div>;
+};
+
+export default MainSearchPage;
