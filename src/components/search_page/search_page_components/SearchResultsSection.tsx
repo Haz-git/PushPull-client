3ccd00067616eeb409782771e56e15ddffabf937
Haz-git <zhouharry1@gmail.com>
2021-08-13 15:04:10 -0700

    Fleshing out features

--- src/components/search_page/search_page_components/SearchResultsSection.tsx
+++ src/components/search_page/search_page_components/SearchResultsSection.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 
 //Components:
+import SearchBar from '../../general_components/SearchBar';
 
 //Styles:
 import styled from 'styled-components';
 
+const MainContainer = styled.section`
+    padding: 2rem 2rem;
+`;
+
+const SearchResultsTextContainer = styled.div`
+    margin: 2rem 0;
+`;
+
+const SearchResultsText = styled.h3`
+    color: ${(props) => props.theme.mainText};
+    font-size: ${(props) => props.theme.fontSizes.lg};
+    font-weight: 700;
+`;
+
 //Interfaces:
 
 const SearchResultsSection = () => {
-    return <div>Search Results should be here.</div>;
+    return (
+        <MainContainer>
+            <SearchBar placeholderText="Search again..." />
+            <SearchResultsTextContainer>
+                <SearchResultsText>
+                    (10) Search results found for 'barbell'
+                </SearchResultsText>
+            </SearchResultsTextContainer>
+        </MainContainer>
+    );
 };
 
 export default SearchResultsSection;
