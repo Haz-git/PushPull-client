d68d1abef4bbfb61c8fc19dc2a54776be777959c
Haz-git <zhouharry1@gmail.com>
2021-08-13 13:35:52 -0700

    Introduced filter buttons to columns

--- src/components/search_page/search_page_components/FilterColumn.tsx
+++ src/components/search_page/search_page_components/FilterColumn.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 //Components:
 import DividerLine from '../../general_components/DividerLine';
+import FilterButton from './FilterButton';
 
 //Styles:
 import styled from 'styled-components';
@@ -12,10 +13,18 @@ const MainContainer = styled.div`
 `;
 
 const FilterText = styled.h2`
-    padding: 2rem 1rem;
+    padding: 2rem 2rem;
     font-size: ${(props) => props.theme.fontSizes.lg};
     color: ${(props) => props.theme.accentColors.orange};
     font-weight: 800;
+`;
+
+const ButtonsContainer = styled.div`
+    padding: 2rem 1rem;
+`;
+
+const ButtonDivider = styled.div`
+    height: 0.5rem;
 `;
 
 //interfaces:
@@ -25,6 +34,13 @@ const FilterColumn = () => {
         <MainContainer>
             <FilterText>Filter By</FilterText>
             <DividerLine border="1px solid #e5e5e5" />
+            <ButtonsContainer>
+                <FilterButton filterLabel="Category" />
+                <ButtonDivider />
+                <FilterButton filterLabel="Location" />
+                <ButtonDivider />
+                <FilterButton filterLabel="Equipment" />
+            </ButtonsContainer>
         </MainContainer>
     );
 };
