7a60ac3843a3773983a666bcc4afcc410f3fd956
Haz-git <zhouharry1@gmail.com>
2021-08-13 09:57:31 -0700

    Added filter column and results container

---
+++ src/components/search_page/search_page_components/FilterColumn.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+//interfaces:
+
+const FilterColumn = () => {
+    return <div>This should be the filter column</div>;
+};
+
+export default FilterColumn;
---
+++ src/components/search_page/search_page_components/SearchResultsSection.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+//Interfaces:
+
+const SearchResultsSection = () => {
+    return <div>Search Results should be here.</div>;
+};
+
+export default SearchResultsSection;
