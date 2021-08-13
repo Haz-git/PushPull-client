176b2346bc57c8a7eb9a0c8c8c271b545784d835
Haz-git <zhouharry1@gmail.com>
2021-08-13 09:57:17 -0700

    Imported two search page components

--- src/components/search_page/MainSearchPage.tsx
+++ src/components/search_page/MainSearchPage.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
 
 //Components:
+import FilterColumn from './search_page_components/FilterColumn';
+import SearchResultsSection from './search_page_components/SearchResultsSection';
 
 //Styles:
+import styled from 'styled-components';
+
+const MainSearchPageContainer = styled.section`
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 2fr 6fr;
+`;
 
 //Interfaces:
 
 const MainSearchPage = () => {
-    return <div>Should be viewing exercises here</div>;
+    return (
+        <MainSearchPageContainer>
+            <FilterColumn />
+            <SearchResultsSection />
+        </MainSearchPageContainer>
+    );
 };
 
 export default MainSearchPage;
