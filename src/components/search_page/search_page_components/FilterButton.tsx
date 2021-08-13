60c9180cbc8ee4d0a3156eedb8dabf2801ebdedb
Haz-git <zhouharry1@gmail.com>
2021-08-13 13:35:36 -0700

    Added reusable filter button for sorting components

--- src/components/search_page/search_page_components/FilterButton.tsx
+++ src/components/search_page/search_page_components/FilterButton.tsx
@@ -4,11 +4,51 @@ import * as React from 'react';
 
 //Styles:
 import styled from 'styled-components';
+import { ChevronRight } from '@styled-icons/boxicons-solid/ChevronRight';
+
+//Icons:
+const RightArrowIcon = styled(ChevronRight)`
+    height: 1.25rem;
+    width: 1.25rem;
+    color: ${(props) => props.theme.subText};
+`;
+
+const MainContainer = styled.button`
+    padding: 1rem 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 0.3rem;
+    cursor: pointer;
+    border: none;
+    background: inherit;
+    transition: 0.3s all ease-in-out;
+
+    &:hover {
+        text-decoration: none;
+        outline: none;
+        background: #dbdbdb;
+    }
+`;
+
+const FilterLabelText = styled.h3`
+    font-size: ${(props) => props.theme.fontSizes.md};
+    color: ${(props) => props.theme.subText};
+`;
 
 //Interfaces:
+interface IComponentProps {
+    filterLabel: string;
+}
 
-const FilterButton = () => {
-    return <div></div>;
+const FilterButton = ({ filterLabel }: IComponentProps): JSX.Element => {
+    return (
+        <MainContainer>
+            <FilterLabelText>{filterLabel}</FilterLabelText>
+            <RightArrowIcon />
+        </MainContainer>
+    );
 };
 
 export default FilterButton;
