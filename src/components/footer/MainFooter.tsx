7c1c651b4df44d4e1eeba075f681921c9b540f00
Haz-git <zhouharry1@gmail.com>
2021-08-15 07:06:59 -0700

    Added responsive footer

--- src/components/footer/MainFooter.tsx
+++ src/components/footer/MainFooter.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { deviceMin } from '../../devices/breakpoints';
 
 //Components:
 
@@ -10,22 +11,72 @@ const MainContainer = styled.section`
     align-items: center;
     justify-content: space-between;
     background: ${(props) => props.theme.lightBackground};
-    padding: 2rem 1rem;
+    padding: 1rem 1rem;
+
+    @media ${deviceMin.mobileS} {
+        display: block;
+        text-align: center;
+        padding: 1rem 1rem;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 1rem;
+    }
 `;
 
-const FooterBlock = styled.div``;
+const FooterTextBlock = styled.div`
+    @media ${deviceMin.mobileS} {
+        margin-bottom: 1rem;
+    }
+
+    @media ${deviceMin.browserSm} {
+        margin-bottom: 0;
+    }
+`;
+
+const FooterBlock = styled.div`
+    @media ${deviceMin.mobileS} {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 1rem;
+    }
+`;
 
 const FooterText = styled.p`
     color: ${(props) => props.theme.mainText};
-    font-size: ${(props) => props.theme.md};
     font-weight: 700;
+
+    @media ${deviceMin.mobileS} {
+        font-size: 0.8rem;
+    }
+
+    @media ${deviceMin.tablet} {
+        font-size: ${(props) => props.theme.fontSizes.md};
+    }
 `;
 
 const FooterTextLinks = styled.a`
     color: ${(props) => props.theme.mainText};
-    margin: 0 1rem;
-    font-size: ${(props) => props.theme.md};
     font-weight: 700;
+
+    @media ${deviceMin.mobileS} {
+        font-size: 0.8rem;
+        margin: 0 0.2rem;
+    }
+
+    @media ${deviceMin.tablet} {
+        margin: 0 1rem;
+        font-size: ${(props) => props.theme.fontSizes.md};
+    }
 `;
 
 //Interfaces:
@@ -33,9 +84,9 @@ const FooterTextLinks = styled.a`
 const MainFooter = () => {
     return (
         <MainContainer>
-            <FooterBlock>
+            <FooterTextBlock>
                 <FooterText>© 2021 PushPull. All rights reserved.</FooterText>
-            </FooterBlock>
+            </FooterTextBlock>
             <FooterBlock>
                 <FooterTextLinks href="https://storyset.com" target="_blank">
                     Illustrations by StorySet
