06cd30f1546769fbb6ffeec55ddaa048113f179d
Haz-git <zhouharry1@gmail.com>
2021-08-17 07:50:36 -0700

    Added view logic

--- src/components/search_page/search_page_components/FilterColumn.tsx
+++ src/components/search_page/search_page_components/FilterColumn.tsx
@@ -7,6 +7,17 @@ import FilterButton from './FilterButton';
 
 //Styles:
 import styled from 'styled-components';
+import { ChevronLeft } from '@styled-icons/boxicons-solid/ChevronLeft';
+
+//Icons:
+
+const LeftArrowIcon = styled(ChevronLeft)`
+    height: 0.9rem;
+    width: 0.9rem;
+    margin-right: 0.15rem;
+    margin-bottom: 0.035rem;
+    color: ${(props) => props.theme.subText};
+`;
 
 const MainContainer = styled.div`
     background: rgba(239, 239, 239, 1);
@@ -15,9 +26,17 @@ const MainContainer = styled.div`
     height: 100%;
 `;
 
-const FilterText = styled.h2`
+const TitleContainer = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: column;
+    text-align: left;
     padding: 2rem 2rem;
-    font-size: ${(props) => props.theme.fontSizes.lg};
+`;
+
+const FilterText = styled.h2`
+    font-size: ${(props) => props.theme.fontSizes.xl};
     color: ${(props) => props.theme.accentColors.orange};
     font-weight: 800;
 `;
@@ -28,6 +47,28 @@ const ButtonsContainer = styled.div`
 
 const ButtonDivider = styled.div`
     height: 0.5rem;
+`;
+
+const ReturnButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    outline: none;
+    color: ${(props) => props.theme.subText};
+    font-size: 0.8rem;
+    font-weight: 700;
+    cursor: pointer;
+    padding: 0.25rem 0.25rem;
+    background: inherit;
+    border-radius: 0.3rem;
+    transition: 0.3s all ease-in-out;
+
+    &:hover {
+        text-decoration: none;
+        outline: none;
+        background: #dbdbdb;
+    }
 `;
 
 //interfaces:
@@ -46,23 +87,71 @@ const FilterColumn = () => {
         RenderView.RENDER_MAIN_FILTER
     );
 
-    return (
-        <MainContainer>
-            <FilterText>Filter By</FilterText>
-            <DividerLine border="1px solid #e5e5e5" />
-            <ButtonsContainer>
-                <FilterButton filterLabel="Category" />
-                <ButtonDivider />
-                <FilterButton filterLabel="Equipment" />
-                <ButtonDivider />
-                <FilterButton filterLabel="Difficulty" />
-                <ButtonDivider />
-                <FilterButton filterLabel="Workout Days" />
-                <ButtonDivider />
-                <FilterButton filterLabel="Workout Length" />
-            </ButtonsContainer>
-        </MainContainer>
-    );
+    const renderFilterView = (view: RenderView) => {
+        if (view) {
+            switch (view) {
+                case RenderView.RENDER_MAIN_FILTER:
+                    return (
+                        <MainContainer>
+                            <TitleContainer>
+                                <FilterText>Filter By</FilterText>
+                            </TitleContainer>
+                            <DividerLine border="1px solid #e5e5e5" />
+                            <ButtonsContainer>
+                                <FilterButton
+                                    filterLabel="Category"
+                                    onClick={() =>
+                                        setRenderState(
+                                            RenderView.RENDER_SELECTION_CATEGORY
+                                        )
+                                    }
+                                />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Equipment" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Difficulty" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Workout Days" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Workout Length" />
+                            </ButtonsContainer>
+                        </MainContainer>
+                    );
+                case RenderView.RENDER_SELECTION_CATEGORY:
+                    return (
+                        <MainContainer>
+                            <TitleContainer>
+                                <ReturnButton
+                                    onClick={() =>
+                                        setRenderState(
+                                            RenderView.RENDER_MAIN_FILTER
+                                        )
+                                    }
+                                >
+                                    <LeftArrowIcon />
+                                    Filters
+                                </ReturnButton>
+                                <FilterText>Category</FilterText>
+                            </TitleContainer>
+                            <DividerLine border="1px solid #e5e5e5" />
+                            <ButtonsContainer>
+                                <FilterButton filterLabel="Weightlifting" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Powerlifting" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Bodybuilding" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Crossfit" />
+                                <ButtonDivider />
+                                <FilterButton filterLabel="Aerobics" />
+                            </ButtonsContainer>
+                        </MainContainer>
+                    );
+            }
+        }
+    };
+
+    return <>{renderFilterView(renderState)}</>;
 };
 
 export default FilterColumn;
