1ba56530e885cebfdecabfb71c6f4570de401a9b
Haz-git <zhouharry1@gmail.com>
2021-08-21 10:17:57 -0700

    Configured redux provider, persistor, and store

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { NormalizeCSS } from '@mantine/core';
 import App from './components/App';
 
+//Redux:
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../src/redux/store';
+
 //Styles:
 
 ReactDOM.render(
     <React.StrictMode>
-        <NormalizeCSS />
-        <App />
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <NormalizeCSS />
+                <App />
+            </PersistGate>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
