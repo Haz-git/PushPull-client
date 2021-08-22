9bc27477576cfc1898603f63852837c6926c1b3b
Haz-git <zhouharry1@gmail.com>
2021-08-22 16:55:02 -0700

    Attached statusCallback

--- src/redux/workoutPrograms/workoutProgramActions.ts
+++ src/redux/workoutPrograms/workoutProgramActions.ts
@@ -3,21 +3,19 @@ import { Dispatch } from 'redux';
 import { WorkoutProgramAction } from './workoutProgramInterfaces';
 import { WorkoutProgramActionType } from './action-types';
 
-export const getWorkoutPrograms = () =>
-    // statusCallback: (status: boolean) => void
-    {
-        return async (dispatch: Dispatch<WorkoutProgramAction>) => {
-            const response = await api.get('/workoutProgram/all');
+export const getWorkoutPrograms = (
+    statusCallback: (status: boolean) => void
+) => {
+    return async (dispatch: Dispatch<WorkoutProgramAction>) => {
+        const response = await api.get('/workoutProgram/all');
 
-            console.log(response);
+        if (response) {
+            statusCallback(true);
+        }
 
-            // if (response) {
-            //     statusCallback(true);
-            // }
-
-            dispatch({
-                type: WorkoutProgramActionType.USER_GET_WORKOUTPROGRAMS,
-                payload: response.data.workoutPrograms,
-            });
-        };
+        dispatch({
+            type: WorkoutProgramActionType.USER_GET_WORKOUTPROGRAMS,
+            payload: response.data.workoutPrograms,
+        });
     };
+};
