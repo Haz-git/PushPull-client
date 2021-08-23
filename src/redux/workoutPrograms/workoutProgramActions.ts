6823afc18701ccccc9d28cb3e2e0306402dda20a
Haz-git <zhouharry1@gmail.com>
2021-08-23 07:02:38 -0700

    Added dispatch function for updating and filtering workout programs. Also intercepted initial request and filtered based on persisted filters

--- src/redux/workoutPrograms/workoutProgramActions.ts
+++ src/redux/workoutPrograms/workoutProgramActions.ts
@@ -3,19 +3,68 @@ import { Dispatch } from 'redux';
 import { WorkoutProgramAction } from './workoutProgramInterfaces';
 import { WorkoutProgramActionType } from './action-types';
 
+//utils:
+import filterWorkoutPrograms from '../../utils/filterWorkoutPrograms';
+
+//State interface for filter access:
+interface FilterState {
+    filters: {
+        category: string;
+        equipment: string;
+        difficulty: string;
+        workoutSchedule: string;
+        workoutLength: string;
+    };
+}
+
+interface WorkoutProgramState {
+    workoutPrograms: {
+        workoutPrograms: any;
+    };
+}
+
+type CombinedState = FilterState & WorkoutProgramState;
+
 export const getWorkoutPrograms = (
     statusCallback: (status: boolean) => void
 ) => {
-    return async (dispatch: Dispatch<WorkoutProgramAction>) => {
+    return async (
+        dispatch: Dispatch<WorkoutProgramAction>,
+        getState: () => FilterState
+    ) => {
+        const { filters } = getState();
+
         const response = await api.get('/workoutProgram/all');
 
-        if (response) {
+        const currWorkoutPrograms = response.data.workoutPrograms;
+
+        const filteredPrograms = filterWorkoutPrograms(
+            currWorkoutPrograms,
+            filters
+        );
+
+        if (response && filteredPrograms) {
             statusCallback(true);
         }
 
         dispatch({
             type: WorkoutProgramActionType.USER_GET_WORKOUTPROGRAMS,
-            payload: response.data.workoutPrograms,
+            payload: filteredPrograms,
         });
+    };
+};
+
+export const filterAndUpdateWorkoutPrograms = (
+    statusCallback: (status: boolean) => void
+) => {
+    return async (
+        dispatch: Dispatch<WorkoutProgramAction>,
+        getState: () => CombinedState
+    ) => {
+        const { filters } = getState();
+
+        const {
+            workoutPrograms: { workoutPrograms },
+        } = getState();
     };
 };
