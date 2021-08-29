4c9d8ea8efa2a479ab6f9c4a5fc1098865dba984
Haz-git <zhouharry1@gmail.com>
2021-08-29 07:29:33 -0700

    Added two action creators

--- src/redux/sortOptions/sortOptionsActions.ts
+++ src/redux/sortOptions/sortOptionsActions.ts
@@ -1,3 +1,21 @@
 import { Dispatch } from 'redux';
 import { SortOptionAction } from './sortOptionsInterface';
 import { SortOptionActionType } from './action-types';
+
+export const updateWorkoutProgramSortOption = (sortOption: string) => {
+    return async (dispatch: Dispatch<SortOptionAction>) => {
+        dispatch({
+            type: SortOptionActionType.USER_UPDATE_WORKOUTPROGRAM_SORTOPTION,
+            payload: sortOption,
+        });
+    };
+};
+
+export const updateReviewSortOption = (sortOption: string) => {
+    return async (dispatch: Dispatch<SortOptionAction>) => {
+        dispatch({
+            type: SortOptionActionType.USER_UPDATE_REVIEW_SORTOPTION,
+            payload: sortOption,
+        });
+    };
+};
