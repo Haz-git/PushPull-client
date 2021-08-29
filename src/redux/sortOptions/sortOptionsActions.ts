ba9101bb1c66238823990d3b45b38de28e0cddc1
Haz-git <zhouharry1@gmail.com>
2021-08-29 08:07:52 -0700

    Attached another action creator

--- src/redux/sortOptions/sortOptionsActions.ts
+++ src/redux/sortOptions/sortOptionsActions.ts
@@ -2,16 +2,28 @@ import { Dispatch } from 'redux';
 import { SortOptionAction } from './sortOptionsInterface';
 import { SortOptionActionType } from './action-types';
 
-export const updateWorkoutProgramSortOption = (sortOption: string) => {
-    return async (dispatch: Dispatch<SortOptionAction>) => {
+//WorkoutProgramAction Creator:
+import { getWorkoutPrograms } from '../workoutPrograms/workoutProgramActions';
+
+export const updateWorkoutProgramSortOption = (
+    statusCallback: (status: boolean) => void,
+    sortOption: string
+) => {
+    return async (dispatch: Dispatch<any>) => {
         dispatch({
             type: SortOptionActionType.USER_UPDATE_WORKOUTPROGRAM_SORTOPTION,
             payload: sortOption,
         });
+
+        //Here, we call getWorkoutPrograms because our sortOption should be updated.
+        dispatch(getWorkoutPrograms(statusCallback, 1));
     };
 };
 
-export const updateReviewSortOption = (sortOption: string) => {
+export const updateReviewSortOption = (
+    statusCallback: (status: boolean) => void,
+    sortOption: string
+) => {
     return async (dispatch: Dispatch<SortOptionAction>) => {
         dispatch({
             type: SortOptionActionType.USER_UPDATE_REVIEW_SORTOPTION,
