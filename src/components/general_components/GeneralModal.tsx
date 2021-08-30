0abb63d2ad43daffe6e82b4e70086da0cb5190bd
Haz-git <zhouharry1@gmail.com>
2021-08-30 09:08:20 -0700

    Created an all-purpose modal

---
+++ src/components/general_components/GeneralModal.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+//Components:
+import { Modal } from '@mantine/core';
+
+//Styles:
+import styled from 'styled-components';
+
+//Interfaces:
+interface IGeneralModal {
+    openBoolean: boolean;
+    closeFunc: () => void;
+    children: React.ReactNode;
+    size?: string;
+    title?: string;
+}
+
+const GeneralModal = ({
+    openBoolean,
+    closeFunc,
+    children,
+    size = 'md',
+    title = 'Modal',
+}: IGeneralModal): JSX.Element => {
+    return (
+        <>
+            <Modal
+                title={title}
+                styles={{
+                    title: {
+                        color: 'rgba(0, 0, 34, 1)',
+                        fontFamily: 'Lato, sans-serif',
+                        fontSize: '1.6rem',
+                        fontWeight: 700,
+                    },
+                    header: {
+                        marginBottom: '.5rem',
+                    },
+                }}
+                opened={openBoolean}
+                onClose={closeFunc}
+                size={size}
+            >
+                {children}
+            </Modal>
+        </>
+    );
+};
+
+export default GeneralModal;
