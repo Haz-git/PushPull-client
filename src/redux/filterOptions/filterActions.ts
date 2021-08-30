bb57b127fff128782f27c517a66685a0613773f3
Haz-git <zhouharry1@gmail.com>
2021-08-30 07:32:50 -0700

    Added action creator to reset all filters and query DB

--- src/redux/filterOptions/filterActions.ts
+++ src/redux/filterOptions/filterActions.ts
@@ -2,6 +2,9 @@ import { Dispatch } from 'redux';
 import { FilterActionType } from './action-types';
 import { FilterAction } from './filterInterfaces';
 
+//WorkoutProgram action creator:
+import { getWorkoutPrograms } from '../workoutPrograms/workoutProgramActions';
+
 export const updateCategory = (filterChoice: string) => {
     return async (dispatch: Dispatch<FilterAction>) => {
         dispatch({
@@ -44,5 +47,19 @@ export const updateWorkoutLength = (filterChoice: string) => {
             type: FilterActionType.USER_UPDATE_WORKOUTLENGTH,
             payload: filterChoice,
         });
+    };
+};
+
+export const resetAllFilters = (statusCallback: (status: boolean) => void) => {
+    return async (dispatch: Dispatch<any>) => {
+        //Dispatch to reset all filter states:
+        dispatch(updateCategory('any'));
+        dispatch(updateEquipment('any'));
+        dispatch(updateDifficulty('any'));
+        dispatch(updateWorkoutSchedule('any'));
+        dispatch(updateWorkoutLength('any'));
+
+        //Dispatch to query for user again after reset:
+        dispatch(getWorkoutPrograms(statusCallback, 1));
     };
 };
