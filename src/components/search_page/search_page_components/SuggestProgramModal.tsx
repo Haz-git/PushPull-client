d3acc4eba582965f34e9b1c6398d2e7dd40050ed
Haz-git <zhouharry1@gmail.com>
2021-08-30 08:29:03 -0700

    Added suggest program modal

---
+++ src/components/search_page/search_page_components/SuggestProgramModal.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+const SuggestProgramModal = () => {
+    return <div></div>;
+};
+
+export default SuggestProgramModal;
