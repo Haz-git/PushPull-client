9d087eaf5888a7aba427165e5c26b8497963459a
Haz-git <zhouharry1@gmail.com>
2021-08-31 07:37:16 -0700

    Created view page for workout program

---
+++ src/components/workout_program_page/MainWorkoutProgramPage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+//Interfaces:
+
+const MainWorkoutProgramPage = () => {
+    //This component should contain all of the views of the workout program page.
+    return <div>WORKOUT PROGRAM PAGE</div>;
+};
+
+export default MainWorkoutProgramPage;
