7827a1acd7673b91e8d36446875d5678f3ffdc5d
Haz-git <zhouharry1@gmail.com>
2021-09-09 07:30:47 -0700

    Added more templating

--- src/components/workout_program_page/workout_program_page_components/RatingColumn.tsx
+++ src/components/workout_program_page/workout_program_page_components/RatingColumn.tsx
@@ -24,22 +24,22 @@ const FullStar = styled(Star)`
 
 const MainContainer = styled.section`
     width: 25rem;
-    padding: 1rem 1rem;
-    border: 1px solid black;
+    border-right: 1px solid #e5e5e5;
     text-align: left;
 `;
 
 const ReturnButtonContainer = styled.div`
     border: 1px solid red;
+    margin: 1rem 1.5rem;
 `;
 
 const ReturnButton = styled(Link)``;
 
 const GeneralRatingText = styled.h1`
-    padding: 0.5rem 0rem 0.5rem 0rem;
-    font-size: 2rem;
+    font-size: 1.8rem;
     color: ${(props) => props.theme.mainText};
     font-weight: 600;
+    margin: 2rem 1.5rem 0rem 1.5rem;
 `;
 
 const GeneralScoreText = styled.h1`
@@ -55,6 +55,18 @@ const RatingStarsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-start;
+    padding: 0.5rem 1.5rem;
+    border-bottom: 1px solid #e5e5e5;
+`;
+
+const RatingBreakdownContainer = styled.div`
+    padding: 2rem 1.5rem;
+`;
+
+const SubcategoryText = styled.p`
+    font-size: 1.5rem;
+    color: ${(props) => props.theme.mainText};
+    font-weight: 700;
 `;
 
 //Interfaces:
@@ -82,6 +94,9 @@ const RatingColumn = () => {
                 />
                 <GeneralScoreText>1.4</GeneralScoreText>
             </RatingStarsContainer>
+            <RatingBreakdownContainer>
+                <SubcategoryText>Ratings</SubcategoryText>
+            </RatingBreakdownContainer>
         </MainContainer>
     );
 };
