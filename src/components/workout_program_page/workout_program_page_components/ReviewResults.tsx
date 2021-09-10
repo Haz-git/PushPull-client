0dddff5e03dd06f14b7ced8dcc38ab7adf2cb479
Haz-git <zhouharry1@gmail.com>
2021-09-10 07:18:03 -0700

    Added Accordion component for program description

--- src/components/workout_program_page/workout_program_page_components/ReviewResults.tsx
+++ src/components/workout_program_page/workout_program_page_components/ReviewResults.tsx
@@ -1,18 +1,73 @@
 import * as React from 'react';
 
 //Components:
+import { Accordion, AccordionItem } from '@mantine/core';
 
 //Styles:
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
-    padding: 1rem 1rem;
+    padding: 2rem 2rem;
+`;
+
+const ProgramHeaderContainer = styled.div``;
+
+const ProgramTitle = styled.h1`
+    font-size: 2.5rem;
+    color: ${(props) => props.theme.mainText};
+    font-weight: 800;
+`;
+
+const ProgramDescAccContainer = styled.div`
+    margin: 1rem 0;
 `;
 
 //Interfaces:
 
-const ReviewResults = () => {
-    return <MainContainer>This should be review results</MainContainer>;
+interface IComponentProps {
+    programTitle: string;
+    programDesc: string;
+    programReviewCount: number;
+}
+
+const ReviewResults = ({
+    programTitle,
+    programDesc,
+    programReviewCount,
+}: IComponentProps): JSX.Element => {
+    return (
+        <MainContainer>
+            <ProgramHeaderContainer>
+                <ProgramTitle>{programTitle}</ProgramTitle>
+                <ProgramDescAccContainer>
+                    <Accordion
+                        transitionDuration={400}
+                        styles={{
+                            label: {
+                                fontFamily: 'Lato, sans-serif',
+                                fontWeight: 700,
+                                fontSize: '1.2rem',
+                                color: 'rgba(0, 0, 34, .7)',
+                            },
+                            item: {
+                                fontFamily: 'Lato, sans-serif',
+                                fontWeight: 500,
+                                fontSize: '1.1rem',
+                                color: 'rgba(0, 0, 34, .7)',
+                                marginLeft: '-.85rem',
+                                borderTop: '2px solid #e5e5e5',
+                                borderBottom: '2px solid #e5e5e5',
+                            },
+                        }}
+                    >
+                        <AccordionItem label={`What's ${programTitle} about?`}>
+                            {programDesc}
+                        </AccordionItem>
+                    </Accordion>
+                </ProgramDescAccContainer>
+            </ProgramHeaderContainer>
+        </MainContainer>
+    );
 };
 
 export default ReviewResults;
