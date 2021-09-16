592a6c7d3eab8d2143c9830651a6696f515700dc
Haz <zhouharry1@gmail.com>
2021-09-16 09:35:39 -0700

    Added more details

--- src/components/workout_program_page/workout_program_page_components/ReportWorkoutProgramForm.tsx
+++ src/components/workout_program_page/workout_program_page_components/ReportWorkoutProgramForm.tsx
@@ -3,11 +3,24 @@ import * as React from 'react';
 //Components:
 
 //Styles:
+import styled from 'styled-components';
+//some styles were copied over from 'AddNewProgramForm'.
+import {
+    MainFormContainer,
+    FormDesc,
+} from '../../search_page/search_page_components/AddNewProgramForm';
 
 //Interface:
 
 const ReportWorkoutProgramForm = () => {
-    return <div>TERST</div>;
+    return (
+        <MainFormContainer>
+            <FormDesc>
+                Thank you for bringing a problem to our attention. Please let us
+                know the issue.
+            </FormDesc>
+        </MainFormContainer>
+    );
 };
 
 export default ReportWorkoutProgramForm;
