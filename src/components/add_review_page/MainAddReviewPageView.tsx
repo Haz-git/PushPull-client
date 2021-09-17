65e432deabaadc8c88a208f121b4e0e6c7acb5b0
Haz <zhouharry1@gmail.com>
2021-09-17 09:00:23 -0700

    Created page for adding reviews to house master form and children

---
+++ src/components/add_review_page/MainAddReviewPageView.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const MainAddReviewPageView = () => {
+    return <div>Add Review Page</div>;
+};
+
+export default MainAddReviewPageView;
