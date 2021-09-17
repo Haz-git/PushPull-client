d8c2a036e215bc806bef1b022706022de41e661d
Haz <zhouharry1@gmail.com>
2021-09-17 09:18:34 -0700

    Added main container for review

--- src/components/add_review_page/MainAddReviewPageView.tsx
+++ src/components/add_review_page/MainAddReviewPageView.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 
 //Components:
+import { deviceMin } from '../../devices/breakpoints';
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    @media ${deviceMin.mobileS} {
+        height: 100%;
+        overflow: hidden;
+    }
+
+    @media ${deviceMin.browserSm} {
+        height: 100vh;
+    }
+`;
 
 //Interfaces:
 
 const MainAddReviewPageView = () => {
-    return <div>Add Review Page</div>;
+    return <MainContainer>Add Review Page</MainContainer>;
 };
 
 export default MainAddReviewPageView;
