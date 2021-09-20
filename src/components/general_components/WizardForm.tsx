e40a57ce06de80ce58624eb154cbe50e699d8195
Haz <zhouharry1@gmail.com>
2021-09-20 13:20:37 -0700

    Added general wizard form

---
+++ src/components/general_components/WizardForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const WizardForm = () => {
+    return <div>General Wizard Form component</div>;
+};
+
+export default WizardForm;
