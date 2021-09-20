400e2634e6a15712a2a8491b98e2d1fe0692e9f7
Haz <zhouharry1@gmail.com>
2021-09-20 13:41:33 -0700

    Added toy back and next button

--- src/components/general_components/WizardForm.tsx
+++ src/components/general_components/WizardForm.tsx
@@ -1,24 +1,47 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 //Components:
+import GeneralButton from '../general_components/GeneralButton';
 
 //Styles:
 
 //Interfaces:
 
 interface IComponentProps {
-    startIndex: number;
-    maxIndex: number;
     children: React.ReactNode;
 }
 
-const WizardForm = ({
-    startIndex,
-    maxIndex,
-    children,
-}: IComponentProps): JSX.Element => {
-    console.log(children);
-    return <div>{children}</div>;
+const WizardForm = ({ children }: IComponentProps): JSX.Element => {
+    const [viewIndex, setViewIndex] = useState(0);
+
+    //Handle Prev or Next Requests:
+    const handleNext = () => {
+        if ((children as any)[viewIndex + 1] !== undefined) {
+            setViewIndex(viewIndex + 1);
+        }
+    };
+
+    const handlePrev = () => {
+        if (viewIndex > 0) {
+            setViewIndex(viewIndex - 1);
+        }
+    };
+
+    //Render different wizard form views:
+    const renderWizardFormViews = () => {
+        if (children) {
+            return <>{(children as any)[viewIndex]}</>;
+        }
+    };
+
+    return (
+        <div>
+            {renderWizardFormViews()}
+            <GeneralButton buttonLabel="Back" onClick={handlePrev} />
+            <GeneralButton buttonLabel="Next" onClick={handleNext} />
+        </div>
+    );
 };
 
 export default WizardForm;
