d1509410e40a065cf7985aa5985d076339c4d3a0
Haz <zhouharry1@gmail.com>
2021-09-21 06:50:57 -0700

    Added crude node picture

--- src/components/general_components/WizardStepNavigationColumn.tsx
+++ src/components/general_components/WizardStepNavigationColumn.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 //Components:
+import { v4 as uuid } from 'uuid';
 
 //Styles:
 import styled from 'styled-components';
@@ -13,9 +14,37 @@ const MainContainer = styled.div`
     padding: 2rem 2rem;
 `;
 
-const NodeContainer = styled.div``;
+const NodeContainer = styled.div`
+    margin: 2rem 0;
+`;
 
-const Nodes = styled.div``;
+const NodeItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 1rem 0;
+`;
+
+const NodeCircle = styled.div`
+    font-size: 1.2rem;
+    color: white;
+    font-weight: 800;
+    text-shadow: ${(props) => props.theme.textShadows.sm};
+    vertical-align: middle;
+    line-height: 2rem;
+    text-align: center;
+    background: ${(props) => props.theme.accentColors.orange};
+    border-radius: 2rem;
+    height: 2rem;
+    width: 2rem;
+    margin-right: 1rem;
+`;
+
+const NodeLabel = styled.p`
+    font-size: 1.2rem;
+    color: ${(props) => props.theme.mainText};
+    font-weight: 800;
+`;
 
 //Interfaces:
 
@@ -30,10 +59,21 @@ const WizardStepNavigationColumn = ({
     currentStep,
     steps,
 }: IComponentProps): JSX.Element => {
+    const renderNodeItem = () => {
+        if (steps && currentStep) {
+            return steps.map((stepItem) => (
+                <NodeItem key={uuid()}>
+                    <NodeCircle>{stepItem.stepNum}</NodeCircle>
+                    <NodeLabel>{stepItem.stepTitle}</NodeLabel>
+                </NodeItem>
+            ));
+        }
+    };
+
     return (
         <MainContainer>
             Step navigation Crumbs {numSteps} STEPS
-            <NodeContainer></NodeContainer>
+            <NodeContainer>{renderNodeItem()}</NodeContainer>
         </MainContainer>
     );
 };
