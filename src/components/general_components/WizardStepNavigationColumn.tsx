05b17c32c08d93e5b3b9e596d9907987c9b1d198
Haz <zhouharry1@gmail.com>
2021-09-21 06:15:30 -0700

    Added css

--- src/components/general_components/WizardStepNavigationColumn.tsx
+++ src/components/general_components/WizardStepNavigationColumn.tsx
@@ -3,11 +3,19 @@ import * as React from 'react';
 //Components:
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    width: 25rem;
+    border-right: 1px solid #e5e5e5;
+    text-align: left;
+    height: 100%;
+`;
 
 //Interfaces:
 
 const WizardStepNavigationColumn = () => {
-    return <div>Step navigation Crumbs</div>;
+    return <MainContainer>Step navigation Crumbs</MainContainer>;
 };
 
 export default WizardStepNavigationColumn;
