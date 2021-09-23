071ef7b4f99938da99839ed019dbdde792b2536d
Haz <zhouharry1@gmail.com>
2021-09-23 06:29:23 -0700

    Added initial template form for star ratings

--- src/components/add_review_page/add_review_page_components/StarRatingsForm.tsx
+++ src/components/add_review_page/add_review_page_components/StarRatingsForm.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
 
 //Components:
+import Rating from 'react-rating';
 
 //Styles:
 import styled from 'styled-components';
+
+//Icons:
+import { Star } from '@styled-icons/evaicons-solid/Star';
+
+const EmptyStar = styled(Star)`
+    height: 3.5rem;
+    width: 3.5rem;
+    color: #c2c2c2;
+`;
+
+const FullStar = styled(Star)`
+    height: 3.5rem;
+    width: 3.5rem;
+    color: rgba(224, 113, 51, 1);
+`;
 
 export const MainContainer = styled.section`
     padding: 3.5rem 2rem;
@@ -22,7 +38,7 @@ export const FormContainer = styled.div`
 `;
 
 export const FormInputContainer = styled.div`
-    margin: 1rem 0;
+    margin: 4rem 0;
 `;
 
 export const FormSubtitle = styled.h2`
@@ -41,6 +57,10 @@ export const FormDesc = styled.h3`
     font-weight: 700;
 `;
 
+export const StarContainer = styled.div`
+    margin: 1rem 0;
+`;
+
 /* 
     Since this StarRatingsForm will be the first form of the series for adding a review, this form will be the standardization for all forms under adding a review. In essence, we'll be exporting styles from this form frequently.
 
@@ -55,12 +75,72 @@ const StarRatingsForm = () => {
             <FormContainer>
                 <FormInputContainer>
                     <FormSubtitle>
-                        How would you rate the efficiency of this workout
-                        program?
+                        How would you rate this program's reliability?
                     </FormSubtitle>
                     <FormDescContainer>
-                        <FormDesc>blahhhh</FormDesc>
+                        <FormDesc>
+                            In other words, how repeatable is this program? Are
+                            you able to perform this program back-to-back
+                            without any repercussions?
+                        </FormDesc>
                     </FormDescContainer>
+                    <StarContainer>
+                        <Rating
+                            start={0}
+                            stop={5}
+                            fractions={2}
+                            readonly={false}
+                            initialRating={0}
+                            emptySymbol={<EmptyStar />}
+                            fullSymbol={<FullStar />}
+                        />
+                    </StarContainer>
+                </FormInputContainer>
+                <FormInputContainer>
+                    <FormSubtitle>
+                        How would you rate this program's difficulty level?
+                    </FormSubtitle>
+                    <FormDescContainer>
+                        <FormDesc>
+                            Is this program accurate in terms of its difficulty
+                            recommendation? Were you able to follow the
+                            exercises and weight progression?
+                        </FormDesc>
+                    </FormDescContainer>
+                    <StarContainer>
+                        <Rating
+                            start={0}
+                            stop={5}
+                            fractions={2}
+                            readonly={false}
+                            initialRating={0}
+                            emptySymbol={<EmptyStar />}
+                            fullSymbol={<FullStar />}
+                        />
+                    </StarContainer>
+                </FormInputContainer>
+                <FormInputContainer>
+                    <FormSubtitle>
+                        How would you rate this program's effectiveness?
+                    </FormSubtitle>
+                    <FormDescContainer>
+                        <FormDesc>
+                            Did this program offer improvement in the proclaimed
+                            areas? How well did you achieve your strength or
+                            weight goals under this program?
+                        </FormDesc>
+                    </FormDescContainer>
+                    <StarContainer>
+                        <Rating
+                            start={0}
+                            stop={5}
+                            fractions={2}
+                            readonly={false}
+                            initialRating={0}
+                            emptySymbol={<EmptyStar />}
+                            fullSymbol={<FullStar />}
+                        />
+                    </StarContainer>
                 </FormInputContainer>
             </FormContainer>
         </MainContainer>
