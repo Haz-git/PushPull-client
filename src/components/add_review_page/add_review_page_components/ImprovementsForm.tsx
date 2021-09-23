76e6ea387aba1e85931a0ac5547a60cb89da171b
Haz <zhouharry1@gmail.com>
2021-09-23 10:34:44 -0700

    Added template for adding improvements

--- src/components/add_review_page/add_review_page_components/ImprovementsForm.tsx
+++ src/components/add_review_page/add_review_page_components/ImprovementsForm.tsx
@@ -3,11 +3,39 @@ import * as React from 'react';
 //Components:
 
 //Styles:
+import styled from 'styled-components';
+import {
+    MainContainer,
+    FormTitleContainer,
+    FormTitle,
+    FormContainer,
+    FormInputContainer,
+    FormSubtitle,
+    FormDescContainer,
+    FormDesc,
+} from './StarRatingsForm';
 
 //Interfaces:
 
 const ImprovementsForm = () => {
-    return <div>Improvements Form.</div>;
+    return (
+        <MainContainer>
+            <FormTitleContainer>
+                <FormTitle>Improvements</FormTitle>
+            </FormTitleContainer>
+            <FormContainer>
+                <FormSubtitle>
+                    What exercises did this program help you develop?
+                </FormSubtitle>
+                <FormDescContainer>
+                    <FormDesc>
+                        Let people know how much you've improved! Our favorites
+                        are Bench Press, Barbell Squat, and Deadlift.
+                    </FormDesc>
+                </FormDescContainer>
+            </FormContainer>
+        </MainContainer>
+    );
 };
 
 export default ImprovementsForm;
