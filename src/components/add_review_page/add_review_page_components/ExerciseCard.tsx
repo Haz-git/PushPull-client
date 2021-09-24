8d46b7dcccc536a4b6265faf2c03dcb50713519e
Haz-git <zhouharry1@gmail.com>
2021-09-24 14:03:16 -0700

    Added styling

--- src/components/add_review_page/add_review_page_components/ExerciseCard.tsx
+++ src/components/add_review_page/add_review_page_components/ExerciseCard.tsx
@@ -4,9 +4,30 @@ import * as React from 'react';
 
 //Styles:
 import styled from 'styled-components';
+import { CaretUp } from '@styled-icons/fluentui-system-filled/CaretUp';
+import { CaretDown } from '@styled-icons/fluentui-system-filled/CaretDown';
+import { ArrowRightAlt } from '@styled-icons/material-rounded/ArrowRightAlt';
+
+//Icons:
+const CaretUpIcon = styled(CaretUp)`
+    height: 1.7rem;
+    width: 1.7rem;
+    color: green;
+`;
+
+const CaretDownIcon = styled(CaretDown)`
+    height: 1.7rem;
+    width: 1.7rem;
+    color: red;
+`;
+
+const ArrowRightIcon = styled(ArrowRightAlt)`
+    height: 1.7rem;
+    width: 1.7rem;
+    color: ${(props) => props.theme.subText};
+`;
 
 const MainContainer = styled.div`
-    padding: 2rem 2rem;
     border-radius: 0.3rem;
     border: none;
     background: #ffffff;
@@ -14,6 +35,34 @@ const MainContainer = styled.div`
         rgba(14, 30, 37, 0.32) 0px 1px 3px 0px;
     margin-bottom: 1rem;
 `;
+
+const ExerciseCardTitle = styled.h2`
+    font-size: 1rem;
+    color: ${(props) => props.theme.mainText};
+    font-weight: 700;
+`;
+
+const ExerciseText = styled.p`
+    font-size: 1rem;
+    color: ${(props) => props.theme.subText};
+    font-weight: 700;
+`;
+
+const TitleContainer = styled.div`
+    padding: 0.5rem 0.5rem;
+    border-bottom: 1px solid #ececec;
+`;
+
+const InfoContainer = styled.div``;
+
+const WeightContainer = styled.div`
+    padding: 0.5rem 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const IconContainer = styled.div``;
 
 //Interfaces:
 
@@ -30,7 +79,26 @@ const ExerciseCard = ({
     finalWeight,
     weightUnit,
 }: IComponentProps): JSX.Element => {
-    return <MainContainer>{exerciseTitle}</MainContainer>;
+    const renderCaretIcon = () => {
+        if (finalWeight > initialWeight) return <CaretUpIcon />;
+        return <CaretDownIcon />;
+    };
+
+    return (
+        <MainContainer>
+            <InfoContainer>
+                <TitleContainer>
+                    <ExerciseCardTitle>{exerciseTitle}</ExerciseCardTitle>
+                </TitleContainer>
+                <WeightContainer>
+                    <ExerciseText>{`${initialWeight} ${weightUnit}`}</ExerciseText>
+                    <ArrowRightIcon />
+                    <ExerciseText>{`${finalWeight} ${weightUnit}`}</ExerciseText>
+                    <IconContainer>{renderCaretIcon()}</IconContainer>
+                </WeightContainer>
+            </InfoContainer>
+        </MainContainer>
+    );
 };
 
 export default ExerciseCard;
