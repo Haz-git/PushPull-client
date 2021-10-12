c5dc77ea8de62626c34cf51739bfd1d5c83114e4
Haz <zhouharry1@gmail.com>
2021-10-12 05:41:57 -0700

    Added templating for foundation

--- src/components/add_review_page/add_review_page_components/MoreDetailsForm.tsx
+++ src/components/add_review_page/add_review_page_components/MoreDetailsForm.tsx
@@ -1,13 +1,44 @@
 import * as React from 'react';
 
 //Components:
+import GeneralButton from '../../general_components/GeneralButton';
 
 //Styles:
+import styled from 'styled-components';
+import {
+    MainContainer,
+    FormTitleContainer,
+    FormTitle,
+    FormContainer,
+    FormInputContainer,
+    FormSubtitle,
+    FormDescContainer,
+    FormDesc,
+} from './StarRatingsForm';
 
 //Interfaces:
 
 const MoreDetailsForm = () => {
-    return <div>more Details</div>;
+    return (
+        <MainContainer>
+            <FormTitleContainer>
+                <FormTitle>More Details</FormTitle>
+            </FormTitleContainer>
+            <FormContainer>
+                <FormInputContainer>
+                    <FormSubtitle>
+                        How was this workout program in general?
+                    </FormSubtitle>
+                    <FormDescContainer>
+                        <FormDesc>
+                            Now's the time to let people know of the specific
+                            details!
+                        </FormDesc>
+                    </FormDescContainer>
+                </FormInputContainer>
+            </FormContainer>
+        </MainContainer>
+    );
 };
 
 export default MoreDetailsForm;
