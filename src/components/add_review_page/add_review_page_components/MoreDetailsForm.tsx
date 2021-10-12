7c10406f51b3af82a7ad5d816f981cdf1f944e5c
Haz <zhouharry1@gmail.com>
2021-10-12 05:29:42 -0700

    Created more details form

---
+++ src/components/add_review_page/add_review_page_components/MoreDetailsForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const MoreDetailsForm = () => {
+    return <div>more Details</div>;
+};
+
+export default MoreDetailsForm;
