e12154082a466e4d37024eb2688137b1eaf4cfb5
Haz <zhouharry1@gmail.com>
2021-10-15 05:50:41 -0700

    Minimizing wizard form

--- src/components/general_components/WizardStepNavigationColumn.tsx
+++ src/components/general_components/WizardStepNavigationColumn.tsx
@@ -2,73 +2,151 @@ import * as React from 'react';
 import { useState } from 'react';
 
 //Components:
+import { deviceMin } from '../../devices/breakpoints';
 import { v4 as uuid } from 'uuid';
 
 //Styles:
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
-    width: 25rem;
-    border-right: 1px solid #e5e5e5;
-    text-align: left;
-    height: 100%;
-    padding: 4rem 2rem;
+    @media ${deviceMin.mobileS} {
+        width: 100%;
+        text-align: left;
+        padding: 0rem 2rem;
+    }
+
+    @media ${deviceMin.browserSm} {
+        width: 25rem;
+        border-right: 1px solid #e5e5e5;
+        text-align: left;
+        height: 100%;
+        padding: 4rem 2rem;
+    }
 `;
 
 const TitleHeader = styled.h2`
-    font-size: 1.8rem;
-    color: ${(props) => props.theme.mainText};
-    font-weight: 800;
-    margin-bottom: 3rem;
+    @media ${deviceMin.mobileS} {
+        display: none;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: block;
+        font-size: 1.8rem;
+        color: ${(props) => props.theme.mainText};
+        font-weight: 800;
+        margin-bottom: 3rem;
+    }
 `;
 
 const NodeContainer = styled.div`
     margin: 2rem 0rem 0rem 0rem;
+
+    @media ${deviceMin.mobileS} {
+        display: flex;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: block;
+    }
 `;
 
 const NodeItem = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    margin: 2rem 0;
-    border: none;
-    background: none;
-    border-radius: 0.3rem;
-    cursor: pointer;
+    @media ${deviceMin.mobileS} {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 0rem 1.5rem 0rem 0rem;
+        border: none;
+        background: none;
+        border-radius: 0.3rem;
+        cursor: pointer;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 2rem 0rem 0rem 0rem;
+        border: none;
+        background: none;
+        border-radius: 0.3rem;
+        cursor: pointer;
+    }
 `;
 
 const NodeCircle = styled.div<NodeCircleProps>`
-    position: relative;
-    font-size: 1.2rem;
-    color: ${(props) =>
-        props.isActive === true ? '#ffffff' : props.theme.subText};
-    font-weight: 800;
-    text-shadow: ${(props) =>
-        props.isActive === true ? props.theme.textShadows.sm : 'none'};
-    vertical-align: middle;
-    line-height: 3rem;
-    text-align: center;
-    background: ${(props) =>
-        props.isActive === true ? props.theme.accentColors.orange : '#e5e5e5'};
-    border-radius: 1.5rem;
-    height: 3rem;
-    width: 3rem;
-    margin-right: 1rem;
-
-    &::before {
-        visibility: ${(props) =>
-            props.isLastNode === true ? 'hidden' : 'visible'};
-        content: '';
-        width: 3px;
-        height: 100%;
+    @media ${deviceMin.mobileS} {
+        position: relative;
+        font-size: 1rem;
+        color: ${(props) =>
+            props.isActive === true ? '#ffffff' : props.theme.subText};
+        font-weight: 800;
+        text-shadow: ${(props) =>
+            props.isActive === true ? props.theme.textShadows.sm : 'none'};
+        vertical-align: middle;
+        line-height: 2rem;
+        text-align: center;
         background: ${(props) =>
             props.isActive === true
                 ? props.theme.accentColors.orange
                 : '#e5e5e5'};
-        display: block;
-        position: absolute;
-        left: 1.45rem;
-        top: 100%;
+        border-radius: 2rem;
+        height: 2rem;
+        width: 2rem;
+        margin-right: 1rem;
+
+        &::before {
+            visibility: ${(props) =>
+                props.isLastNode === true ? 'hidden' : 'visible'};
+            content: '';
+            width: 2.5rem;
+            height: 2px;
+            background: ${(props) =>
+                props.isActive === true
+                    ? props.theme.accentColors.orange
+                    : '#e5e5e5'};
+            display: block;
+            position: absolute;
+            right: -2.5rem;
+            top: 45%;
+        }
+    }
+
+    @media ${deviceMin.browserSm} {
+        position: relative;
+        font-size: 1.2rem;
+        color: ${(props) =>
+            props.isActive === true ? '#ffffff' : props.theme.subText};
+        font-weight: 800;
+        text-shadow: ${(props) =>
+            props.isActive === true ? props.theme.textShadows.sm : 'none'};
+        vertical-align: middle;
+        line-height: 3rem;
+        text-align: center;
+        background: ${(props) =>
+            props.isActive === true
+                ? props.theme.accentColors.orange
+                : '#e5e5e5'};
+        border-radius: 1.5rem;
+        height: 3rem;
+        width: 3rem;
+        margin-right: 1rem;
+
+        &::before {
+            visibility: ${(props) =>
+                props.isLastNode === true ? 'hidden' : 'visible'};
+            content: '';
+            width: 3px;
+            height: 100%;
+            background: ${(props) =>
+                props.isActive === true
+                    ? props.theme.accentColors.orange
+                    : '#e5e5e5'};
+            display: block;
+            position: absolute;
+            left: 1.45rem;
+            top: 100%;
+        }
     }
 `;
 
@@ -77,6 +155,14 @@ const NodeLabel = styled.p<NodeStyledProps>`
     color: ${(props) =>
         props.isActive === true ? props.theme.mainText : props.theme.subText};
     font-weight: 700;
+
+    @media ${deviceMin.mobileS} {
+        display: none;
+    }
+
+    @media ${deviceMin.browserSm} {
+        display: block;
+    }
 `;
 
 //Interfaces:
