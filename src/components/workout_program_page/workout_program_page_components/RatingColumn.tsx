0f61cbcc60f5d89df16bf951057a0191aefa442f
Haz <zhouharry1@gmail.com>
2021-10-21 07:04:51 -0700

    Added new chart

--- src/components/workout_program_page/workout_program_page_components/RatingColumn.tsx
+++ src/components/workout_program_page/workout_program_page_components/RatingColumn.tsx
@@ -260,15 +260,15 @@ const RatingColumn = ({
     const recommendedLevels = [
         {
             name: 'Beginner',
-            Reviewers: recBegCount,
+            'Reviewers Suggested': recBegCount,
         },
         {
             name: 'Intermediate',
-            Reviewers: recIntCount,
+            'Reviewers Suggested': recIntCount,
         },
         {
             name: 'Advanced',
-            Reviewers: recAdvCount,
+            'Reviewers Suggested': recAdvCount,
         },
     ];
 
@@ -461,7 +461,7 @@ const RatingColumn = ({
                             />
                             <CartesianGrid />
                             <Bar
-                                dataKey="Experience Level"
+                                dataKey="Reviewers Suggested"
                                 fill="rgba(224, 113, 51, 1)"
                             />
                         </BarChart>
