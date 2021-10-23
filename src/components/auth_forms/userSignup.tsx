e974ad169043fdc77a8e1961329ca55498b66876
Haz <zhouharry1@gmail.com>
2021-10-23 07:18:36 -0700

    Initialized userfront

--- src/components/auth_forms/userSignup.tsx
+++ src/components/auth_forms/userSignup.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 //Components:
 
 //Styles:
 
 //Interfaces:
+
+//UserFront IDP API initialization;
+const windowObject = window as any;
+const Userfront = windowObject.Userfront;
+
+//Tenant ID could possibly be stored in an ENV var.
+Userfront.init('5nxxrqn7');
 
 const userSignup = () => {
     return <div>Sign up page</div>;
