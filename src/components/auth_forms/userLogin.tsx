a236ecbc06be3f75d6a9a807672c4abfd3d89aab
Haz <zhouharry1@gmail.com>
2021-10-23 07:06:11 -0700

    Added folder for authentication, added signup form and login form

---
+++ src/components/auth_forms/userLogin.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const userLogin = () => {
+    return <div>Login Page</div>;
+};
+
+export default userLogin;
---
+++ src/components/auth_forms/userSignup.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const userSignup = () => {
+    return <div>Sign up page</div>;
+};
+
+export default userSignup;
