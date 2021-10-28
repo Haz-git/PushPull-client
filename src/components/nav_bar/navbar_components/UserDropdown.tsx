74413da88f5869e4df1abcd049066522b79425dd
Haz-git <zhouharry1@gmail.com>
2021-10-28 15:34:03 -0700

    Added rudimentary design for user placard

--- src/components/nav_bar/navbar_components/UserDropdown.tsx
+++ src/components/nav_bar/navbar_components/UserDropdown.tsx
@@ -8,6 +8,7 @@ import { userSignout } from '../../../redux/auth/authActions';
 
 //Components:
 import GeneralButton from '../../../components/general_components/GeneralButton';
+import { Avatar } from '@mantine/core';
 import { Menu, MenuItem, MenuLabel, Divider, Text } from '@mantine/core';
 
 //Styles:
@@ -19,9 +20,23 @@ const MainContainer = styled.div`
 `;
 
 const DropdownContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     border: 1px solid #d3d3d3;
     border-radius: 0.3rem;
-    padding: 1rem 1rem;
+    padding: 0.5rem 1rem;
+    column-gap: 1rem;
+`;
+
+const AvatarContainer = styled.div``;
+
+const HeaderContainer = styled.div``;
+
+const UserDetailText = styled.h3`
+    font-size: 1rem;
+    color: ${(props) => props.theme.mainText};
+    font-weight: 800;
 `;
 
 //Interfaces:
@@ -42,7 +57,21 @@ const UserDropdown = ({
     return (
         <MainContainer>
             <Menu
-                control={<DropdownContainer>test</DropdownContainer>}
+                control={
+                    <DropdownContainer>
+                        <AvatarContainer>
+                            <Avatar
+                                src={image}
+                                size="sm"
+                                alt="User image"
+                                radius="sm"
+                            />
+                        </AvatarContainer>
+                        <HeaderContainer>
+                            <UserDetailText>{username}</UserDetailText>
+                        </HeaderContainer>
+                    </DropdownContainer>
+                }
                 zIndex={999}
             >
                 <MenuItem>Log Out</MenuItem>
