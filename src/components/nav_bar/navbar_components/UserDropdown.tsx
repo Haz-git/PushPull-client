5940c5911ed91140cffbfdc0ce537a323a79b946
Haz-git <zhouharry1@gmail.com>
2021-10-28 15:25:42 -0700

    Added dropdown logic

--- src/components/nav_bar/navbar_components/UserDropdown.tsx
+++ src/components/nav_bar/navbar_components/UserDropdown.tsx
@@ -1,15 +1,53 @@
 import * as React from 'react';
+import { useState } from 'react';
+import Userfront from '@userfront/react';
+
+//Redux:
+import { useDispatch } from 'react-redux';
+import { userSignout } from '../../../redux/auth/authActions';
 
 //Components:
+import GeneralButton from '../../../components/general_components/GeneralButton';
+import { Menu, MenuItem, MenuLabel, Divider, Text } from '@mantine/core';
 
 //Styles:
 
 import styled from 'styled-components';
 
+const MainContainer = styled.div`
+    z-index: 9999;
+`;
+
+const DropdownContainer = styled.div`
+    border: 1px solid black;
+    padding: 0;
+`;
+
 //Interfaces:
 
-const UserDropdown = () => {
-    return <>Dropdown Menu</>;
+interface IComponentProps {
+    email: string;
+    image: string;
+    username: string;
+}
+
+const UserDropdown = ({
+    email,
+    image,
+    username,
+}: IComponentProps): JSX.Element => {
+    const [stateCollapse, setStateCollapse] = useState(false);
+
+    return (
+        <MainContainer>
+            <Menu
+                control={<DropdownContainer>test</DropdownContainer>}
+                zIndex={999}
+            >
+                <MenuItem>Log Out</MenuItem>
+            </Menu>
+        </MainContainer>
+    );
 };
 
 export default UserDropdown;
