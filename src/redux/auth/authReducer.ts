f48777c19ba5fa5d493dbc7393ab696b1e665f42
Haz <zhouharry1@gmail.com>
2021-10-28 06:15:38 -0700

    changed name of authReducer

--- src/redux/auth/authReducer.ts
+++ src/redux/auth/authReducer.ts
@@ -6,9 +6,9 @@ const initialState = {};
 const authReducer = (state: Object = initialState, action: AuthAction) => {
     switch (action.type) {
         case AuthActionType.USER_LOG_IN:
-            return { ...state, userDetails: action.payload };
+            return { ...state, user: action.payload };
         case AuthActionType.USER_SIGN_OUT:
-            return { ...state, userDetails: initialState };
+            return { ...state, user: initialState };
         default:
             return state;
     }
