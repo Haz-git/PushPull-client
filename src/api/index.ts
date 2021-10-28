e8f74a2045e185740c058026e5ed9f0af6a3b03a
Haz <zhouharry1@gmail.com>
2021-10-28 07:33:27 -0700

    Attached bearer token to request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
+import Userfront from '@userfront/react';
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://rankmyworkout.herokuapp.com/api',
+    baseURL: 'http://localhost:8080/api',
+    // baseURL: 'https://rankmyworkout.herokuapp.com/api',
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Userfront.accessToken()}`,
+    },
 });
 
 export default axiosInstance;
