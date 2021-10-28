d25cc1ed37e48b7e65a77c0387edb80aac32e5b2
Haz <zhouharry1@gmail.com>
2021-10-28 06:03:18 -0700

    Configured auth reduer

---
+++ src/redux/auth/authReducer.ts
@@ -0,0 +1,17 @@
+import { AuthActionType } from './action-types';
+import { AuthAction } from './authInterfaces';
+
+const initialState = {};
+
+const authReducer = (state: Object = initialState, action: AuthAction) => {
+    switch (action.type) {
+        case AuthActionType.USER_LOG_IN:
+            return { ...state, userDetails: action.payload };
+        case AuthActionType.USER_SIGN_OUT:
+            return { ...state, userDetails: action.payload };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
