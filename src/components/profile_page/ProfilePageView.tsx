3eb17148ca5d31de24c39e43ac5cf5b3083290e5
Haz <zhouharry1@gmail.com>
2021-11-05 06:10:53 -0700

    Added some more details

--- src/components/profile_page/ProfilePageView.tsx
+++ src/components/profile_page/ProfilePageView.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+//Redux:
+import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
 
 //Components:
+import useLoginStatus from '../../utils/hooks/useLoginStatus';
 
 //Styles:
+import styled from 'styled-components';
 
 //Interfaces:
 
-const ProfilePageView = () => {
-    return <div>profile</div>;
+interface IComponentProps {
+    match: {
+        params: {
+            id: any;
+        };
+    };
+}
+
+const ProfilePageView = ({
+    match: {
+        params: { id },
+    },
+}: IComponentProps): JSX.Element => {
+    return <div>profile of {id}</div>;
 };
 
 export default ProfilePageView;
