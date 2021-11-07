f5e41f08fb75791b667887a2ba789097ce8e353e
Haz <zhouharry1@gmail.com>
2021-11-07 06:35:32 -0800

    Set up redux connections

--- src/components/profile_page/profile_page_components/ProfilePanel.tsx
+++ src/components/profile_page/profile_page_components/ProfilePanel.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+//Redux:
+import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
+
 //Components:
 
 //Styles:
@@ -7,6 +10,9 @@ import * as React from 'react';
 //Interfaces:
 
 const ProfilePanel = () => {
+    const dispatch = useDispatch();
+    const queriedUser = useSelector((state: RootStateOrAny) => state?.profile);
+
     return <div>Profile Panel</div>;
 };
 
