3f9fe7da9506c683d77d1bb0d2aeb346147cbc2a
Haz <zhouharry1@gmail.com>
2021-11-08 07:23:07 -0800

    Created template update form with toggle

--- src/components/profile_page/profile_page_components/ProfilePanelUpdateForm.tsx
+++ src/components/profile_page/profile_page_components/ProfilePanelUpdateForm.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
 
 //Components:
+import GeneralButton from '../../general_components/GeneralButton';
 
 //Styles:
 
 //Interfaces:
 
-const ProfilePanelUpdateForm = () => {
-    return <div>This is the update panel</div>;
+interface IComponentProps {
+    toggleUserUpdateForm: () => void;
+}
+
+const ProfilePanelUpdateForm = ({
+    toggleUserUpdateForm,
+}: IComponentProps): JSX.Element => {
+    return (
+        <div>
+            This is the update panel
+            <div>
+                <GeneralButton
+                    buttonLabel="cancel"
+                    onClick={() => toggleUserUpdateForm()}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default ProfilePanelUpdateForm;
