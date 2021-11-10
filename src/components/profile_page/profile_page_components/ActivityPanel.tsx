d442a3e7b2c3d6a37745bc420ea6d5d542332676
Haz-git <zhouharry1@gmail.com>
2021-11-10 10:25:55 -0800

    Created basic template for activity panel

--- src/components/profile_page/profile_page_components/ActivityPanel.tsx
+++ src/components/profile_page/profile_page_components/ActivityPanel.tsx
@@ -6,11 +6,102 @@ import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
 //Components:
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.section`
+    padding: 2rem 1rem;
+    display: flex;
+    flex-direction: column;
+`;
+
+const PanelHeaderContainer = styled.div`
+    margin-bottom: 2rem;
+`;
+
+const PanelHeader = styled.h2`
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: ${(props) => props.theme.mainText};
+    width: 100%;
+`;
+
+const PanelBlock = styled.div`
+    margin-bottom: 2rem;
+`;
+
+const SubPanelHeader = styled.h3`
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: ${(props) => props.theme.subText};
+    width: 100%;
+    margin-bottom: 0.5rem;
+`;
+
+const WorkoutProgramContainer = styled.div`
+    border: 1px solid #d6d6d6;
+    border-radius: 0.3rem;
+    padding: 2rem 2rem;
+`;
+
+const PostedReviewContainer = styled.div`
+    border: 1px solid #d6d6d6;
+    border-radius: 0.3rem;
+    padding: 2rem 2rem;
+`;
+
+const DefaultText = styled.p`
+    font-size: 1rem;
+    font-weight: 500;
+    color: ${(props) => props.theme.subText};
+    width: 100%;
+`;
 
 //Interfaces:
 
 const ActivityPanel = () => {
-    return <div>ActivityPanel</div>;
+    const queriedUser = useSelector((state: RootStateOrAny) => state?.profile);
+
+    const renderWorkoutPrograms = () => {
+        const { workoutPrograms } = queriedUser?.data || {};
+
+        if (!workoutPrograms)
+            return (
+                <DefaultText>{`${queriedUser.username} has no activity here yet.`}</DefaultText>
+            );
+
+        return <DefaultText>Activity here</DefaultText>;
+    };
+
+    const renderPostedReviews = () => {
+        const { postedReviews } = queriedUser?.data || {};
+
+        if (!postedReviews)
+            return (
+                <DefaultText>{`${queriedUser.username} has no activity here yet.`}</DefaultText>
+            );
+
+        return <DefaultText>Activity here</DefaultText>;
+    };
+
+    return (
+        <MainContainer>
+            <PanelHeaderContainer>
+                <PanelHeader>Activity Hub</PanelHeader>
+            </PanelHeaderContainer>
+            <PanelBlock>
+                <SubPanelHeader>Custom Workout Programs</SubPanelHeader>
+                <WorkoutProgramContainer>
+                    {renderWorkoutPrograms()}
+                </WorkoutProgramContainer>
+            </PanelBlock>
+            <PanelBlock>
+                <SubPanelHeader>Posted Reviews</SubPanelHeader>
+                <PostedReviewContainer>
+                    {renderPostedReviews()}
+                </PostedReviewContainer>
+            </PanelBlock>
+        </MainContainer>
+    );
 };
 
 export default ActivityPanel;
