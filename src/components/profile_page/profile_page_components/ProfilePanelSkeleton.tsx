60e6e66a0d6c5be6bb0d40e78877d0cd33e42e69
Haz <zhouharry1@gmail.com>
2021-11-10 06:10:47 -0800

    Created skeleton for profile panel

--- src/components/profile_page/profile_page_components/ProfilePanelSkeleton.tsx
+++ src/components/profile_page/profile_page_components/ProfilePanelSkeleton.tsx
@@ -6,13 +6,69 @@ import { SkeletonLoaderLine } from '../../general_components/WorkoutProgramSkele
 //Styles:
 import styled from 'styled-components';
 
+import {
+    MainContainer,
+    AvatarContainer,
+    DescriptionContainer,
+    BioDescContainer,
+    EditProfileContainer,
+    OptionalMainContainer,
+    OptionalContainer,
+    LocationIcon,
+    LinkIcon,
+    TwitterIcon,
+} from './ProfilePanel';
+
 const SkeletonContainer = styled.section``;
+
+const ProfileSkeleton = styled(SkeletonLoaderLine)`
+    border-radius: 100%;
+`;
 
 const ProfilePanelSkeleton = () => {
     return (
-        <SkeletonContainer>
-            <SkeletonLoaderLine width="100%" height="10rem" />
-        </SkeletonContainer>
+        <MainContainer>
+            <AvatarContainer>
+                <ProfileSkeleton height="280px" width="280px" />
+            </AvatarContainer>
+            <DescriptionContainer>
+                <SkeletonLoaderLine
+                    height="3rem"
+                    width="100%"
+                    margin="1rem 0rem .5rem 0rem"
+                />
+                <SkeletonLoaderLine height="2rem" width="10rem" />
+            </DescriptionContainer>
+            <BioDescContainer>
+                <SkeletonLoaderLine height="10rem" width="100%" />
+            </BioDescContainer>
+            <OptionalMainContainer>
+                <OptionalContainer>
+                    <LocationIcon />
+                    <SkeletonLoaderLine
+                        height="1.5rem"
+                        width="100%"
+                        margin="0rem 0rem 0rem .5rem"
+                    />
+                </OptionalContainer>
+                <OptionalContainer>
+                    <LinkIcon />
+                    <SkeletonLoaderLine
+                        height="1.5rem"
+                        width="100%"
+                        margin="0rem 0rem 0rem .5rem"
+                    />
+                </OptionalContainer>
+                <OptionalContainer>
+                    <TwitterIcon />
+                    <SkeletonLoaderLine
+                        height="1.5rem"
+                        width="100%"
+                        margin="0rem 0rem 0rem .5rem"
+                    />
+                </OptionalContainer>
+            </OptionalMainContainer>
+        </MainContainer>
     );
 };
 
