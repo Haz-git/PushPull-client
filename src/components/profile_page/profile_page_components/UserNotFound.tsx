238ec2b73f1ee47585874b666887c28d41ecd9d1
Haz <zhouharry1@gmail.com>
2021-11-11 05:55:13 -0800

    Created component to handle user not found

---
+++ src/components/profile_page/profile_page_components/UserNotFound.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const UserNotFound = () => {
+    return <div>NOT FOUND!!!</div>;
+};
+
+export default UserNotFound;
