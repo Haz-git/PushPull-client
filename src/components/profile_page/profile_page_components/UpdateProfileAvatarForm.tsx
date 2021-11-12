aaaaf1e9f64efa5e2ebeee05fa0a6e6823711cc6
Haz <zhouharry1@gmail.com>
2021-11-12 05:21:17 -0800

    Installing dropzone

--- src/components/profile_page/profile_page_components/UpdateProfileAvatarForm.tsx
+++ src/components/profile_page/profile_page_components/UpdateProfileAvatarForm.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 
 //Components:
+import { IMAGE_MIME_TYPE, Dropzone } from '@mantine/dropzone';
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.section`
+    padding: 0rem 0.5rem;
+`;
 
 //Interfaces:
 
 const UpdateProfileAvatarForm = () => {
-    return <div>Update Profile avatar...</div>;
+    return <MainContainer>Update Profile avatar...</MainContainer>;
 };
 
 export default UpdateProfileAvatarForm;
