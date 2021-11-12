b6603734beeac1f2671ad0cd4a442cf5563d106f
Haz <zhouharry1@gmail.com>
2021-11-12 05:52:03 -0800

    Created basic reusable text component

---
+++ src/components/general_components/Text.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+const MainText = styled.p<IComponentProps>`
+    font-weight: ${(props) => props.fontWeight};
+    color: ${(props) => props.textColor};
+    font-size: ${(props) => props.fontSize};
+`;
+
+//Interfaces:
+
+interface IComponentProps {
+    text?: string;
+    fontWeight?: string;
+    textColor?: string;
+    fontSize?: string;
+    subText?: boolean;
+    mainText?: boolean;
+}
+
+const Text = ({
+    text,
+    fontWeight = '700',
+    textColor,
+    fontSize = '1rem',
+    subText,
+    mainText,
+}: IComponentProps): JSX.Element => {
+    const renderTextColor = () => {
+        if (textColor) return textColor;
+        if (!textColor && !mainText && subText) return 'rgba(0, 0, 34, .7)';
+        if (!textColor && !subText && mainText) return 'rgba(0, 0, 34, 1)';
+    };
+
+    return (
+        <MainText
+            fontWeight={fontWeight}
+            textColor={renderTextColor()}
+            fontSize={fontSize}
+        >
+            {text}
+        </MainText>
+    );
+};
+
+export default Text;
