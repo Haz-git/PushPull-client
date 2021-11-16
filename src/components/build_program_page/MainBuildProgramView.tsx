2db829954feb9ce151ea53a9940172dbbb4f58ac
Haz <zhouharry1@gmail.com>
2021-11-16 07:23:56 -0800

    Added view styling

--- src/components/build_program_page/MainBuildProgramView.tsx
+++ src/components/build_program_page/MainBuildProgramView.tsx
@@ -1,8 +1,34 @@
 import * as React from 'react';
+import { deviceMin } from '../../devices/breakpoints';
 
 //Components:
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.section`
+    height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 8fr;
+
+    @media ${deviceMin.mobileS} {
+        display: block;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    @media ${deviceMin.tabletp} {
+        height: 100%;
+        display: grid;
+        grid-template-columns: 1fr 8fr;
+        overflow: visible;
+        padding: 0rem 0rem 2rem 0rem;
+    }
+`;
+
+const ProjectPanelView = styled.section``;
+
+const DashboardPanelView = styled.section``;
 
 //Interfaces:
 
@@ -19,7 +45,12 @@ const MainBuildProgramView = ({
         params: { id },
     },
 }: IComponentProps): JSX.Element => {
-    return <div>Building program {id}</div>;
+    return (
+        <MainContainer>
+            <ProjectPanelView>Test</ProjectPanelView>
+            <DashboardPanelView>Test dashboard</DashboardPanelView>
+        </MainContainer>
+    );
 };
 
 export default MainBuildProgramView;
