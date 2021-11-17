5a96b5dfed3ea539be3d50e1958a20946c72f5bd
Haz <zhouharry1@gmail.com>
2021-11-17 06:52:10 -0800

    Added action templates

---
+++ src/redux/builder/builderActions.ts
@@ -0,0 +1,8 @@
+import api from '../../api';
+import { Dispatch } from 'redux';
+import { BuilderAction } from './builderInterfaces';
+import { BuilderActionType } from './action-types';
+
+export const findProject = () => {
+    return async (dispatch: Dispatch<BuilderAction>) => {};
+};
