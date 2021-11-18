8e984c5fd3439e3c2ea7af4f18d35efa8397a016
Haz <zhouharry1@gmail.com>
2021-11-18 07:17:47 -0800

    Created project form for submitting a new project

---
+++ src/components/build_program_page/build_program_components/AddProjectForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const AddProjectForm = () => {
+    return <div>addProjectForm</div>;
+};
+
+export default AddProjectForm;
