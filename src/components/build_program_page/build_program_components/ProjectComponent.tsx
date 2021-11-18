f4e06cd7802d1175b433fc63bde411d80c17e558
Haz <zhouharry1@gmail.com>
2021-11-18 06:24:13 -0800

    Added color swatch container

--- src/components/build_program_page/build_program_components/ProjectComponent.tsx
+++ src/components/build_program_page/build_program_components/ProjectComponent.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react';
 
 //Components:
+import Text from '../../general_components/Text';
+import { ColorSwatch } from '@mantine/core';
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    padding: 1rem 1rem;
+    border: 1px solid black;
+    margin: 0rem 0rem 1rem 0rem;
+`;
+
+const HeaderContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    column-gap: 2rem;
+`;
+
+const SwatchContainer = styled.div`
+    width: 20px;
+    height: 20px;
+`;
 
 //Interfaces:
 interface IComponentProps {
@@ -29,7 +51,20 @@ const ProjectComponent = ({
     projectName,
     updatedDate,
 }: IComponentProps): JSX.Element => {
-    return <div>{projectName}</div>;
+    return (
+        <MainContainer>
+            <HeaderContainer>
+                <SwatchContainer>
+                    <ColorSwatch
+                        size={20}
+                        radius={50}
+                        color={`#${projectColorHex}`}
+                    />
+                </SwatchContainer>
+                <Text text={projectName} />
+            </HeaderContainer>
+        </MainContainer>
+    );
 };
 
 export default ProjectComponent;
