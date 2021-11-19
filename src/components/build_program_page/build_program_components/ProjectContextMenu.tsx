f788c18929e0a86d2ccdef9b39d75e9dcf1a1936
Haz-git <zhouharry1@gmail.com>
2021-11-19 09:04:16 -0800

    Designed new project context menu

--- src/components/build_program_page/build_program_components/ProjectContextMenu.tsx
+++ src/components/build_program_page/build_program_components/ProjectContextMenu.tsx
@@ -7,13 +7,35 @@ import { Menu, Item, Separator, theme, animation } from 'react-contexify';
 import styled from 'styled-components';
 import 'react-contexify/dist/ReactContexify.css';
 
+import { Color } from '@styled-icons/fluentui-system-regular/Color';
+import { Rename } from '@styled-icons/fluentui-system-regular/Rename';
+import { Delete } from '@styled-icons/fluentui-system-regular/Delete';
+
+const ColorIcon = styled(Color)`
+    color: #ffffff;
+    height: 1.3rem;
+    width: 1.3rem;
+`;
+
+const RenameIcon = styled(Rename)`
+    color: #ffffff;
+    height: 1.3rem;
+    width: 1.3rem;
+`;
+
+const DeleteIcon = styled(Delete)`
+    color: #ffffff;
+    height: 1.5rem;
+    width: 1.5rem;
+    margin: -0.8rem -0.1rem;
+`;
+
 const StyledMenu = styled(Menu)`
-    .react-contexify__separator {
-        background: #dfdfdf;
-    }
     .react-contexify__item {
-        color: #81898f;
-        font-size: 1em;
+        padding: 0;
+        margin: 0;
+        color: #ffffff;
+        font-size: 0.85rem;
         font-weight: 500;
         font-family: 'Lato';
     }
@@ -22,8 +44,15 @@ const StyledMenu = styled(Menu)`
         > .react-contexify__item__content,
     .react-contexify__item:not(.react-contexify__item--disabled):focus
         > .react-contexify__item__content {
-        background-color: rgba(66, 99, 235, 0.85);
+        background-color: rgba(66, 99, 235, 1);
     }
+`;
+
+const ItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    column-gap: 0.5rem;
 `;
 
 //Interfaces:
@@ -33,14 +62,27 @@ interface IComponentProps {
 
 const ProjectContextMenu = ({ id }: IComponentProps): JSX.Element => {
     return (
-        <StyledMenu id={id} animation={animation.fade}>
-            <Item>Rename</Item>
+        <StyledMenu id={id} animation={animation.fade} theme={theme.dark}>
+            <Item>
+                <ItemContainer>
+                    <RenameIcon />
+                    Rename Project
+                </ItemContainer>
+            </Item>
             <Separator />
-            <Item>Move</Item>
+            <Item>
+                <ItemContainer>
+                    <ColorIcon />
+                    Change Color
+                </ItemContainer>
+            </Item>
             <Separator />
-            <Item>Star / Unstar</Item>
-            <Separator />
-            <Item>Delete</Item>
+            <Item>
+                <ItemContainer>
+                    <DeleteIcon />
+                    Delete Project
+                </ItemContainer>
+            </Item>
         </StyledMenu>
     );
 };
