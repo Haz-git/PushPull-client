951518ea1c4179ee06982dbdc4f61d4fd4ab3069
Haz <zhouharry1@gmail.com>
2021-11-21 07:04:53 -0800

    Created delete project form

---
+++ src/components/build_program_page/build_program_components/DeleteProjectForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const DeleteProjectForm = () => {
+    return <div>Confirm Delete Project.</div>;
+};
+
+export default DeleteProjectForm;
