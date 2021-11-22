0b98382d62eb82c893edb1d89e918473c236a7a5
Haz <zhouharry1@gmail.com>
2021-11-22 10:48:05 -0800

    Removed notifications provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NormalizeCSS } from '@mantine/core';
+// import './styles/notifStyles.css';
 import App from './components/App';
-
-import { NotificationsProvider } from '@mantine/notifications';
 
 //Redux:
 import { Provider } from 'react-redux';
@@ -16,14 +15,8 @@ ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <NotificationsProvider
-                    position="bottom-left"
-                    zIndex={99999}
-                    limit={5}
-                >
-                    <NormalizeCSS />
-                    <App />
-                </NotificationsProvider>
+                <NormalizeCSS />
+                <App />
             </PersistGate>
         </Provider>
     </React.StrictMode>,
