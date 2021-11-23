66488075e8f551a48575125976066a20377cea4f
Haz <zhouharry1@gmail.com>
2021-11-23 06:29:54 -0800

    .

--- src/components/nprogress/LoadProgress.tsx
+++ src/components/nprogress/LoadProgress.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 
 //Components:
+imoprt { useNProgress } from '@tanem/react-nprogress';
 
 //Styles:
 
 //Interfaces:
+interface IComponentProps {
+    isAnimating: boolean;
+}
 
-const LoadProgress = () => {
+const LoadProgress = ({ isAnimating }: IComponentProps): JSX.Element => {
+    const { animationDuration, isFinished, Progress } = useNProgress({
+        isAnimating
+    });
+
+    
     return <>Loading..</>;
 };
 
