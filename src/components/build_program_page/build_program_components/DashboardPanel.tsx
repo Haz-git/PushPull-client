9228a12f6a0e8a7c6bdcb8ee676e7bc52eed58d2
Haz-git <zhouharry1@gmail.com>
2021-11-24 13:40:25 -0800

    Added view label switching on switch

--- src/components/build_program_page/build_program_components/DashboardPanel.tsx
+++ src/components/build_program_page/build_program_components/DashboardPanel.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 //Components:
-import useWindowDimensions from '../../../utils/hooks/useWindowDimensions';
+import Text from '../../general_components/Text';
 
 //Router:
 import { useParams } from 'react-router-dom';
@@ -14,14 +14,51 @@ const MainContainer = styled.section`
     width: 100%;
 `;
 
-const ViewLabelContainer = styled.div``;
+const ViewLabelContainer = styled.div`
+    border-bottom: 1px solid #d6d6d6;
+    padding: 1rem 0rem;
+`;
+
+const ViewLabel = styled.div`
+    margin-left: 2rem;
+`;
 
 //Interfaces:
 
 const DashboardPanel = () => {
     let { dashboardView } = useParams<{ dashboardView: string }>();
     console.log(dashboardView);
-    return <MainContainer>{dashboardView || 'dashboard'}</MainContainer>;
+
+    const renderViewLabel = () => {
+        if (dashboardView) {
+            switch (dashboardView) {
+                case 'recents':
+                    return 'Recently Viewed';
+                case 'published':
+                    return 'Published Templates';
+                case 'drafts':
+                    return 'Drafts In Progress';
+                default:
+                    return undefined;
+            }
+        }
+
+        return undefined;
+    };
+
+    return (
+        <MainContainer>
+            <ViewLabelContainer>
+                <ViewLabel>
+                    <Text
+                        text={renderViewLabel()}
+                        fontSize="1.1rem"
+                        fontWeight="900"
+                    />
+                </ViewLabel>
+            </ViewLabelContainer>
+        </MainContainer>
+    );
 };
 
 export default DashboardPanel;
