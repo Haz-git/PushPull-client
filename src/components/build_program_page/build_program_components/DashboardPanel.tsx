ec55ef97d624702d51ed80e11320673efa2a99fd
Haz-git <zhouharry1@gmail.com>
2021-11-24 13:32:48 -0800

    Updated styling

--- src/components/build_program_page/build_program_components/DashboardPanel.tsx
+++ src/components/build_program_page/build_program_components/DashboardPanel.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 
+//Components:
+import useWindowDimensions from '../../../utils/hooks/useWindowDimensions';
+
 //Router:
 import { useParams } from 'react-router-dom';
+//Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.section`
+    background: #ffffff;
+    height: 100%;
+    width: 100%;
+`;
+
+const ViewLabelContainer = styled.div``;
+
+//Interfaces:
 
 const DashboardPanel = () => {
     let { dashboardView } = useParams<{ dashboardView: string }>();
     console.log(dashboardView);
-    return <div>{dashboardView || 'dashboard'}</div>;
+    return <MainContainer>{dashboardView || 'dashboard'}</MainContainer>;
 };
 
 export default DashboardPanel;
