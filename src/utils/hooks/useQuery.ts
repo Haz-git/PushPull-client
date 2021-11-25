ada8cee789299cb05c0f2c33772edc6f6d4b78ab
Haz <zhouharry1@gmail.com>
2021-11-25 06:55:00 -0800

    Added custom hook to process queries in URL

---
+++ src/utils/hooks/useQuery.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useQuery = () => {
+    const { search } = useLocation();
+
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+};
+
+export default useQuery;
