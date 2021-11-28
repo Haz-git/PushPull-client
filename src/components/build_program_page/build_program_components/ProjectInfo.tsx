b258b3d6f04648a6b9501963895d8deb33f01f1e
Haz <zhouharry1@gmail.com>
2021-11-28 07:22:21 -0800

    Added user details

--- src/components/build_program_page/build_program_components/ProjectInfo.tsx
+++ src/components/build_program_page/build_program_components/ProjectInfo.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 //Components:
-import { ColorSwatch } from '@mantine/core';
+import { Avatar, ColorSwatch } from '@mantine/core';
 import Text from '../../general_components/Text';
 import GeneralButton from '../../general_components/GeneralButton';
 import DividerLine from '../../general_components/DividerLine';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import dayjs from 'dayjs';
 
 //Styles:
 import styled from 'styled-components';
@@ -20,7 +22,7 @@ const MainContainer = styled.div`
     padding: 1rem 1rem;
     margin: 0rem 1rem 0rem 1rem;
     border-radius: 0.4rem;
-    background: #ececec;
+    background: #f1f1f1;
 `;
 
 const InfoHeaderContainer = styled.div`
@@ -29,13 +31,32 @@ const InfoHeaderContainer = styled.div`
     justify-content: flex-start;
     column-gap: 1rem;
     margin-bottom: 1.5rem;
+    max-width: 16rem;
 `;
 
-const InfoSwatch = styled.div``;
+const InfoSwatch = styled.div`
+    flex-grow: 1;
+`;
 
-const InfoHeader = styled.div``;
+const InfoHeader = styled.div`
+    flex-grow: 2;
+`;
 
 const InfoDesc = styled.div``;
+
+const CreatedBy = styled.div``;
+
+const CreatedByHeader = styled.div`
+    margin-bottom: 1rem;
+`;
+
+const CreatedByBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    column-gap: 0.5rem;
+    margin-bottom: 0.5rem;
+`;
 
 //Interfaces:
 
@@ -44,6 +65,7 @@ interface IComponentProps {
 }
 
 const ProjectInfo = ({ currProject }: IComponentProps): JSX.Element => {
+    dayjs.extend(LocalizedFormat);
     const {
         projectColorHex,
         projectDesc,
@@ -56,7 +78,14 @@ const ProjectInfo = ({ currProject }: IComponentProps): JSX.Element => {
 
     const renderProjectDesc = () => {
         if (projectDesc !== '')
-            return <Text text={projectDesc} fontSize="1rem" fontWeight="500" />;
+            return (
+                <Text
+                    text={projectDesc}
+                    fontSize="1.05rem"
+                    fontWeight="600"
+                    subText={true}
+                />
+            );
 
         return (
             <GeneralButton
@@ -78,6 +107,10 @@ const ProjectInfo = ({ currProject }: IComponentProps): JSX.Element => {
         );
     };
 
+    const processTime = (time: string) => {
+        if (time) return dayjs(time).format('LLLL');
+    };
+
     return (
         <MainContainer>
             <InfoHeaderContainer>
@@ -87,10 +120,9 @@ const ProjectInfo = ({ currProject }: IComponentProps): JSX.Element => {
                 <InfoHeader>
                     <Text
                         text={projectName}
-                        fontSize="1.5rem"
+                        fontSize="1.25rem"
                         fontWeight="800"
                         mainText={true}
-                        truncateWidth="9rem"
                     />
                 </InfoHeader>
             </InfoHeaderContainer>
@@ -99,6 +131,25 @@ const ProjectInfo = ({ currProject }: IComponentProps): JSX.Element => {
                 border="1px solid #d6d6d6"
                 margin="1.5rem 0rem 1.5rem 0rem"
             />
+            <CreatedBy>
+                <CreatedByHeader>
+                    <Text text="Created By" fontSize="1rem" fontWeight="800" />
+                </CreatedByHeader>
+                <CreatedByBox>
+                    <Avatar
+                        src={createdBy.userImage}
+                        size="sm"
+                        alt="User image"
+                        radius="xl"
+                    />
+                    <Text text={createdBy.username} fontSize="1rem" />
+                </CreatedByBox>
+                <Text
+                    text={processTime(createdBy.createdDate)}
+                    fontSize="1rem"
+                    subText={true}
+                />
+            </CreatedBy>
         </MainContainer>
     );
 };
