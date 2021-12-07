ff7a069d166dbebdd5ac997d88fcedbbc17e3093
Haz-git <zhouharry1@gmail.com>
2021-12-07 10:06:37 -0800

    Created template action types and interfaces

---
+++ src/redux/templates/templateInterfaces.ts
@@ -0,0 +1,26 @@
+import { TemplateActionType } from './action-types';
+
+interface findTemplate {
+    type: TemplateActionType.USER_FIND_TEMPLATE;
+    payload?: any;
+}
+
+interface addTemplate {
+    type: TemplateActionType.USER_ADD_TEMPLATE;
+    payload?: any;
+}
+
+interface updateTemplate {
+    type: TemplateActionType.USER_UPDATE_TEMPLATE;
+    payload?: any;
+}
+interface deleteTemplate {
+    type: TemplateActionType.USER_DELETE_TEMPLATE;
+    payload?: any;
+}
+
+export type TemplateAction =
+    | findTemplate
+    | addTemplate
+    | updateTemplate
+    | deleteTemplate;
