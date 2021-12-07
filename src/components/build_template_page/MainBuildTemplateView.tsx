286b8251659bfb35591eb5c5dd0fa75b49874673
Haz-git <zhouharry1@gmail.com>
2021-12-07 10:30:03 -0800

    Created a file for views to create workout templates

---
+++ src/components/build_template_page/MainBuildTemplateView.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const MainBuildTemplateView = () => {
+    return (
+        <div>
+            This should be the page that allows for users to build and create
+            own workout templates.
+        </div>
+    );
+};
+
+export default MainBuildTemplateView;
