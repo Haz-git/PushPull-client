c57f721fbac5cd19977ce46efe89d18a6c898e60
Haz-git <zhouharry1@gmail.com>
2021-12-07 10:51:09 -0800

    Added projectUUID requirement

--- src/redux/templates/templateActions.ts
+++ src/redux/templates/templateActions.ts
@@ -5,27 +5,24 @@ import { TemplateActionType } from './action-types';
 
 export const findTemplates = (
     statusCallback: (status: boolean) => void,
-    toggleNotif: () => string,
-    updateNotif: (id: string, status: boolean) => void
+    projectUuid: string | null
 ) => {
     return async (dispatch: Dispatch<TemplateAction>) => {
-        const id = toggleNotif();
-
         try {
-            let response = await api.get(`/builder/user`);
+            let response = await api.get(`/template/project/${projectUuid}`);
+
+            console.log(response.data);
 
             dispatch({
                 type: TemplateActionType.USER_FIND_TEMPLATE,
                 payload: response.data.builder,
             });
 
-            if (response.data) {
+            if (response?.data?.status === 'Success') {
                 statusCallback(true);
-                updateNotif(id, true);
             }
         } catch (err) {
             statusCallback(false);
-            updateNotif(id, false);
         }
     };
 };
