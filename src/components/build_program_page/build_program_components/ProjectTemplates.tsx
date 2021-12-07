44e4e15be63df5456b0d679f2e4991a852e5e13e
Haz-git <zhouharry1@gmail.com>
2021-12-07 10:18:31 -0800

    Created new component for generating project templates

---
+++ src/components/build_program_page/build_program_components/ProjectTemplates.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+const ProjectTemplates = () => {
+    return <div>Project Templates...</div>;
+};
+
+export default ProjectTemplates;
