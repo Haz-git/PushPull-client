5f4c0c8737310e5abe09bad2a431d1a70ae2686f
Haz-git <zhouharry1@gmail.com>
2021-12-07 10:52:38 -0800

    Added query on load

--- src/components/build_program_page/build_program_components/ProjectTemplates.tsx
+++ src/components/build_program_page/build_program_components/ProjectTemplates.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+//Redux:
+import { useSelector, RootStateOrAny, useDispatch } from 'react-redux';
+import { findTemplates } from '../../../redux/templates/templateActions';
 
 //Components:
+
+//Utils:
+import useQuery from '../../../utils/hooks/useQuery';
 
 //Styles:
 
 //Interfaces:
 
 const ProjectTemplates = () => {
+    const dispatch = useDispatch();
+    const query = useQuery();
+    const [isProjectsLoaded, setIsProjectsLoaded] = useState(false);
+
+    useEffect(() => {
+        dispatch(
+            findTemplates(
+                (status: boolean) => setIsProjectsLoaded(status),
+                query.get('uuid')
+            )
+        );
+    }, []);
+
     return <div>Project Templates...</div>;
 };
 
