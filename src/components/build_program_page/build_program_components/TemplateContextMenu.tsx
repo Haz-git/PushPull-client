e54649b14fe21ab33b1d00b2a44c7c6b69f56bfc
Haz-git <zhouharry1@gmail.com>
2021-12-10 07:08:22 -0800

    Set up basic context for templates

---
+++ src/components/build_program_page/build_program_components/TemplateContextMenu.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+//Components:
+import { Menu, Item, Separator, theme, animation } from 'react-contexify';
+
+//Styles:
+import 'react-contexify/dist/ReactContexify.css';
+import styled from 'styled-components';
+
+import {
+    ItemContainer,
+    StyledMenu,
+    RenameIcon,
+    DeleteIcon,
+} from './ProjectContextMenu';
+
+interface IComponentProps {
+    id: string | number;
+}
+
+const TemplateContextMenu = ({ id }: IComponentProps): JSX.Element => {
+    return (
+        <StyledMenu id={id} animation={animation.fade} theme={theme.dark}>
+            <Item>
+                <ItemContainer>
+                    <RenameIcon />
+                    Rename Project
+                </ItemContainer>
+            </Item>
+            <Separator />
+            <Item>
+                <ItemContainer>
+                    <DeleteIcon />
+                    Delete Project
+                </ItemContainer>
+            </Item>
+        </StyledMenu>
+    );
+};
+
+export default TemplateContextMenu;
