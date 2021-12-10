36ded6e8cf7820d35a83a0f67c984166e1408e65
Haz-git <zhouharry1@gmail.com>
2021-12-10 07:26:16 -0800

    Added open and open in new tab options

--- src/components/build_program_page/build_program_components/TemplateContextMenu.tsx
+++ src/components/build_program_page/build_program_components/TemplateContextMenu.tsx
@@ -6,13 +6,26 @@ import { Menu, Item, Separator, theme, animation } from 'react-contexify';
 //Styles:
 import 'react-contexify/dist/ReactContexify.css';
 import styled from 'styled-components';
-
+import { Open } from '@styled-icons/fluentui-system-filled/Open';
+import { Window } from '@styled-icons/fluentui-system-regular/Window';
 import {
     ItemContainer,
     StyledMenu,
     RenameIcon,
     DeleteIcon,
 } from './ProjectContextMenu';
+
+const NewTabIcon = styled(Open)`
+    color: #ffffff;
+    height: 1.25rem;
+    width: 1.25rem;
+`;
+
+const BrowserIcon = styled(Window)`
+    color: #ffffff;
+    height: 1.35rem;
+    width: 1.35rem;
+`;
 
 interface IComponentProps {
     id: string | number;
@@ -21,6 +34,20 @@ interface IComponentProps {
 const TemplateContextMenu = ({ id }: IComponentProps): JSX.Element => {
     return (
         <StyledMenu id={id} animation={animation.fade} theme={theme.dark}>
+            <Item>
+                <ItemContainer>
+                    <BrowserIcon />
+                    Open
+                </ItemContainer>
+            </Item>
+            <Separator />
+            <Item>
+                <ItemContainer>
+                    <NewTabIcon />
+                    Open In New Tab
+                </ItemContainer>
+            </Item>
+            <Separator />
             <Item>
                 <ItemContainer>
                     <RenameIcon />
