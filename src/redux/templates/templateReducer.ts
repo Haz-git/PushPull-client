8f38a723960c63288b97d4f85a688e18fe95da91
Haz-git <zhouharry1@gmail.com>
2021-12-15 15:00:32 -0800

    Added new reducer function.

--- src/redux/templates/templateReducer.ts
+++ src/redux/templates/templateReducer.ts
@@ -1,21 +1,12 @@
 import { TemplateActionType } from './action-types';
 import { TemplateAction } from './templateInterfaces';
 
-const initialState: any = [];
+const initialState: any = {};
 
-const templateReducer = (
-    state: Array<any> = initialState,
-    action: TemplateAction
-) => {
+const templateReducer = (state: {} = initialState, action: TemplateAction) => {
     switch (action.type) {
-        case TemplateActionType.USER_FIND_TEMPLATE:
-            return [...action.payload];
-        case TemplateActionType.USER_ADD_TEMPLATE:
-            return [...action.payload];
-        case TemplateActionType.USER_UPDATE_TEMPLATE:
-            return [...action.payload];
-        case TemplateActionType.USER_DELETE_TEMPLATE:
-            return [...action.payload];
+        case TemplateActionType.USER_QUERY_TEMPLATE:
+            return { ...action.payload };
         default:
             return state;
     }
