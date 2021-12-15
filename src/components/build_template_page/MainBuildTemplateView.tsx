8dd0040c74f86c07da71c936a76e0965c5dc62f5
Haz-git <zhouharry1@gmail.com>
2021-12-15 14:58:59 -0800

    Added functions to load state

--- src/components/build_template_page/MainBuildTemplateView.tsx
+++ src/components/build_template_page/MainBuildTemplateView.tsx
@@ -1,18 +1,44 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 //Components:
+import LoadProgress from '../nprogress/LoadProgress';
+
+//Redux:
+import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
+import { queryTemplate } from '../../redux/templates/templateActions';
 
 //Styles:
 
 //Interfaces:
 
-const MainBuildTemplateView = () => {
-    return (
-        <div>
-            This should be the page that allows for users to build and create
-            own workout templates.
-        </div>
-    );
+interface IComponentProps {
+    match: {
+        params: {
+            fileUuid: string;
+        };
+    };
+}
+
+const MainBuildTemplateView = ({
+    match: {
+        params: { fileUuid },
+    },
+}: IComponentProps): JSX.Element => {
+    const dispatch = useDispatch();
+    const [isTemplateLoaded, setIsTemplateLoaded] = useState(false);
+
+    useEffect(() => {
+        dispatch(
+            queryTemplate(fileUuid, (status: boolean) =>
+                setIsTemplateLoaded(status)
+            )
+        );
+    }, []);
+
+    const template = useSelector((state: RootStateOrAny) => state?.template);
+
+    return <>template view</>;
 };
 
 export default MainBuildTemplateView;
