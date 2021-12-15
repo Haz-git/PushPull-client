e5ec041e59af5b3435033b20d6b7584b64ef5809
Haz-git <zhouharry1@gmail.com>
2021-12-15 14:58:29 -0800

    renamed

---
+++ src/redux/templates/projectTemplateReducer.ts
@@ -0,0 +1,29 @@
+import { TemplateActionType } from './action-types';
+import { TemplateAction } from './templateInterfaces';
+
+const initialState: any = [];
+
+/*
+    Will need to refactor this. Currently for the templates part of redux, we have two different reducers:
+    1. one for querying all the templates registered under a specific view (or under a project) and what we do with them.
+    2. Another for entering a template and operations inside that editing mode...
+*/
+const projectTemplateReducer = (
+    state: Array<any> = initialState,
+    action: TemplateAction
+) => {
+    switch (action.type) {
+        case TemplateActionType.USER_FIND_TEMPLATE:
+            return [...action.payload];
+        case TemplateActionType.USER_ADD_TEMPLATE:
+            return [...action.payload];
+        case TemplateActionType.USER_UPDATE_TEMPLATE:
+            return [...action.payload];
+        case TemplateActionType.USER_DELETE_TEMPLATE:
+            return [...action.payload];
+        default:
+            return state;
+    }
+};
+
+export default projectTemplateReducer;
