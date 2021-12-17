65e388614e7ca7897c50d11c7da3229fc44f1e6c
Haz <zhouharry1@gmail.com>
2021-12-17 07:34:23 -0800

    Updated reducer

---
+++ src/redux/uiLoader/uiLoaderReducer.ts
@@ -0,0 +1,18 @@
+import { uiLoaderActionType } from './action-types';
+import { uiLoaderAction } from './uiLoaderInterfaces';
+
+const initialState: any = {};
+
+const uiLoaderReducer = (state: {} = initialState, action: uiLoaderAction) => {
+    switch (action.type) {
+        case uiLoaderActionType.START_LOADER_STATE:
+            return { ...action.payload };
+
+        case uiLoaderActionType.END_LOADER_STATE:
+            return { ...action.payload };
+        default:
+            return state;
+    }
+};
+
+export default uiLoaderReducer;
