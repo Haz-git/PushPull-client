f94d53ca5ee7bfdc675a46b3940a579c9aa25d5e
Haz <zhouharry1@gmail.com>
2021-12-19 09:21:22 -0800

    Created editing surface for creating templates

---
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.section``;
+
+//Interfaces:
+
+const EditingSurface = () => {
+    return <MainContainer>Editing Surface for template</MainContainer>;
+};
+
+export default EditingSurface;
