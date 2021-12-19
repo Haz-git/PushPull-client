d11de126845e690a1ed13161c696255f9e4a47e2
Haz <zhouharry1@gmail.com>
2021-12-19 09:38:07 -0800

    Fixed editing surface..included comment about problem

--- src/components/build_template_page/build_template_components/EditingSurface.tsx
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { deviceMin } from '../../../devices/breakpoints';
 
 //Components:
 
@@ -7,6 +8,35 @@ import styled from 'styled-components';
 
 const MainContainer = styled.section`
     width: 100%;
+    height: 100%;
+
+    /*
+        Incredibly frustrating working with this css. I have the exact same grid logic implemented for MainBuildProgramView, but for some reason grid-template-columns does not push the Editing surface the right of the Toolbar. It works perfectly without these media queries in the programview, but these below must be included in editing surface... css???
+    */
+
+    @media ${deviceMin.mobileS} {
+        margin-left: 11rem;
+    }
+
+    @media ${deviceMin.mobileM} {
+        margin-left: 12rem;
+    }
+
+    @media ${deviceMin.mobileL} {
+        margin-left: 13rem;
+    }
+
+    @media ${deviceMin.browserSm} {
+        margin-left: 14rem;
+    }
+
+    @media ${deviceMin.laptop} {
+        margin-left: 15rem;
+    }
+
+    @media ${deviceMin.laptopL} {
+        margin-left: 16rem;
+    }
 `;
 
 //Interfaces:
@@ -14,6 +44,15 @@ const MainContainer = styled.section`
 const EditingSurface = () => {
     return (
         <MainContainer>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
+            <div>TEETSTSETSETSE</div>
             <div>TEETSTSETSETSE</div>
         </MainContainer>
     );
