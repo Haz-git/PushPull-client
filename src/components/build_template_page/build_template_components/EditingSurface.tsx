5486bb61de0372436ee5b32ec06ab33b77601f87
Haz-git <zhouharry1@gmail.com>
2021-12-20 13:24:35 -0800

    Implemented pan pinch--white

--- src/components/build_template_page/build_template_components/EditingSurface.tsx
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -3,8 +3,11 @@ import { deviceMin } from '../../../devices/breakpoints';
 
 //Components:
 import useWindowDimensions from '../../../utils/hooks/useWindowDimensions';
+import GridLayout from 'react-grid-layout';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 //Styles:
+import 'react-grid-layout/css/styles.css';
 import styled from 'styled-components';
 
 const MainContainer = styled.section<IMainContainerProps>`
@@ -41,6 +44,11 @@ const MainContainer = styled.section<IMainContainerProps>`
     }
 `;
 
+const TestDraggableDiv = styled.div`
+    background: salmon;
+    border: 1px solid black;
+`;
+
 //Interfaces:
 
 interface IMainContainerProps {
@@ -51,9 +59,28 @@ interface IMainContainerProps {
 const EditingSurface = () => {
     const { width, height } = useWindowDimensions();
 
+    const layout = [
+        { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+        { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+        { i: 'c', x: 4, y: 0, w: 1, h: 2 },
+    ];
+
     return (
         <MainContainer height={height} width={width}>
-            test
+            <TransformWrapper initialScale={1}>
+                <TransformComponent>
+                    <GridLayout
+                        layout={layout}
+                        cols={12}
+                        rowHeight={25}
+                        width={width}
+                    >
+                        <TestDraggableDiv key="a">a</TestDraggableDiv>
+                        <TestDraggableDiv key="b">b</TestDraggableDiv>
+                        <TestDraggableDiv key="c">c</TestDraggableDiv>
+                    </GridLayout>
+                </TransformComponent>
+            </TransformWrapper>
         </MainContainer>
     );
 };
