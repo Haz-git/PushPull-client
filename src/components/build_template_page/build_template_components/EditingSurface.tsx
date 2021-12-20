83d636734ae9c7a4706fcb4e8cf4cb19d1b5b9d2
Haz-git <zhouharry1@gmail.com>
2021-12-20 10:55:18 -0800

    Detailed editing surface

--- src/components/build_template_page/build_template_components/EditingSurface.tsx
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 const MainContainer = styled.section<IMainContainerProps>`
     width: ${({ width }) => `${width}px`};
     height: ${({ height }) => `${height}px`};
-    background: #9e1818;
+    background: #ffffff;
 
     /*
         Incredibly frustrating working with this css. I have the exact same grid logic implemented for MainBuildProgramView, but for some reason grid-template-columns does not push the Editing surface the right of the Toolbar. It works perfectly without these media queries in the programview, but these below must be included in editing surface... css???
