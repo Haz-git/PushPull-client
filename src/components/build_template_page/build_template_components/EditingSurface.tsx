ea4b19f6aac0e2b45d8dfe17794a40c25fc7f6ed
Haz <zhouharry1@gmail.com>
2021-12-30 08:36:37 -0800

    Fixed the width of the editing surface

--- src/components/build_template_page/build_template_components/EditingSurface.tsx
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -14,9 +14,10 @@ import 'react-grid-layout/css/styles.css';
 import styled from 'styled-components';
 
 const MainContainer = styled.section<IMainContainerProps>`
-    width: ${({ width }) => `${width}px`};
+    width: ${({ width }) => `${width - 274}px`};
     height: ${({ height }) => `${height}px`};
     background: #ffffff;
+    border: 1px solid black;
 
     /*
         Incredibly frustrating working with this css. I have the exact same grid logic implemented for MainBuildProgramView, but for some reason grid-template-columns does not push the Editing surface the right of the Toolbar. It works perfectly without these media queries in the programview, but these below must be included in editing surface... css???
@@ -95,7 +96,7 @@ const EditingSurface = () => {
                 layout={gridLayout}
                 cols={12}
                 rowHeight={25}
-                width={width}
+                width={width - 274}
                 onDrop={onDrop}
                 isDroppable={true}
                 droppingItem={{
