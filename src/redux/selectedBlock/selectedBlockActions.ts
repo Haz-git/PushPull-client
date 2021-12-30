918388efb4c8eec314c8dc907916fe1c331d80ec
Haz <zhouharry1@gmail.com>
2021-12-30 07:13:47 -0800

    Added block selection actions

---
+++ src/redux/selectedBlock/selectedBlockActions.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from 'redux';
+import { SelectBlockAction } from './selectedBlockInterfaces';
+import { SelectedBlockActionType } from './action-types';
+
+export const selectBlock = (block: any) => {
+    return async (dispatch: Dispatch<SelectBlockAction>) => {
+        dispatch({
+            type: SelectedBlockActionType.USER_SELECT_BLOCK,
+            payload: {
+                block,
+            },
+        });
+    };
+};
+
+export const deselectBlock = () => {
+    return async (dispatch: Dispatch<SelectBlockAction>) => {
+        dispatch({
+            type: SelectedBlockActionType.USER_DESELECT_BLOCK,
+            payload: {},
+        });
+    };
+};
