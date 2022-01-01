e22f848b090802946b8320a75494ca955e0d2504
Haz <zhouharry1@gmail.com>
2022-01-01 10:39:53 -0800

    Created block type date for day blocks

---
+++ src/components/build_template_page/build_template_components/BlockTypeDate.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+interface IComponentProps {
+    blockTitle: string;
+}
+
+const BlockTypeDate = ({ blockTitle }: IComponentProps): JSX.Element => {
+    return <div>Date Block..</div>;
+};
+
+export default BlockTypeDate;
