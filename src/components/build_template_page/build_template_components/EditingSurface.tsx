44d092d57697a80ff73fb3abc93872d2aaa672f1
Haz <zhouharry1@gmail.com>
2022-01-02 09:18:39 -0800

    Abstracted lists from Editing surface

--- src/components/build_template_page/build_template_components/EditingSurface.tsx
+++ src/components/build_template_page/build_template_components/EditingSurface.tsx
@@ -6,8 +6,9 @@ import { deviceMin } from '../../../devices/breakpoints';
 import { RootStateOrAny, useSelector } from 'react-redux';
 
 //Components:
+import ListGrid from './ListGrid';
 import useWindowDimensions from '../../../utils/hooks/useWindowDimensions';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 //Styles:
 import styled from 'styled-components';
 
@@ -68,100 +69,10 @@ const EditingSurface = () => {
     );
     const { width, height } = useWindowDimensions();
 
-    const [items, setItems] = useState([] as any);
-
-    //Fake data gen:
-    const getItems = (count: number) =>
-        Array.from({ length: count }, (v, k) => k).map((k) => ({
-            id: `item-${k}`,
-            content: `item ${k}`,
-        }));
-
-    useEffect(() => {
-        setItems(getItems(10));
-    }, []);
-    //Reorder results:
-    const reorder = (list: any, startIndex: number, endIndex: number) => {
-        const result = Array.from(list);
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-
-        return result;
-    };
-
-    const grid = 8;
-
-    const getItemStyle = (isDragging: any, draggableStyle: any) => ({
-        // some basic styles to make the items look a bit nicer
-        userSelect: 'none',
-        padding: grid * 2,
-        margin: `0 0 ${grid}px 0`,
-
-        // change background colour if dragging
-        background: isDragging ? 'lightgreen' : 'grey',
-
-        // styles we need to apply on draggables
-        ...draggableStyle,
-    });
-
-    const getListStyle = (isDraggingOver: any) => ({
-        background: isDraggingOver ? 'lightblue' : 'lightgrey',
-        padding: grid,
-        width: 250,
-    });
-
-    const onDragEnd = (result: any) => {
-        // dropped outside the list
-        if (!result.destination) {
-            return;
-        }
-
-        const reorderedItems = reorder(
-            items,
-            result.source.index,
-            result.destination.index
-        );
-
-        setItems(reorderedItems);
-    };
-
     return (
         <MainContainer height={height} width={width}>
             <GridContainer>
-                <DragDropContext onDragEnd={onDragEnd}>
-                    <Droppable droppableId="droppable">
-                        {(provided, snapshot) => (
-                            <div
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                style={getListStyle(snapshot.isDraggingOver)}
-                            >
-                                {items.map((item: any, index: any) => (
-                                    <Draggable
-                                        key={item.id}
-                                        draggableId={item.id}
-                                        index={index}
-                                    >
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={getItemStyle(
-                                                    snapshot.isDragging,
-                                                    provided.draggableProps
-                                                        .style
-                                                )}
-                                            >
-                                                {item.content}
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
+                <ListGrid />
             </GridContainer>
         </MainContainer>
     );
