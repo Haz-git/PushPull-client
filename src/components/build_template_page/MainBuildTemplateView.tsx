0b11cb22fb9b4163e1811c397894a4ad427d4275
Haz <zhouharry1@gmail.com>
2022-01-03 15:44:52 -0800

    Wrapped entire component with DragDropContext

--- src/components/build_template_page/MainBuildTemplateView.tsx
+++ src/components/build_template_page/MainBuildTemplateView.tsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from 'react';
 import { deviceMin } from '../../devices/breakpoints';
 
 //Components:
+import { DragDropContext } from 'react-beautiful-dnd';
 import LoadProgress from '../nprogress/LoadProgress';
 import Toolbar from './build_template_components/Toolbar';
 import EditingSurface from './build_template_components/EditingSurface';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import GeneralModal from '../general_components/GeneralModal';
 import AddBlockForm from './build_template_components/AddBlockForm';
+import { v4 as uuid } from 'uuid';
 
 //Redux:
 import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
@@ -39,6 +41,37 @@ const MainContainer = styled.section`
     }
 `;
 
+//Helper Functions: For DragDropContext
+
+//Fake data gen:
+const getItems = (count: any, prefix: any) =>
+    Array.from({ length: count }, (v, k) => k).map((k) => {
+        const randomId = uuid();
+        return {
+            id: `item-${randomId}`,
+            prefix,
+            content: `item ${randomId}`,
+        };
+    });
+
+const removeFromList = (list: any, index: any) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(index, 1);
+    return [removed, result];
+};
+
+const addToList = (list: any, index: any, element: any) => {
+    const result = Array.from(list);
+    result.splice(index, 0, element);
+    return result;
+};
+
+const lists = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
+
+const generateLists = () =>
+    //getItems(10, listKey) <- Pass this into empty array below to provide dummy data.
+    lists.reduce((acc, listKey) => ({ ...acc, [listKey]: [] }), {});
+
 //Interfaces:
 
 interface IComponentProps {
@@ -55,10 +88,16 @@ const MainBuildTemplateView = ({
     },
 }: IComponentProps): JSX.Element => {
     const dispatch = useDispatch();
+
+    //Elements for drag drop context:
+    const [elements, setElements] = useState(generateLists()) as any;
+
     useEffect(() => {
         // controlTemplateLoadingStatus(true);
+        setElements(generateLists());
         dispatch(queryTemplate(fileUuid));
     }, []);
+
     const isLoading = useSelector(
         (state: RootStateOrAny) => state?.uiLoader?.isLoading
     );
@@ -67,6 +106,28 @@ const MainBuildTemplateView = ({
 
     const [openBlockModal, setOpenBlockModal] = useState(false);
     const controlBlockModal = (state: boolean) => setOpenBlockModal(state);
+
+    const onDragEnd = (result: any) => {
+        if (!result.destination) {
+            return;
+        }
+        const listCopy = { ...(elements as any) };
+
+        const sourceList = listCopy[result.source.droppableId];
+        const [removedElement, newSourceList] = removeFromList(
+            sourceList,
+            result.source.index
+        );
+        listCopy[result.source.droppableId] = newSourceList;
+        const destinationList = listCopy[result.destination.droppableId];
+        listCopy[result.destination.droppableId] = addToList(
+            destinationList,
+            result.destination.index,
+            removedElement
+        );
+
+        setElements(listCopy);
+    };
 
     return (
         <>
@@ -88,18 +149,20 @@ const MainBuildTemplateView = ({
                         />
                     </GeneralModal>
                     <MainContainer>
-                        <Toolbar
-                            controlBlockModal={controlBlockModal}
-                            togglePanningStatus={togglePanningStatus}
-                            isPanningDisabled={isPanningDisabled}
-                        />
-                        <TransformWrapper
-                            panning={{ disabled: isPanningDisabled }}
-                        >
-                            <TransformComponent>
-                                <EditingSurface />
-                            </TransformComponent>
-                        </TransformWrapper>
+                        <DragDropContext onDragEnd={onDragEnd}>
+                            <Toolbar
+                                controlBlockModal={controlBlockModal}
+                                togglePanningStatus={togglePanningStatus}
+                                isPanningDisabled={isPanningDisabled}
+                            />
+                            <TransformWrapper
+                                panning={{ disabled: isPanningDisabled }}
+                            >
+                                <TransformComponent>
+                                    <EditingSurface />
+                                </TransformComponent>
+                            </TransformWrapper>
+                        </DragDropContext>
                     </MainContainer>
                 </>
             )}
