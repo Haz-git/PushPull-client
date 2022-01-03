784d5a798bde81b2a18e13db1dd00477d3f2eb46
Haz <zhouharry1@gmail.com>
2022-01-03 09:58:32 -0800

    Added draggableComponent

--- src/components/build_template_page/build_template_components/BlockTypeExercise.tsx
+++ src/components/build_template_page/build_template_components/BlockTypeExercise.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
 
 //Components:
+import { Draggable } from 'react-beautiful-dnd';
 
 //Styles:
+import styled from 'styled-components';
 
 //Interfaces:
 
 interface IComponentProps {
-    blockTitle: string;
+    item: any;
+    index: any;
 }
 
-const BlockTypeExercise = ({ blockTitle }: IComponentProps): JSX.Element => {
-    return <div>{blockTitle}</div>;
+const BlockTypeExercise = ({ item, index }: IComponentProps): JSX.Element => {
+    return (
+        <Draggable draggableId={item.id} index={index}>
+            {(provided, snapshot) => {
+                return (
+                    <div
+                        ref={provided.innerRef}
+                        data-snapshot={snapshot}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                    >
+                        {item.id}
+                    </div>
+                );
+            }}
+        </Draggable>
+    );
 };
 
 export default BlockTypeExercise;
