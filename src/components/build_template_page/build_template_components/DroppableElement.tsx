039c42100ba89b7bd73bdf5c57722cd02d64baea
Haz-git <zhouharry1@gmail.com>
2022-01-05 10:52:25 -0800

    Making columns draggable

--- src/components/build_template_page/build_template_components/DroppableElement.tsx
+++ src/components/build_template_page/build_template_components/DroppableElement.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //Components:
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import BlockTypeExercise from './BlockTypeExercise';
 
 //Styles:
@@ -22,39 +22,49 @@ const DroppableStyles = styled.div`
 interface IComponentProps {
     prefix: any;
     elements: any;
+    columnIndex: any;
 }
 
 const DroppableElement = ({
     prefix,
     elements,
+    columnIndex,
 }: IComponentProps): JSX.Element => {
+    //The naming for this DroppableElement should be changed in the future to something like Columns, as this represents the Day columns on the editing surface and the individual blocks column in the toolbar.
     const disableDropForToolbar = () => {
         if (prefix === 'Blocks') return true;
         return false;
     };
 
     return (
-        <DroppableStyles>
-            <ColumnHeader>{prefix}</ColumnHeader>
-            <Droppable
-                droppableId={`${prefix}`}
-                isDropDisabled={disableDropForToolbar()}
-            >
-                {(provided: any) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {elements?.map((item: any, index: any) => (
-                            <BlockTypeExercise
-                                key={item.id}
-                                item={item}
-                                index={index}
-                                content={item.content}
-                            />
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </DroppableStyles>
+        <Draggable draggableId={`${prefix}`} index={columnIndex}>
+            {() => (
+                <DroppableStyles>
+                    <ColumnHeader>{prefix}</ColumnHeader>
+                    <Droppable
+                        droppableId={`${prefix}`}
+                        isDropDisabled={disableDropForToolbar()}
+                    >
+                        {(provided: any) => (
+                            <div
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                {elements?.map((item: any, index: any) => (
+                                    <BlockTypeExercise
+                                        key={item.id}
+                                        item={item}
+                                        index={index}
+                                        content={item.content}
+                                    />
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DroppableStyles>
+            )}
+        </Draggable>
     );
 };
 
