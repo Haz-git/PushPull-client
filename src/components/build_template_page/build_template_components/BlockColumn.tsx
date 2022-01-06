6b2ebf586175acf6d557912f81e0aa4b2cb41d6a
Haz-git <zhouharry1@gmail.com>
2022-01-06 13:27:03 -0800

    Added new column for holding columns in toolbar

---
+++ src/components/build_template_page/build_template_components/BlockColumn.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+//Components:
+import { Droppable } from 'react-beautiful-dnd';
+import BlockTypeExercise from './BlockTypeExercise';
+
+//Styles:
+import styled from 'styled-components';
+
+const ColumnHeader = styled.div`
+    text-transform: uppercase;
+    margin-bottom: 20px;
+`;
+
+const DroppableStyles = styled.div`
+    padding: 10px;
+    border-radius: 6px;
+    background: #d4d4d4;
+`;
+
+//Interfaces:
+interface IComponentProps {
+    prefix: any;
+    elements: any;
+}
+
+const BlockColumn = ({ prefix, elements }: IComponentProps): JSX.Element => {
+    return (
+        <DroppableStyles>
+            <ColumnHeader>{prefix}</ColumnHeader>
+            <Droppable droppableId={`${prefix}`} type={'EXERCISE_BLOCK'}>
+                {(provided: any) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {elements?.map((item: any, index: any) => (
+                            <BlockTypeExercise
+                                key={item.id}
+                                item={item}
+                                index={index}
+                                content={item.content}
+                            />
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+        </DroppableStyles>
+    );
+};
+
+export default BlockColumn;
