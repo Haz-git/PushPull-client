eb41041a9d8460cb2568e8a0a93b4ca83072c07c
Haz <zhouharry1@gmail.com>
2022-01-11 10:10:21 -0800

    Completed addBlockForm layout and template

--- src/components/build_template_page/build_template_components/AddBlockForm.tsx
+++ src/components/build_template_page/build_template_components/AddBlockForm.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 
 //Components:
 import GeneralButton from '../../general_components/GeneralButton';
+import { TextInput, Textarea, NumberInput } from '@mantine/core';
 
 //Redux:
 import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
@@ -11,6 +13,23 @@ import styled from 'styled-components';
 
 const MainContainer = styled.div`
     padding: 0rem 0.5rem;
+`;
+
+const FormContainer = styled.div``;
+
+const Spacer = styled.div`
+    height: 1rem;
+`;
+
+const FlexWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: 1rem;
+`;
+
+const ButtonContainer = styled.div`
+    margin: 2rem 0rem 1rem 0rem;
 `;
 
 //Interfaces:
@@ -25,6 +44,26 @@ const AddBlockForm = ({ closeModal }: IComponentProps): JSX.Element => {
         (state: RootStateOrAny) => state?.template
     );
 
+    //Modal input state
+    const [userInput, setUserInput] = useState({
+        name: '',
+        desc: '',
+        sets: '',
+        reps: '',
+    });
+
+    //Error state:
+    const [hasError, setHasError] = useState(false);
+
+    const handleUserInput = (name: string, val: string | number) => {
+        setUserInput({
+            ...userInput,
+            [name]: val,
+        });
+
+        console.log(userInput);
+    };
+
     const dispatchBlock = () => {
         dispatch(
             addToolbarBlock(currTemplate.id, {
@@ -37,12 +76,126 @@ const AddBlockForm = ({ closeModal }: IComponentProps): JSX.Element => {
 
     return (
         <MainContainer>
-            <div>
+            <FormContainer>
+                <TextInput
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '1rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 500,
+                        },
+                    }}
+                    required
+                    label="Block Name"
+                    placeholder={'Name your exercise'}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        if (hasError) setHasError(false);
+                        handleUserInput('name', e.target.value);
+                    }}
+                    value={userInput.name}
+                    error={hasError}
+                    // disabled={isCreatingNewProject}
+                />
+                <Spacer />
+                <Textarea
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '1rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 500,
+                            height: '8rem',
+                        },
+                    }}
+                    label="Block Description"
+                    placeholder="Block name and description can be updated at any time"
+                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                        handleUserInput('desc', e.target.value);
+                    }}
+                    value={userInput.desc}
+                    // disabled={isCreatingNewProject}
+                />
+                <Spacer />
+                <FlexWrapper>
+                    <NumberInput
+                        value={Number(userInput.sets)}
+                        label="Total Sets"
+                        min={0}
+                        max={99}
+                        required
+                        styles={{
+                            root: {
+                                maxWidth: '40rem',
+                            },
+                            label: {
+                                color: 'rgba(0, 0, 34, .7)',
+                                fontFamily: 'Lato, sans-serif',
+                                fontSize: '1.05rem',
+                                fontWeight: 700,
+                                marginBottom: '.25rem',
+                            },
+                            input: {
+                                color: 'rgba(0, 0, 34, 1)',
+                                fontFamily: 'Lato, sans-serif',
+                                fontSize: '1.05rem',
+                                fontWeight: 700,
+                            },
+                        }}
+                        onChange={(val: number) =>
+                            handleUserInput('sets', String(val))
+                        }
+                    />
+                    <NumberInput
+                        value={Number(userInput.reps)}
+                        label="Reps Per Set"
+                        min={0}
+                        max={99}
+                        required
+                        styles={{
+                            root: {
+                                maxWidth: '40rem',
+                            },
+                            label: {
+                                color: 'rgba(0, 0, 34, .7)',
+                                fontFamily: 'Lato, sans-serif',
+                                fontSize: '1.05rem',
+                                fontWeight: 700,
+                                marginBottom: '.25rem',
+                            },
+                            input: {
+                                color: 'rgba(0, 0, 34, 1)',
+                                fontFamily: 'Lato, sans-serif',
+                                fontSize: '1.05rem',
+                                fontWeight: 700,
+                            },
+                        }}
+                        onChange={(val: number) =>
+                            handleUserInput('reps', String(val))
+                        }
+                    />
+                </FlexWrapper>
+            </FormContainer>
+            <ButtonContainer>
                 <GeneralButton
                     buttonLabel="Click to add test block"
                     onClick={dispatchBlock}
                 />
-            </div>
+            </ButtonContainer>
         </MainContainer>
     );
 };
