6daa3cacbac3572621a1da192670961aa6634a15
Haz <zhouharry1@gmail.com>
2022-01-13 09:41:23 -0800

    Configured reducer to alter start and end loader states

--- src/redux/uiLoader/uiLoaderReducer.ts
+++ src/redux/uiLoader/uiLoaderReducer.ts
@@ -1,14 +1,25 @@
 import { uiLoaderActionType } from './action-types';
 import { uiLoaderAction } from './uiLoaderInterfaces';
 
-const initialState: any = {};
+const initialState: any = {
+    mainBuildTemplateView: {
+        isLoading: false,
+    },
+    addBlockModal: {
+        isLoading: false,
+    },
+};
 
 const uiLoaderReducer = (state: {} = initialState, action: uiLoaderAction) => {
     switch (action.type) {
-        case uiLoaderActionType.START_LOADER_STATE:
-            return { ...action.payload };
-        case uiLoaderActionType.END_LOADER_STATE:
-            return { ...action.payload };
+        case uiLoaderActionType.START_ADDBLOCKMODAL_LOADER:
+            return { ...state, addBlockModal: action.payload };
+        case uiLoaderActionType.START_MAINBUILDTEMPLATEVIEW_LOADER:
+            return { ...state, mainBuildTemplateView: action.payload };
+        case uiLoaderActionType.END_ADDBLOCKMODAL_LOADER:
+            return { ...state, addBlockModal: action.payload };
+        case uiLoaderActionType.END_MAINBUILDTEMPLATEVIEW_LOADER:
+            return { ...state, mainBuildTemplateView: action.payload };
         default:
             return state;
     }
