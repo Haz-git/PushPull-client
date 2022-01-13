cf663243ea7517e961eba888c1b1241591aa3d72
Haz <zhouharry1@gmail.com>
2022-01-13 09:39:50 -0800

    Expanded action creators to dispatch according to case

--- src/redux/uiLoader/uiLoaderActions.ts
+++ src/redux/uiLoader/uiLoaderActions.ts
@@ -2,24 +2,48 @@ import { Dispatch } from 'redux';
 import { uiLoaderAction } from './uiLoaderInterfaces';
 import { uiLoaderActionType } from './action-types';
 
-export const invokeLoaderState = () => {
+export const invokeLoaderState = (loaderType: string) => {
     return async (dispatch: Dispatch<uiLoaderAction>) => {
-        dispatch({
-            type: uiLoaderActionType.START_LOADER_STATE,
-            payload: {
-                isLoading: true,
-            },
-        });
+        switch (loaderType) {
+            case 'MAIN_BUILD_TEMPLATE_VIEW':
+                return dispatch({
+                    type: uiLoaderActionType.START_MAINBUILDTEMPLATEVIEW_LOADER,
+                    payload: {
+                        isLoading: true,
+                    },
+                });
+            case 'ADD_BLOCK_MODAL':
+                return dispatch({
+                    type: uiLoaderActionType.START_ADDBLOCKMODAL_LOADER,
+                    payload: {
+                        isLoading: true,
+                    },
+                });
+            default:
+                throw new Error('No loaderType was provided');
+        }
     };
 };
 
-export const disableLoaderState = () => {
+export const disableLoaderState = (loaderType: string) => {
     return async (dispatch: Dispatch<uiLoaderAction>) => {
-        dispatch({
-            type: uiLoaderActionType.END_LOADER_STATE,
-            payload: {
-                isLoading: false,
-            },
-        });
+        switch (loaderType) {
+            case 'MAIN_BUILD_TEMPLATE_VIEW':
+                return dispatch({
+                    type: uiLoaderActionType.END_MAINBUILDTEMPLATEVIEW_LOADER,
+                    payload: {
+                        isLoading: false,
+                    },
+                });
+            case 'ADD_BLOCK_MODAL':
+                return dispatch({
+                    type: uiLoaderActionType.END_ADDBLOCKMODAL_LOADER,
+                    payload: {
+                        isLoading: false,
+                    },
+                });
+            default:
+                throw new Error('No loaderType was provided');
+        }
     };
 };
