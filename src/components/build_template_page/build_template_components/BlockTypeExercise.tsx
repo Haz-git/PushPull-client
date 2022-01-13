52e0cf33495f881269e0c81f9a53a952b11fd0c4
Haz <zhouharry1@gmail.com>
2022-01-13 10:54:40 -0800

    Created hoverable more options button to top right of draggable

--- src/components/build_template_page/build_template_components/BlockTypeExercise.tsx
+++ src/components/build_template_page/build_template_components/BlockTypeExercise.tsx
@@ -6,8 +6,38 @@ import Text from '../../general_components/Text';
 
 //Styles:
 import styled from 'styled-components';
+import { MoreHorizontal } from '@styled-icons/fluentui-system-regular/MoreHorizontal';
+
+const MoreDots = styled(MoreHorizontal)`
+    color: #ffffff;
+    height: 1rem;
+    width: 1rem;
+`;
+
+const HoverableButton = styled.button`
+    opacity: 0;
+    display: block;
+    height: 1.5rem;
+    width: 1.5rem;
+    position: absolute;
+    right: -0.5rem;
+    top: -0.5rem;
+    border: none;
+    background: #e07133;
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: baseline;
+    cursor: pointer;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+        box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+            rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    }
+`;
 
 const MainContainer = styled.div`
+    position: relative;
     margin: 0.5rem 0rem;
     border-radius: 0.2rem;
     background: #ffffff;
@@ -15,6 +45,16 @@ const MainContainer = styled.div`
     border: 2px solid #e07133;
     box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
         rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    transition: all 0.1s ease-in;
+
+    &:hover {
+        box-shadow: rgba(9, 30, 66, 0.5) 0px 4px 8px -2px,
+            rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    }
+
+    &:hover ${HoverableButton} {
+        opacity: 100%;
+    }
 `;
 
 const BlockHeader = styled.div`
@@ -62,6 +102,9 @@ const BlockTypeExercise = ({
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                     >
+                        <HoverableButton>
+                            <MoreDots />
+                        </HoverableButton>
                         <BlockHeader>
                             <Text
                                 text={name}
