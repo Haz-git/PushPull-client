2be838fea9fecff0e6020d9e5333741f1bdb7d3b
Haz <zhouharry1@gmail.com>
2022-01-20 16:57:13 -0800

    Added basic layout

--- src/components/build_template_page/build_template_components/ViewerInteractionsForm.tsx
+++ src/components/build_template_page/build_template_components/ViewerInteractionsForm.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
 //Components:
+import Text from '../../general_components/Text';
 
 //Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    padding: 0rem 0.5rem;
+    position: relative;
+`;
+
+const DescContainer = styled.div``;
 
 const ViewerInteractionsForm = () => {
-    return <div>Test form for viewer interactions.</div>;
+    return (
+        <MainContainer>
+            <DescContainer>
+                <Text
+                    subText={true}
+                    text="Customize how your viewers interact with your template by organizing your blocks with colors to describe corresponding exercises, or, adding inputs where viewers can fill in information."
+                />
+            </DescContainer>
+        </MainContainer>
+    );
 };
 
 export default ViewerInteractionsForm;
