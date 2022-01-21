a70108b91dc305c6bf13aab5cfd3c675900bc41e
Haz <zhouharry1@gmail.com>
2022-01-21 09:38:48 -0800

    Added basic layout form without input handlers

--- src/components/build_template_page/build_template_components/AddColorForm.tsx
+++ src/components/build_template_page/build_template_components/AddColorForm.tsx
@@ -1,12 +1,104 @@
 import * as React from 'react';
 
 //Components:
+import { TextInput, Textarea, ColorInput } from '@mantine/core';
 
 //Styles:
 import styled from 'styled-components';
 
+const MainContainer = styled.div``;
+
+const FormContainer = styled.div``;
+
+const Divider = styled.div`
+    height: 0.5rem;
+`;
+
 const AddColorForm = () => {
-    return <div>Form for color add in popover.</div>;
+    return (
+        <MainContainer>
+            <FormContainer>
+                <TextInput
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.8rem',
+                            fontWeight: 500,
+                        },
+                    }}
+                    required
+                    label="Color Label"
+                    placeholder={'Label your color'}
+                    // onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    //     if (hasError) setHasError(false);
+                    //     setProjectName(e.target.value);
+                    // }}
+                    // value={projectName}
+                    // error={hasError}
+                    // disabled={isCreatingNewProject}
+                />
+                <Divider />
+                <Textarea
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.8rem',
+                            fontWeight: 500,
+                            height: '4rem',
+                        },
+                    }}
+                    label="Color Description"
+                    placeholder="Describe what your color means.."
+                    // onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                    //     setProjectDesc(e.target.value)
+                    // }
+                    // value={projectDesc}
+                    // disabled={isCreatingNewProject}
+                />
+                <Divider />
+                <ColorInput
+                    required
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                    }}
+                    placeholder="Pick your color"
+                    label="Select Color"
+                    disallowInput
+                    dropdownZIndex={9999}
+                    // onChange={(e: string) => setProjectColor(e)}
+                    // value={projectColor}
+                    // rightSection={
+                    //     <RandomButton onClick={generateRandomHexColor}>
+                    //         <RandomIcon />
+                    //     </RandomButton>
+                    // }
+                    // disabled={isCreatingNewProject}
+                />
+            </FormContainer>
+        </MainContainer>
+    );
 };
 
 export default AddColorForm;
