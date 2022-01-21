8f299dcadedafeaa68c43b0d4b738a1231cd40ac
Haz <zhouharry1@gmail.com>
2022-01-21 09:14:57 -0800

    Added layout for form addition and subtraction buttons

--- src/components/build_template_page/build_template_components/ViewerInteractionsForm.tsx
+++ src/components/build_template_page/build_template_components/ViewerInteractionsForm.tsx
@@ -7,12 +7,26 @@ import { Tooltip } from '@mantine/core';
 //Styles:
 import styled from 'styled-components';
 import { Info } from '@styled-icons/fluentui-system-filled/Info';
+import { Add } from '@styled-icons/fluentui-system-filled/Add';
+import { Subtract } from '@styled-icons/fluentui-system-regular/Subtract';
 
 const InfoIcon = styled(Info)`
     margin-bottom: -0.02rem;
     height: 1.5rem;
     width: 1.5rem;
     color: #e07133;
+`;
+
+const AddIcon = styled(Add)`
+    height: 1.25rem;
+    width: 1.25rem;
+    color: #ffffff;
+`;
+
+const SubtractIcon = styled(Subtract)`
+    height: 1.25rem;
+    width: 1.25rem;
+    color: #ffffff;
 `;
 
 const MainContainer = styled.div`
@@ -42,6 +56,33 @@ const ActionContainer = styled.div`
     border-radius: 0.3rem;
     height: 10rem;
     width: 100%;
+`;
+
+const ActionableButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 0.5rem 0rem;
+    width: 4rem;
+`;
+
+const AddButton = styled.button`
+    border: none;
+    text-decoration: none;
+    background: #e07133;
+    padding: 0.15rem 0.15rem;
+    border-radius: 0.3rem;
+    cursor: pointer;
+    margin-right: 0.15rem;
+`;
+
+const RemoveButton = styled.button`
+    border: none;
+    text-decoration: none;
+    background: #e07133;
+    padding: 0.15rem 0.15rem;
+    border-radius: 0.4rem;
+    cursor: pointer;
 `;
 
 const ViewerInteractionsForm = () => {
@@ -75,6 +116,14 @@ const ViewerInteractionsForm = () => {
                     </Tooltip>
                 </OptionHeader>
                 <ActionContainer></ActionContainer>
+                <ActionableButtonContainer>
+                    <AddButton>
+                        <AddIcon />
+                    </AddButton>
+                    <RemoveButton>
+                        <SubtractIcon />
+                    </RemoveButton>
+                </ActionableButtonContainer>
             </LegendContainer>
             <ViewerInputsContainer>
                 <OptionHeader>
@@ -92,7 +141,7 @@ const ViewerInteractionsForm = () => {
                         placement="center"
                         transition="fade"
                         transitionDuration={200}
-                        label="After adding an input question, the viewer will be prompted to enter his/her details when viewing the template. You can use his/her input details to dynamically calculate weights for each block."
+                        label="After adding an input question, the viewer will be prompted to enter his/her details when viewing the template. You can access his/her input details to dynamically calculate weights for each block."
                     >
                         <InfoIcon />
                     </Tooltip>
