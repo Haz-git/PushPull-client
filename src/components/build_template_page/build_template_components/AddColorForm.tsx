d0915f0ebf63365ec8517f531de2406b9a441c22
Haz <zhouharry1@gmail.com>
2022-01-21 09:17:14 -0800

    Created dummy form for adding colors

---
+++ src/components/build_template_page/build_template_components/AddColorForm.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+const AddColorForm = () => {
+    return <div>Form for color add in popover.</div>;
+};
+
+export default AddColorForm;
