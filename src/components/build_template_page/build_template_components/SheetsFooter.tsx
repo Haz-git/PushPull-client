a21e5978b0539f5435d30196b0208082754c9d6a
Haz-git <zhouharry1@gmail.com>
2022-01-25 13:07:05 -0800

    Created new component

---
+++ src/components/build_template_page/build_template_components/SheetsFooter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Components:
+
+//Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div``;
+
+//Interfaces:
+
+export const SheetsFooter = () => {
+    return <MainContainer>This should contain SHEETS</MainContainer>;
+};
