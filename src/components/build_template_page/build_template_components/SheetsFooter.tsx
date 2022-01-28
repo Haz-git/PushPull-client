38cd71370b2c5c66fe5c0b4569fe47ff6f0bf04e
Haz <zhouharry1@gmail.com>
2022-01-28 15:36:38 -0800

    Created button to add sheets

--- src/components/build_template_page/build_template_components/SheetsFooter.tsx
+++ src/components/build_template_page/build_template_components/SheetsFooter.tsx
@@ -5,9 +5,17 @@ import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
 
 //Components:
 import { SheetTab } from './SheetTab';
+import GeneralButton from '../../general_components/GeneralButton';
 
 //Styles:
 import styled from 'styled-components';
+import { PlusLg } from '@styled-icons/bootstrap/PlusLg';
+
+const AddIcon = styled(PlusLg)`
+    color: #e07133;
+    height: 1.2rem;
+    width: 1.2rem;
+`;
 
 const MainContainer = styled.div`
     position: fixed;
@@ -17,6 +25,8 @@ const MainContainer = styled.div`
     height: 2.5rem;
     width: 100%;
 `;
+
+const AddSheetContainer = styled.div``;
 
 const SheetContainer = styled.div`
     display: flex;
@@ -36,6 +46,21 @@ export const SheetsFooter = (): JSX.Element => {
     return (
         <MainContainer>
             <SheetContainer>
+                <AddSheetContainer>
+                    <GeneralButton
+                        height="2.5rem"
+                        borderRadius="0"
+                        hoverTransform="none"
+                        hoverShadow="none"
+                        buttonLabel=""
+                        buttonIconLeft={<AddIcon />}
+                        buttonBackground="#2c2c2c"
+                        leftIconMargin="0rem"
+                        rightIconMargin="0rem"
+                        margin="1px 1px 0px 0px"
+                        outline="1px solid #d6d6d6"
+                    />
+                </AddSheetContainer>
                 {sheets?.map((sheet: any) => (
                     <SheetTab
                         sheetName={sheet.weekName}
