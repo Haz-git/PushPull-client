7903256c0651bbac8f9721185e754fbe56cb0798
Haz <zhouharry1@gmail.com>
2022-01-30 10:16:03 -0800

    changed to const

--- src/redux/templates/templateActions.ts
+++ src/redux/templates/templateActions.ts
@@ -152,7 +152,7 @@ export const queryTemplate = (templateId: string) => {
         try {
             dispatch(invokeLoaderState(loaderTypes.MAIN_BUILD_TEMPLATE_VIEW));
 
-            let response = await api.get(`/template/query/${templateId}`);
+            const response = await api.get(`/template/query/${templateId}`);
 
             dispatch({
                 type: TemplateActionType.QUERY_TEMPLATE,
