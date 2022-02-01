5aa87d14af923b1e5dcb898d847fd1745bdd6a1c
Haz <zhouharry1@gmail.com>
2022-02-01 15:09:20 -0800

    Created dummy menu for sheet

---
+++ src/components/build_template_page/build_template_components/SheetMenu.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+//Components:
+import { Menu, MenuItem, Divider, Text } from '@mantine/core';
+
+//Styles:
+
+//Interfaces:
+
+const SheetMenu = () => {
+    return <div>This should be sheet's menu</div>;
+};
+
+export default SheetMenu;
