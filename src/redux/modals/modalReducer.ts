bdb3d8a9b4b4e2d2d78394d0e910399eaa2b5517
Haz-git <zhouharry1@gmail.com>
2022-02-02 11:50:44 -0800

    Updated Reducer

--- src/redux/modals/modalReducer.ts
+++ src/redux/modals/modalReducer.ts
@@ -1,12 +1,16 @@
 import { ModalActionTypes } from './action-types';
 import { ModalAction } from './modalInterfaces';
 
-const initialState: any = {};
+const initialState: any = {
+    DELETE_SHEET_CONFIRMATION: {
+        isOpen: false,
+    },
+};
 
 export const modalReducer = (state: {} = initialState, action: ModalAction) => {
     if (!Object.keys(ModalActionTypes).includes(action.type)) {
         return state;
     }
 
-    return { ...action.payload };
+    return { ...state, [action.type]: action.payload };
 };
