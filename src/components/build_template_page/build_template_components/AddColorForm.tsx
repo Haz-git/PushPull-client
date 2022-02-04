cf0dedd0a18b8f45a99a60031a8172eec571d6e3
Haz-git <zhouharry1@gmail.com>
2022-02-04 15:01:44 -0800

    Added input styles to resemble other inputs

--- src/components/build_template_page/build_template_components/AddColorForm.tsx
+++ src/components/build_template_page/build_template_components/AddColorForm.tsx
@@ -87,6 +87,12 @@ export const AddColorForm = () => {
                             fontWeight: 700,
                             marginBottom: '.25rem',
                         },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.8rem',
+                            fontWeight: 500,
+                        },
                     }}
                     placeholder="Pick your color"
                     label="Select Color"
