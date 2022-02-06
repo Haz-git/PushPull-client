40920e0fd041689a0e577947a01ffb8df2349c3c
Haz <zhouharry1@gmail.com>
2022-02-06 07:35:20 -0800

    Created deletion handling

--- src/components/build_template_page/build_template_components/DeleteColorForm.tsx
+++ src/components/build_template_page/build_template_components/DeleteColorForm.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 
+//Redux:
+import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+import { ModalActionTypes } from '../../../redux/modals/action-types';
+import { toggleModal } from '../../../redux/modals/modalActions';
+import { updateTemplate } from '../../../redux/templates/templateActions';
+
 //Components:
 import GeneralButton from '../../general_components/GeneralButton';
 import Text from '../../general_components/Text';
@@ -17,8 +23,50 @@ const TextDivider = styled.div`
 `;
 
 //Interfaces:
+interface IComponentProps {
+    currentSelectedColorId: string;
+}
 
-export const DeleteColorForm = () => {
+export const DeleteColorForm = ({
+    currentSelectedColorId,
+}: IComponentProps): JSX.Element => {
+    const dispatch = useDispatch();
+    const currentSavedColors = useSelector(
+        (state: RootStateOrAny) => state?.template?.templateLegend
+    );
+    const template = useSelector((state: RootStateOrAny) => state?.template);
+
+    const composeColorsPostDeletion = (): any[] => {
+        let savedColors = [...currentSavedColors];
+        const targetIndex = savedColors.findIndex(
+            (color) => color.id === currentSelectedColorId
+        );
+        savedColors.splice(targetIndex, 1);
+        return savedColors;
+    };
+
+    const handleColorDeletion = (): void => {
+        if (!currentSelectedColorId) {
+            return;
+        }
+
+        const newColorArray = composeColorsPostDeletion();
+
+        dispatch(
+            updateTemplate(
+                (status) => {},
+                template.id,
+                { templateLegend: newColorArray },
+                true,
+                null
+            )
+        );
+
+        dispatch(
+            toggleModal(ModalActionTypes.DELETE_COLOR_SWATCH_POPOVER, 'CLOSE')
+        );
+    };
+
     return (
         <MainContainer>
             <div>
@@ -46,6 +94,7 @@ export const DeleteColorForm = () => {
                     fontSize="1rem"
                     height="2rem"
                     leftIconMargin="0rem .3rem -.2rem 0rem"
+                    onClick={handleColorDeletion}
                 />
             </ButtonContainer>
         </MainContainer>
