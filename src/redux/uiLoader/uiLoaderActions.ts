b0d4a3257ce908c1b076d7710dc78f52ff371802
Haz <zhouharry1@gmail.com>
2022-02-07 16:14:22 -0800

    Refactored from switch case

--- src/redux/uiLoader/uiLoaderActions.ts
+++ src/redux/uiLoader/uiLoaderActions.ts
@@ -2,76 +2,32 @@ import { Dispatch } from 'redux';
 import { uiLoaderAction } from './uiLoaderInterfaces';
 import { uiLoaderActionType } from './action-types';
 
-export const invokeLoaderState = (loaderType: string) => {
+export const invokeLoaderState = (loaderType: any): Function => {
     return async (dispatch: Dispatch<uiLoaderAction>) => {
-        switch (loaderType) {
-            case 'START_TEMPLATE_BLOCK_LOADER':
-                return dispatch({
-                    type: uiLoaderActionType.START_TEMPLATE_BLOCK_LOADER,
-                    payload: {
-                        isLoading: true,
-                    },
-                });
-            case 'MAIN_BUILD_TEMPLATE_VIEW':
-                return dispatch({
-                    type: uiLoaderActionType.START_MAINBUILDTEMPLATEVIEW_LOADER,
-                    payload: {
-                        isLoading: true,
-                    },
-                });
-            case 'ADD_BLOCK_MODAL':
-                return dispatch({
-                    type: uiLoaderActionType.START_ADDBLOCKMODAL_LOADER,
-                    payload: {
-                        isLoading: true,
-                    },
-                });
-            case 'GLOBAL_SETTINGS_MODAL':
-                return dispatch({
-                    type: uiLoaderActionType.START_GLOBALSETTINGSMODAL_LOADER,
-                    payload: {
-                        isLoading: true,
-                    },
-                });
-            default:
-                throw new Error('No loaderType was provided');
+        if (!Object.values(uiLoaderActionType).includes(loaderType)) {
+            return;
         }
+
+        return dispatch({
+            type: loaderType,
+            payload: {
+                isLoading: true,
+            },
+        });
     };
 };
 
-export const disableLoaderState = (loaderType: string) => {
+export const disableLoaderState = (loaderType: any) => {
     return async (dispatch: Dispatch<uiLoaderAction>) => {
-        switch (loaderType) {
-            case 'END_TEMPLATE_BLOCK_LOADER':
-                return dispatch({
-                    type: uiLoaderActionType.END_TEMPLATE_BLOCK_LOADER,
-                    payload: {
-                        isLoading: false,
-                    },
-                });
-            case 'MAIN_BUILD_TEMPLATE_VIEW':
-                return dispatch({
-                    type: uiLoaderActionType.END_MAINBUILDTEMPLATEVIEW_LOADER,
-                    payload: {
-                        isLoading: false,
-                    },
-                });
-            case 'ADD_BLOCK_MODAL':
-                return dispatch({
-                    type: uiLoaderActionType.END_ADDBLOCKMODAL_LOADER,
-                    payload: {
-                        isLoading: false,
-                    },
-                });
-            case 'GLOBAL_SETTINGS_MODAL':
-                return dispatch({
-                    type: uiLoaderActionType.END_GLOBALSETTINGSMODAL_LOADER,
-                    payload: {
-                        isLoading: false,
-                    },
-                });
-            default:
-                throw new Error('No loaderType was provided');
+        if (!Object.values(uiLoaderActionType).includes(loaderType)) {
+            return;
         }
+
+        return dispatch({
+            type: loaderType,
+            payload: {
+                isLoading: false,
+            },
+        });
     };
 };
