dbf88240b8fdee71b2a6edb74f5acf19b2f734da
Haz <zhouharry1@gmail.com>
2022-02-07 16:26:07 -0800

    Refactored reducer

--- src/redux/uiLoader/uiLoaderReducer.ts
+++ src/redux/uiLoader/uiLoaderReducer.ts
@@ -14,29 +14,17 @@ const initialState: any = {
     globalSettingsModal: {
         isLoading: false,
     },
+    viewerInteractionsSettingsModal: {
+        isLoading: false,
+    },
 };
 
 const uiLoaderReducer = (state: {} = initialState, action: uiLoaderAction) => {
-    switch (action.type) {
-        case uiLoaderActionType.START_ADDBLOCKMODAL_LOADER:
-            return { ...state, addBlockModal: action.payload };
-        case uiLoaderActionType.START_MAINBUILDTEMPLATEVIEW_LOADER:
-            return { ...state, mainBuildTemplateView: action.payload };
-        case uiLoaderActionType.START_TEMPLATE_BLOCK_LOADER:
-            return { ...state, statusTemplateBlocks: action.payload };
-        case uiLoaderActionType.START_GLOBALSETTINGSMODAL_LOADER:
-            return { ...state, globalSettingsModal: action.payload };
-        case uiLoaderActionType.END_GLOBALSETTINGSMODAL_LOADER:
-            return { ...state, globalSettingsModal: action.payload };
-        case uiLoaderActionType.END_TEMPLATE_BLOCK_LOADER:
-            return { ...state, statusTemplateBlocks: action.payload };
-        case uiLoaderActionType.END_ADDBLOCKMODAL_LOADER:
-            return { ...state, addBlockModal: action.payload };
-        case uiLoaderActionType.END_MAINBUILDTEMPLATEVIEW_LOADER:
-            return { ...state, mainBuildTemplateView: action.payload };
-        default:
-            return state;
+    if (!Object.keys(uiLoaderActionType).includes(action.type)) {
+        return state;
     }
+
+    return { ...state, [action.type]: action.payload };
 };
 
 export default uiLoaderReducer;
