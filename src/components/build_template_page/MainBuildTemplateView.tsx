d0334b00f2927fd9285363d2ec730cea9467567c
Haz <zhouharry1@gmail.com>
2022-02-07 17:22:18 -0800

    Attached isLoading items to modals

--- src/components/build_template_page/MainBuildTemplateView.tsx
+++ src/components/build_template_page/MainBuildTemplateView.tsx
@@ -107,11 +107,20 @@ const MainBuildTemplateView = ({
 
     const isMainViewLoading = useSelector(
         (state: RootStateOrAny) =>
-            state?.uiLoader?.mainBuildTemplateView?.isLoading
+            state?.uiLoader?.MAIN_BUILD_TEMPLATE_VIEW?.isLoading
     );
 
     const isAddBlockModalLoading = useSelector(
-        (state: RootStateOrAny) => state?.uiLoader?.addBlockModal?.isLoading
+        (state: RootStateOrAny) => state?.uiLoader?.ADD_BLOCK_MODAL?.isLoading
+    );
+
+    const isViewerInteractionsSettingsModalLoading = useSelector(
+        (state: RootStateOrAny) =>
+            state?.uiLoader?.VIEWER_INTERACTIONS_SETTINGS_MODAL?.isLoading
+    );
+    //Loader state for global settings modal on update
+    const { isLoading: isGlobalSettingsModalLoading } = useSelector(
+        (state: RootStateOrAny) => state?.uiLoader?.GLOBAL_SETTINGS_MODAL
     );
 
     const error = useSelector((state: RootStateOrAny) => state?.errors);
@@ -183,7 +192,6 @@ const MainBuildTemplateView = ({
     const controlViewerInteractionsModal = (state: boolean): void => {
         setOpenViewerInteractionsModal(state);
     };
-
     // Modal for viewing and editing Global Inputs:
     const [openGlobalModal, setOpenGlobalModal] = useState(false);
     const controlGlobalModal = (state: boolean): void => {
@@ -195,11 +203,6 @@ const MainBuildTemplateView = ({
     const controlBlockModal = (state: boolean): void => {
         setOpenBlockModal(state);
     };
-
-    //Loader state for global settings modal on update
-    const { isLoading: isGlobalSettingsModalLoading } = useSelector(
-        (state: RootStateOrAny) => state?.uiLoader?.globalSettingsModal
-    );
 
     //Helper Functions: For DragDropContext
 
@@ -350,6 +353,7 @@ const MainBuildTemplateView = ({
                             />
                         </GeneralModal>
                         <GeneralModal
+                            isLoading={isViewerInteractionsSettingsModalLoading}
                             size="lg"
                             closeOnClickOutside={false}
                             title="Viewer Interactions Settings"
@@ -361,22 +365,22 @@ const MainBuildTemplateView = ({
                             <ViewerInteractionsForm />
                         </GeneralModal>
                         <GeneralModal
+                            isLoading={isGlobalSettingsModalLoading}
                             closeOnClickOutside={false}
                             title="Template Global Settings"
                             openBoolean={openGlobalModal}
                             closeFunc={() => setOpenGlobalModal(false)}
-                            isLoading={isGlobalSettingsModalLoading}
                         >
                             <GlobalSettingsForm
                                 toggleGlobalSettingsModal={controlGlobalModal}
                             />
                         </GeneralModal>
                         <GeneralModal
+                            isLoading={isAddBlockModalLoading}
                             closeOnClickOutside={false}
                             title="Add New Block"
                             openBoolean={openBlockModal}
                             closeFunc={() => setOpenBlockModal(false)}
-                            isLoading={isAddBlockModalLoading}
                         >
                             <AddBlockForm
                                 closeModal={() => setOpenBlockModal(false)}
