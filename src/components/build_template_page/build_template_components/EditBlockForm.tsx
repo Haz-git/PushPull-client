6ad09eae00ca0494c9dea4ae4dcbdfbdc4ae7d1b
Haz <zhouharry1@gmail.com>
2022-02-08 17:22:47 -0800

    Added placeholders for linking interactions

--- src/components/build_template_page/build_template_components/EditBlockForm.tsx
+++ src/components/build_template_page/build_template_components/EditBlockForm.tsx
@@ -8,7 +8,9 @@ import { ModalActionTypes } from '../../../redux/modals/action-types';
 
 //Components:
 import GeneralButton from '../../general_components/GeneralButton';
-import { TextInput, Textarea, NumberInput } from '@mantine/core';
+import DividerLine from '../../general_components/DividerLine';
+import { TextInput, Textarea, NumberInput, Select } from '@mantine/core';
+import Text from '../../general_components/Text';
 
 //Styles:
 import styled from 'styled-components';
@@ -180,6 +182,57 @@ export const EditBlockForm = () => {
                         // }
                     />
                 </FlexWrapper>
+                <DividerLine
+                    border="1px solid #d6d6d6"
+                    margin="2rem 0rem 1rem 0rem"
+                />
+                <Text text="Linked Interactions" fontSize="1.5rem" />
+                <Spacer />
+                <Select
+                    clearable
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '1rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 500,
+                        },
+                    }}
+                    label="Color Legend"
+                    placeholder="Link a color"
+                    data={[{ value: 'Super Set 1', label: 'Super Set 1' }]}
+                    required
+                />
+                <Spacer />
+                <Select
+                    clearable
+                    styles={{
+                        label: {
+                            color: 'rgba(0, 0, 34, .7)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '1rem',
+                            fontWeight: 700,
+                            marginBottom: '.25rem',
+                        },
+                        input: {
+                            color: 'rgba(0, 0, 34, 1)',
+                            fontFamily: 'Lato, sans-serif',
+                            fontSize: '.9rem',
+                            fontWeight: 500,
+                        },
+                    }}
+                    label="Viewer Input"
+                    placeholder="Link a viewer input"
+                    data={[{ value: 'maxBench', label: 'Max Bench' }]}
+                    required
+                />
             </FormContainer>
             <ButtonContainer>
                 <GeneralButton buttonLabel="Update Block" />
