a90298ce6637607d18fa562cdedd71bdb0a305dd
Haz <zhouharry1@gmail.com>
2022-02-16 16:39:30 -0800

    Added debounced values

--- src/components/build_template_page/build_template_components/SetConfigurationMenu.tsx
+++ src/components/build_template_page/build_template_components/SetConfigurationMenu.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 //Components:
 import { SetConfigurationField } from './SetConfigurationField';
 import { v4 as uuid } from 'uuid';
+import { useDebouncedValue } from '@mantine/hooks';
 
 //Styles:
 
@@ -17,14 +18,27 @@ export const SetConfigurationMenu = ({
     isOpen,
     totalSets,
 }: IComponentProps): JSX.Element => {
+    //Using debounced value here to delay rendering configuration fields to user. Not sure if correct usage here, but I would consider mapping new items to the dom as expensive?
+    const [debouncedNumberOfSets] = useDebouncedValue(totalSets, 200, {
+        leading: true,
+    });
+
     const renderConfigurationFields = () => {
-        if (!totalSets || Number(totalSets) === 0) {
+        const hasMissingRequirements =
+            !Number(debouncedNumberOfSets) ||
+            Number(debouncedNumberOfSets) === 0 ||
+            Number(debouncedNumberOfSets) > 15;
+
+        if (hasMissingRequirements) {
             return null;
         }
 
-        return Array.from(Array(Number(totalSets)), (element, index) => (
-            <SetConfigurationField fieldId={index} key={uuid()} />
-        ));
+        return Array.from(
+            Array(Number(debouncedNumberOfSets)),
+            (element, index) => (
+                <SetConfigurationField fieldId={index} key={uuid()} />
+            )
+        );
     };
 
     return <>{isOpen ? <>{renderConfigurationFields()}</> : null}</>;
