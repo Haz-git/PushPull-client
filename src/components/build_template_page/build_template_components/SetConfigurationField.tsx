7fdac342b02b442ac51690fe1cc586802bececed
Haz <zhouharry1@gmail.com>
2022-02-16 16:24:47 -0800

    Added field Id

--- src/components/build_template_page/build_template_components/SetConfigurationField.tsx
+++ src/components/build_template_page/build_template_components/SetConfigurationField.tsx
@@ -5,7 +5,12 @@ import * as React from 'react';
 //Styles:
 
 //Interfaces:
+interface IComponentProps {
+    fieldId: number;
+}
 
-export const SetConfigurationField = () => {
+export const SetConfigurationField = ({
+    fieldId,
+}: IComponentProps): JSX.Element => {
     return <div>SetConfigurationField</div>;
 };
