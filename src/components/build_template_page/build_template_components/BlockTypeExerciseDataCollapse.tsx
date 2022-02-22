17c552261fb787093ed10b82b842004da2fd51a1
Haz <zhouharry1@gmail.com>
2022-02-21 16:48:52 -0800

    Documenting Error I need to come back to

--- src/components/build_template_page/build_template_components/BlockTypeExerciseDataCollapse.tsx
+++ src/components/build_template_page/build_template_components/BlockTypeExerciseDataCollapse.tsx
@@ -70,7 +70,10 @@ export const BlockTypeExerciseDataCollapse = ({
         });
     }, [configuredSets]);
 
+    console.log(configuredSets);
+
     const renderTotalSets = (): React.ReactNode => {
+        //For some reason, this still returns a "unique key prop" error even though outermost element has a unique key...
         return Object.values(configuredSets).map((set: any) => (
             <SetWrapper key={set.uuid}>
                 <Spacer />
