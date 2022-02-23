fba6f2c6f933b001a7e8b20418cb72f9c4af67fc
Haz <zhouharry1@gmail.com>
2022-02-22 16:49:16 -0800

    Created new component for managing scroll

---
+++ src/components/build_template_page/build_template_components/SheetsFooterNavButtons.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+//Components:
+import GeneralButton from '../../general_components/GeneralButton';
+
+//Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #2c2c2c;
+    height: 100%;
+    outline: 1px solid #d6d6d6;
+    margin-top: 1px;
+    margin-left: 1px;
+`;
+
+//Interfaces:
+
+export const SheetsFooterNavButtons = () => {
+    return <MainContainer>test</MainContainer>;
+};
