fef473e05b619a23d03cfe660768cb83bec9c387
Haz <zhouharry1@gmail.com>
2022-02-24 17:08:13 -0800

    Added new props, rendering different colors on output

--- src/components/build_template_page/build_template_components/SheetsFooterNavButtons.tsx
+++ src/components/build_template_page/build_template_components/SheetsFooterNavButtons.tsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 
 //Components:
 import GeneralButton from '../../general_components/GeneralButton';
+import { Tooltip } from '@mantine/core';
 
 //Styles:
 import styled from 'styled-components';
 import { CaretLeft } from 'styled-icons/fluentui-system-filled';
 import { CaretRight } from 'styled-icons/fluentui-system-filled';
 
-const LeftIcon = styled(CaretLeft)`
-    height: 1.5rem;
-    width: 1.5rem;
-    color: #e07133;
+const LeftIcon = styled(CaretLeft)<IScrollButton>`
+    height: 1.25rem;
+    width: 1.25rem;
+    color: ${({ isAllowed }) => (isAllowed === true ? '#e07133' : '#ececec')};
 `;
 
-const RightIcon = styled(CaretRight)`
-    height: 1.5rem;
-    width: 1.5rem;
-    color: #e07133;
+const RightIcon = styled(CaretRight)<IScrollButton>`
+    height: 1.25rem;
+    width: 1.25rem;
+    color: ${({ isAllowed }) => (isAllowed === true ? '#e07133' : '#ececec')};
 `;
 
 const MainContainer = styled.div`
@@ -29,6 +30,8 @@ const MainContainer = styled.div`
     outline: 1px solid #d6d6d6;
     margin-top: 1px;
     margin-left: 1px;
+    padding: 0rem 0.5rem;
+    column-gap: 0.5rem;
 `;
 
 const MoveButton = styled.button`
@@ -39,27 +42,39 @@ const MoveButton = styled.button`
 
 //Interfaces:
 
+interface IScrollButton {
+    isAllowed: boolean;
+}
+
 interface IComponentProps {
     handleScrollSlide: (shift: number) => void;
     horizontalScrollWidth: number;
+    allowScrollLeft: boolean;
+    allowScrollRight: boolean;
 }
 
 export const SheetsFooterNavButtons = ({
     handleScrollSlide,
     horizontalScrollWidth,
+    allowScrollLeft,
+    allowScrollRight,
 }: IComponentProps): JSX.Element => {
     return (
         <MainContainer>
-            <MoveButton
-                onClick={() => handleScrollSlide(-horizontalScrollWidth)}
-            >
-                <LeftIcon />
-            </MoveButton>
-            <MoveButton
-                onClick={() => handleScrollSlide(horizontalScrollWidth)}
-            >
-                <RightIcon />
-            </MoveButton>
+            <Tooltip transition="fade" label="Scroll Left" color="dark">
+                <MoveButton
+                    onClick={() => handleScrollSlide(-horizontalScrollWidth)}
+                >
+                    <LeftIcon isAllowed={allowScrollLeft} />
+                </MoveButton>
+            </Tooltip>
+            <Tooltip transition="fade" label="Scroll Right" color="dark">
+                <MoveButton
+                    onClick={() => handleScrollSlide(horizontalScrollWidth)}
+                >
+                    <RightIcon isAllowed={allowScrollRight} />
+                </MoveButton>
+            </Tooltip>
         </MainContainer>
     );
 };
