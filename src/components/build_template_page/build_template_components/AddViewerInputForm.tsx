e7e7ebbaa7eda55de6ef3837698e26d57489716c
Haz <zhouharry1@gmail.com>
2022-02-28 16:46:02 -0800

    Experimenting with React-hook-form-- basic form

--- src/components/build_template_page/build_template_components/AddViewerInputForm.tsx
+++ src/components/build_template_page/build_template_components/AddViewerInputForm.tsx
@@ -1,9 +1,40 @@
 import * as React from 'react';
 
 //Components:
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 //Styles:
+import styled from 'styled-components';
+
+//Interfaces / enums:
+enum ViewerResponseTypeEnum {
+    Text = 'Text',
+    Number = 'Number',
+}
+
+interface IFormInput {
+    InputQuestion: String;
+    ResponseType: ViewerResponseTypeEnum;
+}
+
+const MainContainer = styled.div``;
 
 export const AddViewerInputForm = () => {
-    return <div>Form currently under construction.</div>;
+    const { register, handleSubmit } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+    return (
+        <MainContainer>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Question</label>
+                <input {...register('InputQuestion')} />
+                <label>Response Type</label>
+                <select {...register('ResponseType')}>
+                    <option value="Text">Text</option>
+                    <option value="Number">Number</option>
+                </select>
+                <input type="submit" />
+            </form>
+        </MainContainer>
+    );
 };
