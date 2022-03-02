0be72cb368a0564d39e680e75c2b569d325c25c8
Haz <zhouharry1@gmail.com>
2022-03-01 18:37:55 -0800

    Created initial designs and wrapped with controller components. TODO-get error states working

--- src/components/build_template_page/build_template_components/AddViewerInputForm.tsx
+++ src/components/build_template_page/build_template_components/AddViewerInputForm.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
 //Components:
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { defaultFieldStyle } from '../../../styles/fieldStyles';
+import { TextInput, Select } from '@mantine/core';
 
 //Styles:
 import styled from 'styled-components';
@@ -20,19 +22,62 @@ interface IFormInput {
 const MainContainer = styled.div``;
 
 export const AddViewerInputForm = () => {
-    const { register, handleSubmit } = useForm<IFormInput>();
+    const { handleSubmit, control } = useForm<IFormInput>({
+        defaultValues: {
+            InputQuestion: '',
+            ResponseType: ViewerResponseTypeEnum.Number,
+        },
+    });
+
     const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+    console.log('render');
+
+    //Controller component auto registers for non-native components.
 
     return (
         <MainContainer>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <label>Question</label>
-                <input {...register('InputQuestion')} />
-                <label>Response Type</label>
-                <select {...register('ResponseType')}>
-                    <option value="Text">Text</option>
-                    <option value="Number">Number</option>
-                </select>
+                <Controller
+                    name="InputQuestion"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({
+                        field: { onChange },
+                        fieldState: { error },
+                    }) => (
+                        <TextInput
+                            styles={defaultFieldStyle}
+                            required
+                            label="Question"
+                            onChange={onChange}
+                            // error={error}
+                        />
+                    )}
+                />
+                <Controller
+                    name="ResponseType"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({
+                        //Objects produced by controller are used to send controlled input values back to library
+                        field: { onChange },
+                        fieldState: { error },
+                    }) => (
+                        <Select
+                            label="Response Type"
+                            placeholder="Number"
+                            styles={defaultFieldStyle}
+                            data={[
+                                { value: 'Number', label: 'Number' },
+                                { value: 'Text', label: 'Text' },
+                            ]}
+                            onChange={onChange}
+                            // error={error}
+                        />
+                    )}
+                />
+
                 <input type="submit" />
             </form>
         </MainContainer>
