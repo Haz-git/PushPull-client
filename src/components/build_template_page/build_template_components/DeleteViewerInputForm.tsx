ec57c7cad246daa29301325ec50ed80393b40af0
Haz-git <zhouharry1@gmail.com>
2022-03-03 14:34:58 -0800

    Added deletion component to handle removal of viewer inputs

---
+++ src/components/build_template_page/build_template_components/DeleteViewerInputForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Redux:
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+export const DeleteViewerInputForm = () => {
+    return <div>DeleteViewerInputForm</div>;
+};
