8ebd5f6d85cead813911404ad4ff63049b971daf
Haz <zhouharry1@gmail.com>
2022-03-05 16:04:17 -0800

    Added placeholders for view template actions

---
+++ src/redux/viewTemplates/viewTemplateActions.ts
@@ -0,0 +1,14 @@
+import api from '../../api';
+import { Dispatch } from 'redux';
+import { ViewTemplateActions } from './viewTemplateInterfaces';
+import { uiLoaderAction } from '../uiLoader/uiLoaderInterfaces';
+import { ViewTemplateActionType } from './action-types';
+import { loaderTypes } from '../uiLoader/loader-types';
+
+export const findViewTemplate = () => {};
+
+export const addViewTemplate = () => {};
+
+export const updateViewTemplate = () => {};
+
+export const deleteViewTemplate = () => {};
