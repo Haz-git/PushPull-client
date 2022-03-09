2d1d12c2a2bad6183dffd36831f10817d58cf77b
Haz <zhouharry1@gmail.com>
2022-03-08 18:15:56 -0800

    Added more scaffolding

--- src/redux/viewTemplates/viewTemplateActions.ts
+++ src/redux/viewTemplates/viewTemplateActions.ts
@@ -5,6 +5,12 @@ import { uiLoaderAction } from '../uiLoader/uiLoaderInterfaces';
 import { ViewTemplateActionType } from './action-types';
 import { loaderTypes } from '../uiLoader/loader-types';
 
+type ComposedViewTemplate = {
+    id: string;
+    savedTemplate: any; //Todo..
+    templateFileId: string;
+};
+
 export const findViewTemplate = () => {
     return async (dispatch: Dispatch<ViewTemplateActions>) => {
         try {
@@ -15,9 +21,16 @@ export const findViewTemplate = () => {
     };
 };
 
-export const addViewTemplate = () => {
+export const addViewTemplate = (
+    composedViewTemplate: ComposedViewTemplate
+): Function => {
     return async (dispatch: Dispatch<ViewTemplateActions>) => {
         try {
+            const response = await api.post(`/viewTemplate/add`, {
+                composedViewTemplate,
+            });
+
+            console.log(response);
         } catch (err) {
             //TODO: Link error action creator for error handling.
             console.error(err);
