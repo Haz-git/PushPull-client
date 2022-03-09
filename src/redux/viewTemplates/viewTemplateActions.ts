d0ee74b44bffdd2ddd9cc71b6d7468db0ad68eb4
Haz <zhouharry1@gmail.com>
2022-03-08 18:00:40 -0800

    Updated viewTemplate functions

--- src/redux/viewTemplates/viewTemplateActions.ts
+++ src/redux/viewTemplates/viewTemplateActions.ts
@@ -5,10 +5,42 @@ import { uiLoaderAction } from '../uiLoader/uiLoaderInterfaces';
 import { ViewTemplateActionType } from './action-types';
 import { loaderTypes } from '../uiLoader/loader-types';
 
-export const findViewTemplate = () => {};
+export const findViewTemplate = () => {
+    return async (dispatch: Dispatch<ViewTemplateActions>) => {
+        try {
+        } catch (err) {
+            //TODO: Link error action creator for error handling.
+            console.error(err);
+        }
+    };
+};
 
-export const addViewTemplate = () => {};
+export const addViewTemplate = () => {
+    return async (dispatch: Dispatch<ViewTemplateActions>) => {
+        try {
+        } catch (err) {
+            //TODO: Link error action creator for error handling.
+            console.error(err);
+        }
+    };
+};
 
-export const updateViewTemplate = () => {};
+export const updateViewTemplate = () => {
+    return async (dispatch: Dispatch<ViewTemplateActions>) => {
+        try {
+        } catch (err) {
+            //TODO: Link error action creator for error handling.
+            console.error(err);
+        }
+    };
+};
 
-export const deleteViewTemplate = () => {};
+export const deleteViewTemplate = () => {
+    return async (dispatch: Dispatch<ViewTemplateActions>) => {
+        try {
+        } catch (err) {
+            //TODO: Link error action creator for error handling.
+            console.error(err);
+        }
+    };
+};
