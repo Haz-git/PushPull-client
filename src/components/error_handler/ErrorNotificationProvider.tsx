d1405a66cb0c635f98244d90358c946f6e1be009
Harry Zhou <haz@its-bio-144-232.ucsd.edu>
2022-03-11 14:41:30 -0800

    Scaffolding Error Provider Further..

--- src/components/error_handler/ErrorNotificationProvider.tsx
+++ src/components/error_handler/ErrorNotificationProvider.tsx
@@ -1,18 +1,63 @@
-import * as React from 'react'
+import * as React from 'react';
+import { useEffect } from 'react';
 
 //Components:
+import { useNotifications } from '@mantine/notifications';
 
 //Redux:
+import { useSelector, RootStateOrAny } from 'react-redux';
 
 //Styles:
+import { CancelIcon } from '../build_program_page/build_program_components/AddProjectForm';
 
 //Interfaces:
+import { ErrorProps } from '../../redux/errors/errorInterfaces';
+
+type ActiveErrorType = {
+    hasError: boolean;
+    errorProps: ErrorProps;
+};
 interface IComponentProps {
     children: React.ReactNode;
 }
 
-export const ErrorNotificationProvider = ({ children }: IComponentProps): JSX.Element => {
-  return (
-    <div>ErrorNotificationProvider</div>
-  )
-}
+export const ErrorNotificationProvider = ({
+    children,
+}: IComponentProps): JSX.Element => {
+    const errors = useSelector((state: RootStateOrAny) => state?.errors);
+    const notifications = useNotifications();
+
+    const findActiveError = (): any => {
+        //Check if hasError: true for any errors in store.
+        //TODO: ActiveErrorType throws error.
+        return Object.values(errors).find(
+            (error: any) => error.hasError === true
+        );
+    };
+
+    const displayErrorNotification = () => {
+        const activeErrorObject = findActiveError();
+        console.log(activeErrorObject);
+        if (!activeErrorObject) {
+            return;
+        }
+
+        return notifications.showNotification({
+            color: 'red',
+            title: activeErrorObject.errorProps.errorMessage,
+            message: `An error might have occurred, or you aren't connected to the internet right now. Please report this issue, or try again later.`,
+            autoClose: activeErrorObject.errorProps.openDuration,
+            icon: <CancelIcon />,
+        });
+    };
+
+    useEffect(() => {
+        displayErrorNotification();
+    }, [errors]);
+
+    return (
+        <>
+            <>{children}</>
+        </>
+    );
+};
