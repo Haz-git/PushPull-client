3a4b5405e3e2489fbd8772590c8dbf6961469eeb
Harry Zhou <haz@Harrys-MBP.lan>
2022-03-14 19:20:15 -0700

    updated to camelCase

--- src/redux/errors/action-types/index.ts
+++ src/redux/errors/action-types/index.ts
@@ -1,8 +1,8 @@
 //Appended ERROR here to avoid conflict with other reducer types, e.g QUERY_TEMPLATE
 
 export enum ErrorType {
-    queryTemplateError = 'QUERY_TEMPLATE_ERROR',
-    queryViewTemplateError = 'QUERY_VIEW_TEMPLATE_ERROR',
-    saveViewTemplateError = 'SAVE_VIEW_TEMPLATE_ERROR',
-    viewPreviewError = 'VIEW_PREVIEW_ERROR',
+    queryTemplateError = 'queryTemplateError',
+    queryViewTemplateError = 'queryViewTemplateError',
+    saveViewTemplateError = 'saveViewTemplateError',
+    viewPreviewError = 'viewPreviewError',
 }
