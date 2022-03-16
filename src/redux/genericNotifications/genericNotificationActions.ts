e0e88d5c5cb7f9fa91ff920078ca4357d6b6ca71
Harry Zhou <haz@its-bio-144-232.ucsd.edu>
2022-03-16 13:27:38 -0700

    Added actions for toggling and resetting generic notifications

--- src/redux/genericNotifications/genericNotificationActions.ts
+++ src/redux/genericNotifications/genericNotificationActions.ts
@@ -4,3 +4,49 @@ import {
     GenericNotificationAction,
     NotificationProps,
 } from './genericNotificationInterfaces';
+
+export const toggleGenericNotification = (
+    genericNotificationType: GenericNotificationType,
+    notificationProps: NotificationProps
+): any => {
+    //Todo: Find types here
+    return async (dispatch: Dispatch<GenericNotificationAction>) => {
+        if (
+            !Object.values(GenericNotificationType).includes(
+                genericNotificationType
+            )
+        ) {
+            return;
+        }
+
+        return dispatch({
+            type: genericNotificationType,
+            payload: {
+                shouldDisplay: true,
+                notificationProps,
+            },
+        });
+    };
+};
+
+export const resetGenericNotification = (
+    genericNotificationType: GenericNotificationType
+): any => {
+    return async (dispatch: Dispatch<GenericNotificationAction>) => {
+        if (
+            !Object.values(GenericNotificationType).includes(
+                genericNotificationType
+            )
+        ) {
+            return;
+        }
+
+        return dispatch({
+            type: genericNotificationType,
+            payload: {
+                shouldDisplay: false,
+                notificationProps: {},
+            },
+        });
+    };
+};
