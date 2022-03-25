216ca31a8f92fc019e277a43dab836722e81699d
Haz <zhouharry1@gmail.com>
2022-03-25 14:23:38 -0700

    Scaffolding input items and list

---
+++ src/components/view_template_page/view_template_components/InputItem.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Redux:
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+export const InputItem = () => {
+    return <div>InputItem</div>;
+};
---
+++ src/components/view_template_page/view_template_components/InputList.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+//Redux:
+
+//Components:
+
+//Styles:
+
+//Interface
+
+export const InputList = () => {
+    return <div>InputList</div>;
+};
