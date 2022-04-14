c5a605ae568978a1581ef020b530573151a41f36
Haz <zhouharry1@gmail.com>
2022-04-14 16:52:38 -0700

    Created two new views for fixed toolbar

---
+++ src/components/view_template_page/view_template_components/FixedToolbarAnonymousView.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+//Redux
+
+//Components
+
+//Styles
+
+//Interfaces
+
+/**
+ * @description This view differs from FixedToolbarGuestView as this view is meant for users that are not logged into PushPull.
+ */
+
+export const FixedToolbarAnonymousView = () => {
+    return <div>FixedToolbarAnonymousView</div>;
+};
---
+++ src/components/view_template_page/view_template_components/FixedToolbarGuestView.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+//Redux
+
+//Components
+
+//Styles
+
+//Interfaces
+
+/**
+ * @description This view differs from FixedToolbarAnonymousView as this view is meant for users that are logged into PushPull.
+ */
+export const FixedToolbarGuestView = () => {
+    return <div>FixedToolbarGuestView</div>;
+};
