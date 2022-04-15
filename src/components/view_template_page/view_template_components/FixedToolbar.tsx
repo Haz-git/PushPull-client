629b473933b3fae3b6f97600196f7047db81a128
Haz <zhouharry1@gmail.com>
2022-04-14 17:02:34 -0700

    Scaffolding owner view

--- src/components/view_template_page/view_template_components/FixedToolbar.tsx
+++ src/components/view_template_page/view_template_components/FixedToolbar.tsx
@@ -9,6 +9,9 @@ import { Tooltip } from '@mantine/core';
 import GeneralButton from '../../general_components/GeneralButton';
 import historyObject from '../../../utils/historyObject';
 import { TemplateDocument } from './TemplateDocument';
+import { FixedToolbarOwnerView } from './FixedToolbarOwnerView';
+import { FixedToolbarAnonymousView } from './FixedToolbarAnonymousView';
+import { FixedToolbarGuestView } from './FixedToolbarGuestView';
 
 //Styles:
 import styled from 'styled-components';
@@ -18,31 +21,31 @@ import { FileCopy } from '@styled-icons/material-rounded/FileCopy';
 import { CloudUpload } from '@styled-icons/material-rounded/CloudUpload';
 import { KeyboardBackspace } from '@styled-icons/material-rounded/KeyboardBackspace';
 
-const DownloadIcon = styled(FileDownload)`
+export const DownloadIcon = styled(FileDownload)`
     height: 1.5rem;
     width: 1.5rem;
     color: #ffffff;
 `;
 
-const ViewingIcon = styled(RemoveRedEye)`
+export const ViewingIcon = styled(RemoveRedEye)`
     height: 1.5rem;
     width: 1.5rem;
     color: #ffffff;
 `;
 
-const CopyIcon = styled(FileCopy)`
+export const CopyIcon = styled(FileCopy)`
     height: 1.5rem;
     width: 1.5rem;
     color: #ffffff;
 `;
 
-const PublishIcon = styled(CloudUpload)`
+export const PublishIcon = styled(CloudUpload)`
     height: 1.5rem;
     width: 1.5rem;
     color: #ffffff;
 `;
 
-const BackIcon = styled(KeyboardBackspace)`
+export const BackIcon = styled(KeyboardBackspace)`
     height: 1.5rem;
     width: 1.5rem;
     color: #ffffff;
@@ -60,7 +63,7 @@ const MainContainer = styled.div`
 
 const ButtonContainer = styled.div``;
 
-const ButtonWrapper = styled.div`
+export const ButtonWrapper = styled.div`
     padding: 0rem 0.75rem;
 `;
 
@@ -97,67 +100,11 @@ export const FixedToolbar = ({
                     </PDFDownloadLink>
                 </ButtonWrapper>
             </Tooltip>
-            <Tooltip
-                label="Configure Viewing Access"
-                position="right"
-                placement="center"
-            >
-                <ButtonWrapper>
-                    <GeneralButton
-                        buttonLabel=""
-                        buttonIconLeft={<ViewingIcon />}
-                        leftIconMargin="0"
-                        rightIconMargin="0"
-                    />
-                </ButtonWrapper>
-            </Tooltip>
-            <Tooltip
-                label="Configure Duplication Access"
-                position="right"
-                placement="center"
-            >
-                <ButtonWrapper>
-                    <GeneralButton
-                        buttonLabel=""
-                        buttonIconLeft={<CopyIcon />}
-                        leftIconMargin="0"
-                        rightIconMargin="0"
-                    />
-                </ButtonWrapper>
-            </Tooltip>
-            <Tooltip
-                label="Upload To PushPull Ranking Catalog"
-                position="right"
-                placement="center"
-            >
-                <ButtonWrapper>
-                    <GeneralButton
-                        buttonLabel=""
-                        buttonIconLeft={<PublishIcon />}
-                        leftIconMargin="0"
-                        rightIconMargin="0"
-                    />
-                </ButtonWrapper>
-            </Tooltip>
-            <Tooltip
-                label="Return To Templates"
-                position="right"
-                placement="center"
-            >
-                <ButtonWrapper>
-                    <GeneralButton
-                        buttonLabel=""
-                        buttonIconLeft={<BackIcon />}
-                        leftIconMargin="0"
-                        rightIconMargin="0"
-                        onClick={() =>
-                            historyObject.push(
-                                `/file/${templateId}?sheetId=${onReturnSheetId}`
-                            )
-                        }
-                    />
-                </ButtonWrapper>
-            </Tooltip>
+            <FixedToolbarOwnerView
+                templateId={templateId}
+                onReturnSheetId={onReturnSheetId}
+                shouldDisplay={true}
+            />
         </MainContainer>
     );
 };
