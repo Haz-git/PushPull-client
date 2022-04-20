38cc5b223fcc808e46a0e23468cf5c626cadfdef
Harry Zhou <haz@its-bio-144-232.ucsd.edu>
2022-04-20 13:02:16 -0700

    Added default time styles

--- src/styles/fieldStyles.ts
+++ src/styles/fieldStyles.ts
@@ -13,3 +13,19 @@ export const defaultFieldStyle = {
         fontWeight: 500,
     },
 };
+
+export const defaultTimeFieldStyle = {
+    label: {
+        color: 'rgba(0, 0, 34, .7)',
+        fontFamily: 'Lato, sans-serif',
+        fontSize: '.9rem',
+        fontWeight: 700,
+        marginBottom: '.25rem',
+    },
+    input: {
+        color: 'rgba(0, 0, 34, 1)',
+        fontFamily: 'Lato, sans-serif',
+        fontSize: '.8rem',
+        fontWeight: 500,
+    },
+};
