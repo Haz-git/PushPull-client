9e73a1657c0771d8719ee83c11b8b87041f3be03
Haz <zhouharry1@gmail.com>
2022-04-21 14:13:44 -0700

    Init facade over react-simple-timefield

---
+++ src/components/general_components/UITimeField.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+//Redux:
+
+//Components:
+
+//Styles:
+
+//Interfaces:
+
+/**
+ * @description Facade over react-simple-timefield
+ *
+ */
+
+export const UITimeField = () => {
+    return <div>UITimeField</div>;
+};
