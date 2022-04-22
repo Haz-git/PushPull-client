97fff3309f81890c7fc2cb350d6ef3830345c42e
Harry Zhou <haz@its-bio-144-232.ucsd.edu>
2022-04-22 14:53:29 -0700

    Added new rest block styling

--- src/components/build_template_page/build_template_components/BlockTypeExerciseDataGrid.tsx
+++ src/components/build_template_page/build_template_components/BlockTypeExerciseDataGrid.tsx
@@ -5,8 +5,11 @@ import Text from '../../general_components/Text';
 //Styles:
 import styled from 'styled-components';
 
-export const BlockExerciseLengthContainer = styled.div`
+const MainContainer = styled.div`
     padding: 0.5rem 0.25rem 0.5rem 0.25rem;
+`;
+
+const BlockExerciseLengthContainer = styled.div`
     display: grid;
     align-items: center;
     justify-content: center;
@@ -18,6 +21,15 @@ const ExerciseDetails = styled.div`
     padding: 0.2rem 0.2rem;
     background: #ececec;
     margin: 0 0.1rem;
+    border-radius: 0.3rem;
+`;
+
+export const RestBlock = styled.div`
+    width: 98.5%;
+    text-align: center;
+    padding: 0.2rem 0.2rem;
+    background: #ececec;
+    margin: 0.2rem 0.2rem 0rem 0.1rem;
     border-radius: 0.3rem;
 `;
 
@@ -33,6 +45,7 @@ interface IComponentProps {
     reps: string;
     weightUnit: string | undefined;
     weight: string | undefined;
+    restTime: string;
 }
 
 export const BlockTypeExerciseDataGrid = ({
@@ -41,58 +54,69 @@ export const BlockTypeExerciseDataGrid = ({
     reps,
     weightUnit,
     weight,
+    restTime,
 }: IComponentProps): JSX.Element => {
     //This component is the triple-column grid layout underneath block exercise name.
     return (
         <>
             {shouldShowGridLayout ? (
-                <BlockExerciseLengthContainer>
-                    <ExerciseDetails>
+                <MainContainer>
+                    <BlockExerciseLengthContainer>
+                        <ExerciseDetails>
+                            <Text
+                                text={`Sets`}
+                                fontSize=".75rem"
+                                fontWeight="800"
+                                subText={true}
+                            />
+                            <ExerciseDetailSpacer />
+                            <Text
+                                text={sets}
+                                fontSize="1rem"
+                                fontWeight="800"
+                                mainText={true}
+                            />
+                        </ExerciseDetails>
+                        <ExerciseDetails>
+                            <Text
+                                text={`Reps`}
+                                fontSize=".75rem"
+                                fontWeight="800"
+                                subText={true}
+                            />
+                            <ExerciseDetailSpacer />
+                            <Text
+                                text={reps}
+                                fontSize="1rem"
+                                fontWeight="800"
+                                mainText={true}
+                            />
+                        </ExerciseDetails>
+                        <ExerciseDetails>
+                            <Text
+                                text={weightUnit}
+                                fontSize=".75rem"
+                                fontWeight="800"
+                                subText={true}
+                            />
+                            <ExerciseDetailSpacer />
+                            <Text
+                                text={weight}
+                                fontSize="1rem"
+                                fontWeight="800"
+                                mainText={true}
+                            />
+                        </ExerciseDetails>
+                    </BlockExerciseLengthContainer>
+                    <RestBlock>
                         <Text
-                            text={`Sets`}
+                            text={`Rest: ${restTime} m`}
                             fontSize=".75rem"
-                            fontWeight="800"
-                            subText={true}
-                        />
-                        <ExerciseDetailSpacer />
-                        <Text
-                            text={sets}
-                            fontSize="1rem"
                             fontWeight="800"
                             mainText={true}
                         />
-                    </ExerciseDetails>
-                    <ExerciseDetails>
-                        <Text
-                            text={`Reps`}
-                            fontSize=".75rem"
-                            fontWeight="800"
-                            subText={true}
-                        />
-                        <ExerciseDetailSpacer />
-                        <Text
-                            text={reps}
-                            fontSize="1rem"
-                            fontWeight="800"
-                            mainText={true}
-                        />
-                    </ExerciseDetails>
-                    <ExerciseDetails>
-                        <Text
-                            text={weightUnit}
-                            fontSize=".75rem"
-                            fontWeight="800"
-                            subText={true}
-                        />
-                        <ExerciseDetailSpacer />
-                        <Text
-                            text={weight}
-                            fontSize="1rem"
-                            fontWeight="800"
-                            mainText={true}
-                        />
-                    </ExerciseDetails>
-                </BlockExerciseLengthContainer>
+                    </RestBlock>
+                </MainContainer>
             ) : null}
         </>
     );
