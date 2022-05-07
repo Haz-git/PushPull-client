375480962ff73821a78db2216b7f3a0ea6f908ae
Haz <zhouharry1@gmail.com>
2022-05-07 07:28:14 -0700

    Updated tests for buttons

--- src/components/nav_bar/Navbar.test.tsx
+++ src/components/nav_bar/Navbar.test.tsx
@@ -7,7 +7,6 @@ import Navbar from './Navbar';
 /*
  * Testing Navbar:
  * - Should render at all times.
- * - Should have logo at left at all times.
  * - Check if isUserLoggedIn boolean is true -> render UserDropdown
  * - If isUserLoggedIn boolean is false -> render auth options
  * - Clicking auth options should open authentication drawer.
@@ -26,3 +25,26 @@ describe('Navbar', () => {
         expect(queryByTestId(/navbar/i)).toBeTruthy();
     });
 });
+
+describe('when isUserLogged is false', () => {
+    test('should render both login and signup buttons', () => {
+        const { getByText } = render(
+            <TestWrapper>
+                <Navbar
+                    isUserLoggedIn={false}
+                    toggleAuthDrawerWithView={(state, view) => {}}
+                />
+            </TestWrapper>
+        );
+
+        expect(() =>
+            getByText(/Log in/i, { selector: 'button' })
+        ).not.toThrow();
+
+        expect(() =>
+            getByText(/Sign up/i, { selector: 'button' })
+        ).not.toThrow();
+    });
+});
+
+//To be cont.
